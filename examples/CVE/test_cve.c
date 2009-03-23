02652bb5b461cf6cc8bda2629e4ba25c3aa62804
Daniel Kopecek <mildew@sapropelus.(none)>
2009-03-23 13:04:35 +0100

    Modified & enabled CVE test

--- examples/CVE/test_cve.c
+++ examples/CVE/test_cve.c
@@ -17,21 +17,21 @@ int main(int argc, char **argv)
 		return -1;
 	}
 
-	fprintf(stderr, "[begin: parse]\n");
+	fprintf(stdout, "[begin: parse]\n");
 	i = cveParse(argv[1], &cveList);
 	if (i < 0) {
 		fprintf(stderr, "cveParse returned error: %d.\n", i);
 		return -1;
 	}
-	fprintf(stderr, "[end: parse]\n");
-	fprintf(stderr, "[done]\n");
+	fprintf(stdout, "[end: parse]\n");
+	fprintf(stdout, "[done]\n");
 
 	fprintf(stderr, "Total cves processed: %d\n", i);
 
 	cve = cveList;
 	while (cve != NULL) {
-		fprintf(stderr, "[cveInfo: %s, %s, %s, %s, %s]\n", cve->id, cve->pub, cve->mod, cve->cwe ? cve->cwe : "", cve->summary);
-		fprintf(stderr, "[cvss: %s, %s, %s, %s, %s, %s, %s, %s, %s]\n", cve->score, cve->vector, cve->complexity, cve->authentication, cve->confidentiality, cve->integrity, cve->availability, cve->source, cve->generated);
+		fprintf(stdout, "[cveInfo: %s, %s, %s, %s, %s]\n", cve->id, cve->pub, cve->mod, cve->cwe ? cve->cwe : "", cve->summary);
+		fprintf(stdout, "[cvss: %s, %s, %s, %s, %s, %s, %s, %s, %s]\n", cve->score, cve->vector, cve->complexity, cve->authentication, cve->confidentiality, cve->integrity, cve->availability, cve->source, cve->generated);
 
 		i = 0;
 		ref = cve->refs;
@@ -39,14 +39,14 @@ int main(int argc, char **argv)
 			i++;
 			ref = ref->next;
 		}
-		fprintf(stderr, "References: %d\n", i);
+		fprintf(stdout, "References: %d\n", i);
 
 		ref = cve->refs;
 		while (ref != NULL) {
-			fprintf(stderr, "\t[cveReference: %s, %s, %s, %s]\n", ref->summary, ref->href, ref->refType, ref->source);
+			fprintf(stdout, "\t[cveReference: %s, %s, %s, %s]\n", ref->summary, ref->href, ref->refType, ref->source);
 			ref = ref->next;
 		}
-		fprintf(stderr, "\n");
+		fprintf(stdout, "\n");
 
 		cve = cve->next;
 	}
