4fd6c22ab74eeb81e7f52b60ad2a18bbbf381e35
Peter Vrabec <pvrabec@redhat.com>
2009-04-03 12:24:02 +0200

    OVAL: add license info into every source
    - add OVAL files to doxygen

--- src/OVAL/oval_agent_api_impl.h
+++ src/OVAL/oval_agent_api_impl.h
@@ -1,14 +1,38 @@
-/*
- * oval_agent_api_impl.h
+/**
+ * @file oval_agent_api_impl.h
+ * \brief Open Vulnerability and Assessment Language
  *
- *  Created on: Mar 9, 2009
- *      Author: Compaq_Administrator
+ * See more details at http://oval.mitre.org/
+ */
+
+/*
+ * Copyright 2008 Red Hat Inc., Durham, North Carolina.
+ * All Rights Reserved.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Authors:
+ *      "David Niemoller" <David.Niemoller@g2-inc.com>
  */
 
 #ifndef OVAL_AGENT_API_IMPL_H_
 #define OVAL_AGENT_API_IMPL_H_
+
 #include "api/oval_agent_api.h"
 #include "oval_definitions_impl.h"
+
 void add_oval_definition(struct oval_object_model *, struct oval_definition *);
 void add_oval_test(struct oval_object_model *, struct oval_test *);
 void add_oval_object(struct oval_object_model *, struct oval_object *);
@@ -21,4 +45,5 @@ struct oval_test *get_oval_test_new(struct oval_object_model *, char *);
 struct oval_object *get_oval_object_new(struct oval_object_model *, char *);
 struct oval_state *get_oval_state_new(struct oval_object_model *, char *);
 struct oval_variable *get_oval_variable_new(struct oval_object_model *, char *);
+
 #endif				/* OVAL_AGENT_API_IMPL_H_ */
--- src/OVAL/oval_value.c
+++ src/OVAL/oval_value.c
@@ -1,9 +1,32 @@
-/*
- * oval_value.c
+/**
+ * @file oval_value.c
+ * \brief Open Vulnerability and Assessment Language
  *
- *  Created on: Mar 3, 2009
- *      Author: david.niemoller
+ * See more details at http://oval.mitre.org/
  */
+
+/*
+ * Copyright 2008 Red Hat Inc., Durham, North Carolina.
+ * All Rights Reserved.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Authors:
+ *      "David Niemoller" <David.Niemoller@g2-inc.com>
+ */
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
