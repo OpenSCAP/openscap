ff72d73844c428eb6e5111e233f197c0fafe79ed
Daniel Kopecek <mildew@paranoiac.(none)>
2009-04-17 14:52:47 +0200

    More checks in runtests.sh

--- runtests.sh
+++ runtests.sh
@@ -2,14 +2,43 @@
 
 # Cleanup first
 gmake -s clean distclean
-
 ./autogen.sh || exit 1
 
-export CFLAGS="-fmudflap -lmudflap"
+# fstack-protector-all
+echo '***************** CHECK: SSP *******************'
+
+export CFLAGS="-fstack-protector-all -g -O1"
+./configure && gmake -s && gmake check || exit 1
+gmake -s clean
+
+# mudflap
+echo '***************** CHECK: mudflap *******************'
+
+export CFLAGS="-fmudflap -lmudflap -g -O1"
+export MUDFLAP_OPTIONS='-mode-nop'
 ./configure && gmake -s || exit 1
- 
 export MUDFLAP_OPTIONS='-mode-check -viol-abort -check-initialization -heur-start-end -heur-stack-bound -heur-proc-map -internal-checking'
 gmake -s check || exit 1
+gmake -s clean
 
-export MUDFLAP_OPTIONS='-mode-nop'
-gmake distcheck -s || exit 1
+# malloc checks
+echo '***************** CHECK: malloc *******************'
+
+export CFLAGS="-g -O1"
+./configure && gmake -s || exit 1
+case $(uname -s) in;
+	[Ll][Ii][Nn][Uu][Xx])
+		export MALLOC_CHECK_=3
+		;;
+	[Ff][Rr][Ee][Ee][Bb][Ss][Dd])
+		export MALLOC_OPTIONS=A
+		;;
+	*)
+		echo "Sorry, don't know how to enable malloc debuging"
+		exit 1
+		;;
+esac
+gmake -s check || exit 1
+unset MALLOC_CHECK_
+unset MALLOC_OPTIONS
+gmake -s clean
