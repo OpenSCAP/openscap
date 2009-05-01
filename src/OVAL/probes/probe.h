9c0c9e3bb42c66de99b8db0a97e79fdaa650fbb3
Daniel Kopecek <dkopecek@redhat.com>
2009-05-01 13:01:23 +0200

    New configure option for enabling/disabling Fedora/RHEL probes

--- src/OVAL/probes/probe.h
+++ src/OVAL/probes/probe.h
@@ -10,6 +10,10 @@ typedef struct {
 #include <stdarg.h>
 
 SEXP_t *SEXP_OVALobj_create (const char *obj_name, OVALobj_attr_t obj_attrs[], ...);
+
+#define SEXP_OVALobj_getelement_value SEXP_OVALobj_getelm_val
+SEXP_t *SEXP_OVALobj_getelm_val (SEXP_t *obj, const char *name);
+
 SEXP_t *oval_object_to_sexp (const char *typestr, struct oval_object *object);
 
 #endif /* PROBE_H */
