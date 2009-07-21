4644dec891e46d3553696b012bb99f9e436996c6
Daniel Kopecek <dkopecek@redhat.com>
2009-07-21 10:25:41 +0200

    New malloc wrapper

---
+++ src/common/alloc.c
@@ -0,0 +1,144 @@
+#include <stdlib.h>
+#include <errno.h>
+#include "alloc.h"
+
+#if defined(NDEBUG)
+/*
+ * Normal
+ */
+
+void *__oscap_alloc (size_t s)
+{
+        void *m;
+#if defined(OSCAP_ALLOC_STRICT)
+        _A(s > 0);
+#endif
+        m = malloc (s);
+#if defined(OSCAP_ALLOC_EXIT)
+        if (m == NULL)
+                exit (ENOMEM);
+#endif
+        return (m);
+}
+
+void *__oscap_calloc (size_t n, size_t s)
+{
+        void *m;
+#if defined(OSCAP_ALLOC_STRICT)
+        _A(n > 0);
+        _A(s > 0);
+#endif
+        m = calloc (n, s);
+#if defined(OSCAP_ALLOC_EXIT)
+        if (m == NULL)
+                exit (ENOMEM);
+#endif
+        return (m);
+}
+
+void *__oscap_realloc (void *p, size_t s)
+{
+        void *m;
+
+        m = realloc (p, s);
+#if defined(OSCAP_ALLOC_EXIT)
+        if (m == NULL && s > 0)
+                exit (ENOMEM);
+#endif
+        return (m);
+}
+
+void *__oscap_reallocf (void *p, size_t s)
+{
+        void *m;
+
+        m = realloc (p, s);
+        if (m == NULL && s > 0) {
+                oscap_free (p);
+#if defined(OSCAP_ALLOC_EXIT)
+                exit (ENOMEM);
+#endif
+        }
+        return (m);
+}
+
+void __oscap_free (void *p)
+{
+#if defined(OSCAP_ALLOC_STRICT)
+        _A(p != NULL);
+#endif
+        if (p != NULL)
+                free (p);
+        return;
+}
+
+#else
+/*
+ * Debug
+ */
+
+void *__oscap_alloc_dbg (size_t s, const char *func, size_t line)
+{
+        void *m;
+#if defined(OSCAP_ALLOC_STRICT)
+        _A(s > 0);
+#endif
+        m = malloc (s);
+#if defined(OSCAP_ALLOC_EXIT)
+        if (m == NULL)
+                exit (ENOMEM);
+#endif
+        return (m);
+}
+
+void *__oscap_calloc_dbg (size_t n, size_t s, const char *f, size_t l)
+{
+        void *m;
+#if defined(OSCAP_ALLOC_STRICT)
+        _A(n > 0);
+        _A(s > 0);
+#endif
+        m = calloc (n, s);
+#if defined(OSCAP_ALLOC_EXIT)
+        if (m == NULL)
+                exit (ENOMEM);
+#endif
+        return (m);
+}
+
+void *__oscap_realloc_dbg (void *p, size_t s, const char *f, size_t l)
+{
+        void *m;
+        m = realloc (p, s);
+#if defined(OSCAP_ALLOC_EXIT)
+        if (m == NULL && s > 0)
+                exit (ENOMEM);
+#endif
+        return (m);
+}
+
+void *__oscap_reallocf_dbg (void *p, size_t s, const char *f, size_t l)
+{
+        void *m;
+        m = realloc (p, s);
+        if (m == NULL && s > 0) {
+                oscap_free (p);
+#if defined(OSCAP_ALLOC_EXIT)
+                exit (ENOMEM);
+#endif
+        }
+        return (m);
+}
+
+void __oscap_free_dbg (void **p, const char *f, size_t l)
+{
+#if defined(OSCAP_ALLOC_STRICT)
+        _A(p != NULL);
+#endif
+        free (*p);
+#if defined(OSCAP_ALLOC_RESET)
+        *p = NULL;
+#endif
+        return;
+}
+#endif
---
+++ src/common/alloc.h
@@ -0,0 +1,40 @@
+#pragma once
+#ifndef OSCAP_ALLOC_H
+#define OSCAP_ALLOC_H
+
+#if defined(NDEBUG)
+void *__oscap_alloc    (size_t s);
+void *__oscap_calloc   (size_t n, size_t s);
+void *__oscap_realloc  (void  *p, size_t s);
+void *__oscap_reallocf (void  *p, size_t s);
+void  __oscap_free     (void  *p);
+
+# define oscap_alloc(s)       __oscap_alloc (s)
+# define oscap_calloc(n, s)   __oscap_calloc (n, s);
+# define oscap_realloc(p, s)  __oscap_realloc ((void *)(p), s)
+# define oscap_reallocf(p, s) __oscap_reallocf((void *)(p), s)
+# define oscap_free(p)        __oscap_free ((void *)(p))
+#else
+void *__oscap_alloc_dbg    (size_t s,           const char *f, size_t l);
+void *__oscap_calloc_dbg   (size_t n, size_t s, const char *f, size_t l);
+void *__oscap_realloc_dbg  (void  *p, size_t s, const char *f, size_t l);
+void *__oscap_reallocf_dbg (void  *p, size_t s, const char *f, size_t l);
+void  __oscap_free_dbg     (void **p,           const char *f, size_t l);
+
+# define oscap_alloc(s)       __oscap_alloc_dbg (s, __PRETTY_FUNCTION__, __LINE__)
+# define oscap_calloc(n, s)   __oscap_calloc_dbg (n, s, __PRETTY_FUNCTION__, __LINE__)
+# define oscap_realloc(p, s)  __oscap_realloc_dbg ((void *)(p), s, __PRETTY_FUNCTION__, __LINE__)
+# define oscap_reallocf(p, s) __oscap_reallocf_dbg ((void *)(p), s, __PRETTY_FUNCTION__, __LINE__)
+# define oscap_free(p)        __oscap_free_dbg ((void **)&(p), __PRETTY_FUNCTION__, __LINE__)
+#endif
+
+#define  oscap_talloc(T) ((T *) oscap_alloc(sizeof(T)))
+#define  oscap_valloc(v) ((typeof(v) *) oscap_alloc(sizeof v))
+
+#if defined(OSCAP_ALLOC_STRICT)
+# include <assert.h>
+#ifndef _A
+# define _A(x) assert(x)
+#endif /* _A */
+#endif /* OSCAP_ALLOC_STRICT */
+#endif /* OSCAP_ALLOC_H */
