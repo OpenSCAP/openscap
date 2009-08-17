84a464d8c74f653872765096474624be785e8f82
Daniel Kopecek <dkopecek@redhat.com>
2009-08-17 12:17:48 +0200

    [SEAP] pqueue update

--- src/OVAL/probes/SEAP/generic/pqueue.h
+++ src/OVAL/probes/SEAP/generic/pqueue.h
@@ -4,11 +4,26 @@
 
 typedef struct pqueue pqueue_t;
 
+#define PQUEUE_EFAIL  1
+#define PQUEUE_EFULL  2
+#define PQUEUE_EEMPTY 3
+
 pqueue_t *pqueue_new  (size_t max);
 void      pqueue_free (pqueue_t *q);
 
-int   pqueue_add   (pqueue_t *q, void *ptr);
+int pqueue_add (pqueue_t *q, void *ptr);
+
+int pqueue_add_first (pqueue_t *q, void *ptr);
+int pqueue_add_last  (pqueue_t *q, void *ptr);
+
 void *pqueue_first (pqueue_t *q);
 void *pqueue_last  (pqueue_t *q);
+
+void *pqueue_pick (pqueue_t *q, int (*pickp) (void *ptr));
+void *pqueue_pick_first (pqueue_t *q, int (*pickp) (void *ptr));
+void *pqueue_pick_last  (pqueue_t *q, int (*pickp) (void *ptr));
+
+size_t pqueue_count (pqueue_t *q);
+int    pqueue_notempty (pqueue_t *q);
 
 #endif /* PQUEUE_H */
