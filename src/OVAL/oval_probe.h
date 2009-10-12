fcb93f5a5ac753d515e8397af06a9f29d6127dae
Peter Vrabec <pvrabec@redhat.com>
2009-10-12 13:58:39 +0200

    adjust paths to probes

--- src/OVAL/oval_probe.h
+++ src/OVAL/oval_probe.h
@@ -16,7 +16,7 @@ OSCAP_HIDDEN_START;
 
 #define OVAL_PROBE_SCHEME "pipe"
 #ifndef OVAL_PROBE_DIR
-# define OVAL_PROBE_DIR    "/usr/libexec/openscap/oval"
+# define OVAL_PROBE_DIR    "/usr/libexec/openscap"
 #endif
 
 typedef struct {
