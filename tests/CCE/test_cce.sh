de243b6fbd6e4b8471c1f90fa2a9f248f50a0ea8
Maros Barabas <mbarabas@redhat.com>
2009-11-23 10:57:18 +0100

    Added CCE & CVSS to test suite

--- tests/CCE/test_cce.sh
+++ tests/CCE/test_cce.sh
@@ -1,3 +1,69 @@
 #!/bin/sh
-./test_cce "${srcdir}/CCE/CCE_Sample.xml" CCE-3416-5 >/dev/null && \
-./test_cce "${srcdir}/CCE/CCE_Sample.xml" CCE-4218-4 >/dev/null
+
+# Copyright 2008 Red Hat Inc., Durham, North Carolina.
+# All Rights Reserved.
+#
+# OpenScap CCE Module Test Suite.
+#
+# Authors:
+#      Maros Barabas <mbarabas@redhat.com>
+#
+
+. ${srcdir}/test_common.sh
+
+# Setup.
+function test_cce_setup {
+    local ret_val=0
+    
+    return $ret_val
+}
+
+# Test Cases.
+
+function test_cce_tc00 {
+    local ret_val=0
+
+    ./test_cce "${srcdir}/CCE/CCE_Sample.xml" CCE-3416-5 > test_cce.out.1
+    ret_val=$?
+
+    return $ret_val
+}
+
+function test_cce_tc01 {
+
+    local ret_val=0
+
+    ./test_cce "${srcdir}/CCE/CCE_Sample.xml" CCE-4218-4 > test_cce.out.2
+    ret_val=$?
+
+    return $ret_val
+}
+
+# Cleanup.
+function test_cce_cleanup {
+    local ret_val=0
+
+    rm test_cce.out.1 test_cce.out.2
+    ret_val=$?
+    return $ret_val
+}
+
+# TESTING.
+
+echo "------------------------------------------"
+
+result=0
+log=test_cce.log
+
+exec 2>$log
+
+#---- function ------#-------------------------- reporting -----------------------#--------------------------#
+test_cce_setup    ; ret_val=$? ; report_result "test_CCE_setup"   $ret_val  ; result=$[$result+$ret_val]
+test_cce_tc00     ; ret_val=$? ; report_result 'Test_CCE_TC00'    $ret_val  ; result=$[$result+$ret_val]   
+test_cce_tc01     ; ret_val=$? ; report_result 'Test_CCE_TC01'    $ret_val  ; result=$[$result+$ret_val]   
+test_cce_cleanup  ; ret_val=$? ; report_result "test_CCE_cleanup" $ret_val  ; result=$[$result+$ret_val]
+
+echo "------------------------------------------"
+echo "See ${srcdir}/${log}"
+
+exit $result
--- tests/CVSS/test_cvsscalc.sh
+++ tests/CVSS/test_cvsscalc.sh
@@ -1,4 +1,57 @@
 #!/bin/sh
 
-./test_cvsscalc
+# Copyright 2008 Red Hat Inc., Durham, North Carolina.
+# All Rights Reserved.
+#
+# OpenScap CVSS Module Test Suite.
+#
+# Authors:
+#      Maros Barabas <mbarabas@redhat.com>
+#
+
+. ${srcdir}/test_common.sh
+
+# Setup.
+function test_cvss_setup {
+    local ret_val=0
+    
+    return $ret_val
+}
+
+# Test Cases.
+
+function test_cvss_tc00 {
+    local ret_val=0
+
+    ./test_cvsscalc 2>&1 > test_cvss.out
+    ret_val=$?
+
+    return $ret_val
+}
+
+# Cleanup.
+function test_cvss_cleanup {
+    local ret_val=0
+
+    return $ret_val
+}
+
+# TESTING.
+
+echo "------------------------------------------"
+
+result=0
+log=test_cvss.log
+
+exec 2>$log
+
+#---- function ------#-------------------------- reporting -----------------------#--------------------------#
+test_cvss_setup    ; ret_val=$? ; report_result "test_CVSS_setup"   $ret_val  ; result=$[$result+$ret_val]
+test_cvss_tc00     ; ret_val=$? ; report_result 'Test_CVSS_TC01'    $ret_val  ; result=$[$result+$ret_val]   
+test_cvss_cleanup  ; ret_val=$? ; report_result "test_CVSS_cleanup" $ret_val  ; result=$[$result+$ret_val]
+
+echo "------------------------------------------"
+echo "See ${srcdir}/${log}"
+
+exit $result
 
