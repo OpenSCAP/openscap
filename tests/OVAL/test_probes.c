978cc074c55b0ce13124aa6d038964e19488f618
Tomas Heinrich <theinric@redhat.com>
2009-11-26 14:30:48 +0100

    update tests/OVAL/test_probes.c

--- tests/OVAL/test_probes.c
+++ tests/OVAL/test_probes.c
@@ -14,7 +14,6 @@
 
 int _test_error_handler(struct oval_xml_error *error, void *null)
 {
-	//ERROR HANDLING IS TODO
 	return 1;
 }
 
@@ -22,41 +21,80 @@ void oval_syschar_to_print(struct oval_syschar*, const char*, int);
 
 int main(int argc, char **argv)
 {
+	int idx, ret;
         oval_pctx_t *pctx;
+	char *id, *probe_dir;
+	struct oval_definition_model *definition_model;
+	struct oval_syschar_model *syschar_model;
+	struct oval_import_source *source;
+	struct oval_object_iterator *obj_it;
+	struct oval_object *object;
+	struct oval_syschar *syschar;
 
-	struct oval_definition_model *model = oval_definition_model_new();
-
-	struct oval_import_source *is = oval_import_source_new_file(argv[1]);
-	oval_definition_model_import(model, is, &_test_error_handler, NULL);
-	oval_import_source_free(is);
-
-	struct oval_object_iterator *objects = oval_definition_model_get_objects(model);
-	if (!oval_object_iterator_has_more(objects)) {
-		printf("NO DEFINITIONS FOUND\n");
+	if (argc != 2) {
+		printf("usage: %s <oval_definitions.xml>\n", argv[0]);
 		return 1;
 	}
 
-        pctx = oval_pctx_new (model);
-        
-        if (pctx == NULL)
-                return (1);
-
-        oval_pctx_setattr (pctx, OVAL_PCTX_ATTR_DIR, getenv ("OVAL_PROBE_DIR"));
-        
-	int idx;
-	for (idx = 1; oval_object_iterator_has_more(objects); idx++) {
-		struct oval_object *object = oval_object_iterator_next(objects);
-		oval_object_to_print(object, "    ", idx);
-		printf("Callin probe on object\n");
-		struct oval_syschar* syschar = oval_probe_object_eval (pctx, object);
-		printf("System characteristics:\n");
-		if (syschar)
-			oval_syschar_to_print(syschar, "    ", idx);
-		else
-			printf("(NULL)\n");
+	source = oval_import_source_new_file(argv[1]);
+	definition_model = oval_definition_model_new();
+	ret = oval_definition_model_import(definition_model, source, &_test_error_handler, NULL);
+	if(ret != 1) {
+		printf("oval_definition_model_import() failed.\n");
+		return 2;
 	}
-        
+	oval_import_source_free(source);
+	syschar_model = oval_syschar_model_new(definition_model);
+
+	obj_it = oval_definition_model_get_objects(definition_model);
+	if (!oval_object_iterator_has_more(obj_it)) {
+		printf("No definitions found.\n");
+		return 3;
+	}
+
+	probe_dir = getenv ("OVAL_PROBE_DIR");
+	if (probe_dir == NULL) {
+		printf("'OVAL_PROBE_DIR' not found in environment.\n");
+		return 4;
+	}
+	printf("probe_dir: %s.\n", probe_dir);
+        pctx = oval_pctx_new (syschar_model);
+	if (pctx == NULL) {
+		printf("oval_pctx_new() failed.\n");
+		return 5;
+	}
+        oval_pctx_setattr (pctx, OVAL_PCTX_ATTR_DIR, probe_dir);
+
+	for (idx = 1; oval_object_iterator_has_more(obj_it); idx++) {
+		object = oval_object_iterator_next(obj_it);
+		id = oval_object_get_id(object);
+
+		printf("=== Object definition ===\n");
+		oval_object_to_print(object, "    ", idx);
+
+		syschar = oval_syschar_model_get_syschar(syschar_model, id);
+
+		if (syschar == NULL) {
+			printf("=== Calling probe on object ===\n");
+			syschar = oval_probe_object_eval(pctx, object);
+
+			if (syschar == NULL) {
+				syschar = oval_syschar_new(object);
+				oval_syschar_set_flag(syschar, SYSCHAR_FLAG_NOT_COLLECTED);
+			}
+
+			oval_syschar_model_add_syschar(syschar_model, syschar);
+		}
+
+		printf("=== System characteristics ===\n");
+		oval_syschar_to_print(syschar, "    ", idx);
+	}
+
         oval_pctx_free (pctx);
+	oval_object_iterator_free(obj_it);
+        oval_definition_model_free(definition_model);
+        oval_syschar_model_free(syschar_model);
+
 
 	return 0;
 }
