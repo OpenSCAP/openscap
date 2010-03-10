5dbb011e73d5076124c4001bd8ff51e1f10ab43e
Peter Vrabec <pvrabec@redhat.com>
2010-03-10 17:21:05 +0100

    add missing copyright and authorship

--- src/OVAL/oval_resultDefinition.c
+++ src/OVAL/oval_resultDefinition.c
@@ -9,7 +9,7 @@
  * Copyright 2008 Red Hat Inc., Durham, North Carolina.
  * All Rights Reserved.
  *
- * This library is oscap_free software; you can redistribute it and/or
+ * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
--- src/OVAL/probes/unix/shadow.c
+++ src/OVAL/probes/unix/shadow.c
@@ -1,25 +1,24 @@
 /*
-* shadow.c - Check metadata associated with the UNIX shadow password file
-* Copyright (c) 2010 Red Hat Inc., Durham, North Carolina.
-* All Rights Reserved. 
-*
-* This software may be freely redistributed and/or modified under the
-* terms of the GNU General Public License as published by the Free
-* Software Foundation; either version 2, or (at your option) any
-* later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with this program; see the file COPYING. If not, write to the
-* Free Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
-*
-* Authors:
-*   Steve Grubb <sgrubb@redhat.com>
-*/
+ * Copyright 2008 Red Hat Inc., Durham, North Carolina.
+ * All Rights Reserved.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Authors:
+ *   Steve Grubb <sgrubb@redhat.com>
+ */
 
 /*
  * shadow probe:
