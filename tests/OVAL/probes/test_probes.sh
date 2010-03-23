485d7a18dffa634d9331545ba3768c0d3fcce815
Ondrej Moris <omoris@redhat.com>
2010-03-24 00:10:29 +0100

    family probe test added

--- tests/OVAL/probes/test_probes.sh
+++ tests/OVAL/probes/test_probes.sh
@@ -104,6 +104,93 @@ function test_probes_api {
     return $ret_val
 }
 
+function test_probes_family {
+    local ret_val=0;
+    local LOGFILE="test_probes_family.out"
+    local EXECDIR="$(pwd)"
+    local DEFFILE="${srcdir}/OVAL/probes/test_probes_family.xml"
+    local RESFILE="test_probes_family.xml.results.xml"
+
+    eval "\"${EXECDIR}/test_probes\" \"$DEFFILE\" \"$RESFILE\"" >> "$LOGFILE"
+
+    if [ $? -eq 0 ] && [ -e $RESFILE ]; then
+
+	COUNT=7; ID=1
+	while [ $ID -le $COUNT ]; do
+	    
+	    DEF_DEF=`cat "$DEFFILE" | grep "id=\"oval:1:def:\${ID}\""`
+	    DEF_RES=`cat "$RESFILE" | grep "definition_id=\"oval:1:def:${ID}\""`
+
+	    if (echo $DEF_RES | grep -q "result=\"true\""); then
+		RES="TRUE"
+	    elif (echo $DEF_RES | grep -q "result=\"false\""); then
+		RES="FALSE"
+	    else
+		RES="ERROR"
+	    fi
+
+	    if (echo $DEF_DEF | grep -q "comment=\"true\""); then
+		CMT="TRUE"
+	    elif (echo $DEF_DEF | grep -q "comment=\"false\""); then
+		CMT="FALSE"
+	    else
+		CMT="ERROR"
+	    fi
+
+	    if [ ! $RES = $CMT ]; then
+		echo "Result of oval:1:def:${ID} should be ${CMT}!" >&2
+		ret_val=$[$ret_val + 1]
+	    fi
+	    
+	    ID=$[$ID+1]
+	done
+
+	COUNT=42; ID=1
+	while [ $ID -le $COUNT ]; do
+	    
+	    TEST_DEF=`cat "$DEFFILE" | grep "id=\"oval:1:tst:${ID}\""`
+	    TEST_RES=`cat "$RESFILE" | grep "test_id=\"oval:1:tst:${ID}\""`
+
+	    if (echo $TEST_RES | grep -q "result=\"true\""); then
+		RES="TRUE"
+	    elif (echo $TEST_RES | grep -q "result=\"false\""); then
+		RES="FALSE"
+	    else
+		RES="ERROR"
+	    fi
+
+	    if (echo $TEST_DEF | grep -q "comment=\"true\""); then
+		CMT="TRUE"
+	    elif (echo $TEST_DEF | grep -q "comment=\"false\""); then
+		CMT="FALSE"
+	    else
+		CMT="ERROR"
+	    fi
+
+	    if [ ! $RES = $CMT ]; then
+		echo "Result of oval:1:tst:${ID} should be ${CMT}!" >&2
+		ret_val=$[$ret_val + 1]
+	    fi
+	    
+	    ID=$[$ID+1]
+	done
+
+	if [ ! $ret_val -eq 0 ]; then
+	    echo "" >&2
+	    cat "$RESFILE" >&2
+	    echo "" >&2
+	    ret_val=2
+	fi
+
+    else 
+	ret_val=1
+    fi
+
+    [ -e $RESFILE ] && rm -f "$RESFILE"
+    
+    return $ret_val
+}
+
 function test_probes_file {
     local ret_val=0;
     local LOGFILE="test_probes_file.out"
@@ -592,6 +679,11 @@ result=$[$result+$ret_val]
 # ret_val=$?
 # report_result "test_probes_api" $ret_val  
 # result=$[$result+$ret_val]   
+
+test_probes_family
+ret_val=$?
+report_result "test_probes_family" $ret_val  
+result=$[$result+$ret_val]   
 
 test_probes_file
 ret_val=$?
