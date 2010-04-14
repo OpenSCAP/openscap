f425bb59eaa6cf22d7b78de814905db9bd8e252d
Daniel Kopecek <dkopecek@redhat.com>
2010-04-15 01:56:18 +0200

    [tests] new test: test_crapi_mdigest

--- tests/OVAL/probes/test_probes.sh
+++ tests/OVAL/probes/test_probes.sh
@@ -1011,6 +1011,40 @@ function test_crapi_digest {
         #echo "$file: ret $?, 5: $sum_md5, 1: $sum_sha1"
     done
 
+    rm -f "${TEMPDIR}/{a,b,c,d,e,f}"
+    rmdir "${TEMPDIR}"
+
+    return 0
+}
+
+function test_crapi_mdigest {
+    local ret_val=0;
+    local TEMPDIR="$(mktemp -d -t -q tmp.XXXXXX)"
+    local sum_md5="";
+    local sum_sha1="";
+
+    dd if=/dev/urandom of="${TEMPDIR}/a" count=1   bs=1k || return 2
+    dd if=/dev/urandom of="${TEMPDIR}/b" count=123 bs=1  || return 2
+    dd if=/dev/urandom of="${TEMPDIR}/c" count=1   bs=8k || return 2
+    dd if=/dev/urandom of="${TEMPDIR}/d" count=321 bs=1  || return 2
+    dd if=/dev/urandom of="${TEMPDIR}/e" count=1   bs=1M || return 2
+    dd if=/dev/urandom of="${TEMPDIR}/f" count=312 bs=1  || return 2
+    
+    for file in a b c d e f; do
+        sum_md5=$((md5sum "${TEMPDIR}/${file}" || openssl md5 "${TEMPDIR}/${file}") | sed -n 's|^.*\([0-9a-f]\{32\}\).*$|\1|p')
+        sum_sha1=$((sha1sum "${TEMPDIR}/${file}" || openssl sha1 "${TEMPDIR}/${file}") | sed -n 's|^.*\([0-9a-f]\{40\}\).*$|\1|p')
+
+        if [[ "$sum_md5" == "" || "$sum_sha1" == "" ]]; then
+            return 2
+        fi
+
+        ./test_crapi_mdigest "${TEMPDIR}/${file}" "$sum_md5" "$sum_sha1" || return 1
+        #echo "$file: ret $?, 5: $sum_md5, 1: $sum_sha1"
+    done
+
+    rm -f "${TEMPDIR}/{a,b,c,d,e,f}"
+    rmdir "${TEMPDIR}"
+
     return 0
 }
 
@@ -1133,6 +1167,11 @@ result=$[$result+$?]
 test_crapi_digest
 ret_val=$?
 report_result "test_crapi_digest" $ret_val  
+result=$[$result+$ret_val]   
+
+test_crapi_mdigest
+ret_val=$?
+report_result "test_crapi_mdigest" $ret_val  
 result=$[$result+$ret_val]   
 
 test_probes_cleanup
