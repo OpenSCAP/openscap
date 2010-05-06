050cbf4177d717a6617ea410cbb7b3bd0d2dabaf
Tomas Heinrich <theinric@redhat.com>
2010-05-06 18:37:27 +0200

    [probes] password: adjust probe for handling variables

--- src/OVAL/probes/unix/password.c
+++ src/OVAL/probes/unix/password.c
@@ -36,6 +36,7 @@
 #include "config.h"
 #include "seap.h"
 #include "probe-api.h"
+#include "probe-entcmp.h"
 #include "alloc.h"
 #include <stdlib.h>
 #include <string.h>
@@ -53,34 +54,6 @@ struct result_info {
 	const char *home_dir;
 	const char *login_shell;
 };
-
-oval_operation_t username_op;
-char *username = NULL;
-
-static int option_compare(oval_operation_t op, const char *s1, const char * s2)
-{
-	if (op == OVAL_OPERATION_EQUALS) {
-		if (strcmp(s1, s2) == 0)
-			return 1;
-		else
-			return 0;
-	} else if (op == OVAL_OPERATION_NOT_EQUAL) {
-		if (strcmp(s1, s2) == 0)
-			return 0;
-	}
-
-	// All matched
-	return 1;
-}
-
-static int eval_data(const char *cur_username)
-{
-	if (username) {
-		if (option_compare(username_op, cur_username, username) == 0)
-			return 0;
-	}
-	return 1;
-}
 
 static void report_finding(struct result_info *res, SEXP_t *probe_out)
 {
@@ -102,13 +75,16 @@ static void report_finding(struct result_info *res, SEXP_t *probe_out)
 	SEXP_free(item_sexp);
 }
 
-static int read_password(SEXP_t *probe_out)
+static int read_password(SEXP_t *un_ent, SEXP_t *probe_out)
 {
 	struct passwd *pw;
 
 	while ((pw = getpwent())) {
+		SEXP_t *un;
+
 		_D("Have user: %s\n", pw->pw_name);
-		if (eval_data(pw->pw_name)) {
+		un = SEXP_string_newf("%s", pw->pw_name);
+		if (probe_entobj_cmp(un_ent, un) == OVAL_RESULT_TRUE) {
 			struct result_info r;
 			r.username = pw->pw_name;
 			r.password = pw->pw_passwd;
@@ -120,6 +96,7 @@ static int read_password(SEXP_t *probe_out)
 
 			report_finding(&r, probe_out);
 		}
+		SEXP_free(un);
 	}
 	endpwent();
 	return 0;
@@ -127,51 +104,18 @@ static int read_password(SEXP_t *probe_out)
 
 SEXP_t *probe_main(SEXP_t *object, int *err, void *arg)
 {
-	SEXP_t *probe_out, *val = NULL, *ent;
+	SEXP_t *probe_out, *ent;
 
 	ent = probe_obj_getent(object, "username", 1);
-	if (ent)
-		val = probe_ent_getval(ent);
-	if (ent == NULL || val == NULL) {
+	if (ent == NULL) {
 		*err = PROBE_ENOVAL;
 		return NULL;
 	}
-	username = SEXP_string_cstr(val);
-	if (username == NULL) {
-		_D("username error\n");
-		switch (errno) {
-			case EINVAL:
-				*err = PROBE_EINVAL;
-				break;
-			case EFAULT:
-				*err = PROBE_ENOELM;
-				break;
-		}
-		return NULL;
-	}
-	SEXP_free(val);
-	val = probe_ent_getattrval(ent, "operation");
-	if (val == NULL)
-		username_op = OVAL_OPERATION_EQUALS;
-	else {
-		username_op = (oval_operation_t) SEXP_number_geti_32(val);
-		switch (username_op) {
-			case OVAL_OPERATION_EQUALS:
-			case OVAL_OPERATION_NOT_EQUAL:
-				break;
-			default:
-				*err = PROBE_EOPNOTSUPP;
-				SEXP_free(val);
-				oscap_free(username);
-				return (NULL);
-		}
-		SEXP_free(val);
-	}
-	SEXP_free(ent);
 	probe_out = SEXP_list_new(NULL);
 
 	// Now we check the file...
-	read_password(probe_out);
+	read_password(ent, probe_out);
+	SEXP_free(ent);
 
 	*err = 0;
 	return probe_out;
