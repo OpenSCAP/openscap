2bcb8d58d820744fb333bae41c0050964d4664e1
Daniel Kopecek <dkopecek@redhat.com>
2010-05-06 23:46:37 +0200

    [SEAP] Close debug log file at exit

--- src/OVAL/probes/SEAP/seap-debug.c
+++ src/OVAL/probes/SEAP/seap-debug.c
@@ -44,6 +44,11 @@ static FILE *__debuglog_fp = NULL;
 # define __UNLOCK_FP while(0)
 #endif
 
+static void __seap_debuglog_close(void)
+{
+        fclose(__debuglog_fp);
+}
+
 void __seap_debuglog (const char *file, const char *fn, size_t line, const char *fmt, ...)
 {
         va_list ap;
@@ -74,8 +79,9 @@ void __seap_debuglog (const char *file, const char *fn, size_t line, const char 
                 
                 ut = time (NULL);
                 st = ctime (&ut);
-                
+
                 fprintf (__debuglog_fp, "=============== LOG: %.24s ===============\n", st);
+                atexit(&__seap_debuglog_close);
         }
         
         if (flock (fileno (__debuglog_fp), LOCK_EX | LOCK_NB) == -1) {
