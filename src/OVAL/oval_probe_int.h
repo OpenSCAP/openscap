cdc836a640750a0f360f94d3e74172dd35751b11
Daniel Kopecek <dkopecek@redhat.com>
2010-05-21 08:45:44 +0200

    [OVAL] added missing header file

---
+++ src/OVAL/oval_probe_int.h
@@ -0,0 +1,4 @@
+#include "public/oval_definitions.h"
+
+int oval_probe_envvar_handler(oval_subtype_t type, void *ptr, int act, ...);
+int oval_probe_var_handler(oval_subtype_t type, void *ptr, int act, ...);
