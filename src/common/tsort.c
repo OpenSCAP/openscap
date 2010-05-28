9a470db531291219bafc66ae3e60755b9b19f9aa
Lukas Kuklinek <lkuklinek@redhat.com>
2010-05-28 16:09:19 +0200

    topological sort

---
+++ src/common/tsort.c
@@ -0,0 +1,131 @@
+/*
+ * Copyright 2010 Red Hat Inc., Durham, North Carolina.
+ * All Rights Reserved.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software 
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Authors:
+ *      Lukas Kuklinek <lkuklinek@redhat.com>
+ */
+
+
+// topological sort
+
+#include "tsort.h"
+
+struct oscap_tsort_context {
+	struct oscap_list *visited;
+	struct oscap_list *cur_stack;
+	struct oscap_list *result;
+	oscap_tsort_edge_func edge_func;
+	oscap_cmp_func cmp_func;
+	void *userdata;
+};
+
+static struct oscap_tsort_context *oscap_tsort_context_new(oscap_tsort_edge_func edge_func, oscap_cmp_func cmp_func, void *userdata)
+{
+	struct oscap_tsort_context *ctx = oscap_calloc(1, sizeof(struct oscap_tsort_context));
+	ctx->visited = oscap_list_new();
+	ctx->cur_stack = oscap_list_new();
+	ctx->result = oscap_list_new();
+	ctx->edge_func = edge_func;
+	ctx->cmp_func = cmp_func;
+	ctx->userdata = userdata;
+	return ctx;
+}
+
+static void oscap_tsort_context_free(struct oscap_tsort_context *ctx)
+{
+	if (ctx != NULL) {
+		oscap_list_free(ctx->visited, NULL);
+		oscap_list_free(ctx->cur_stack, NULL);
+		oscap_list_free(ctx->result, NULL);
+		oscap_free(ctx);
+	}
+}
+
+static bool oscap_tsort_visit(void *node, struct oscap_tsort_context* ctx);
+
+inline static bool oscap_tsort_visit_all(struct oscap_list *nodelist, struct oscap_tsort_context* ctx)
+{
+	bool ret = true;
+	struct oscap_iterator *it = oscap_iterator_new(nodelist);
+
+	while (oscap_iterator_has_more(it)) {
+		if (!oscap_tsort_visit(oscap_iterator_next(it), ctx)) {
+			ret = false;
+			break;
+		}
+	}
+
+	oscap_iterator_free(it);
+	return ret;
+}
+
+static bool oscap_tsort_visit(void *node, struct oscap_tsort_context* ctx)
+{
+	// loop detection
+	if (oscap_list_contains(ctx->cur_stack, node, ctx->cmp_func)) return false;
+
+	// skip already visited node
+	if (oscap_list_contains(ctx->visited, node, ctx->cmp_func)) return true;
+
+	// mark as visited & update stack
+	oscap_list_add(ctx->visited, node);
+	oscap_list_push(ctx->cur_stack, node);
+
+	// visit all next nodes (dependencies)
+	struct oscap_list *next = ctx->edge_func(node, ctx->userdata);
+	bool ret = oscap_tsort_visit_all(next, ctx);
+	oscap_list_free(next, NULL);
+
+	// update stack & add node to result
+	if (ret) oscap_list_pop(ctx->cur_stack, NULL);
+	oscap_list_add(ctx->result, node);
+
+	return ret;
+}
+
+bool oscap_tsort(struct oscap_list *input, struct oscap_list **output, oscap_tsort_edge_func edge_func, oscap_cmp_func cmp_func, void *userdata)
+{
+	assert(edge_func != NULL);
+	assert(input != NULL);
+
+	if (output != NULL) *output = NULL;
+	if (cmp_func == NULL) cmp_func = oscap_ptr_cmp;
+
+	struct oscap_tsort_context *ctx = oscap_tsort_context_new(edge_func, cmp_func, userdata);
+	bool ret = oscap_tsort_visit_all(input, ctx);
+
+	if (output != NULL) {
+		if (ret) {
+			// the graph has been topologically sorted, return result
+			*output = ctx->result;
+			ctx->result = NULL;
+		}
+		else {
+			// a loop has been ecountered in the graph, return the loop
+			*output = ctx->cur_stack;
+			ctx->cur_stack = NULL;
+		}
+	}
+
+	oscap_tsort_context_free(ctx);
+
+	return ret;
+}
+
+
+
---
+++ src/common/tsort.h
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2010 Red Hat Inc., Durham, North Carolina.
+ * All Rights Reserved.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software 
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Authors:
+ *      Lukas Kuklinek <lkuklinek@redhat.com>
+ */
+
+
+// topological sort
+
+#include "list.h"
+
+#ifndef OSCAP_TSORT_H_
+#define OSCAP_TSORT_H_
+
+OSCAP_HIDDEN_START;
+
+// returns nodest with an edge from given node
+typedef struct oscap_list* (*oscap_tsort_edge_func)(void *node, void *userdata);
+
+/*
+ * Topological sort.
+ *
+ * Performs a topological sort on a directed graph.
+ *
+ * @a edge_func is supposed to return an oscap_list of nodes edges coming from the current node
+ * (passed to it as an argument) are pointing to. It takes the node as a first argument
+ * and a pointer passed as @a userdata as a second argument.
+ *
+ * The purpose of @a cmp_func is to compare nodes. If it is NULL raw pointer comparison
+ * is used, which should be good enough in most cases.
+ *
+ * Pointer @a output will be set to a list with result. If you want to just check whether
+ * there is a topological order defined on the graph (i.e. it is an acyclic graph), pass NULL.
+ * If the function manages to find a topological order of the nodes (returns true),
+ * it is returned in this variable. Otherwise, it will contain an encountered loop.
+ * You are responsible to free this list.
+ *
+ * @param input set of nodes to sort
+ * @param output this pointer will be set to the result of the algorithm
+ * @param edge_func function to return target nodes of outgoing edges from the current one
+ * @param userdata arbitrary data pointer to be forwarded to the edge_func
+ * @returns whether the algorithm managed to topologically sort the graph
+ * @retval true input was sorted, result is in the *output pointer
+ * &retval false a loop was detected, *output points to the encountered loop
+ */
+bool oscap_tsort(struct oscap_list *input, struct oscap_list **output, oscap_tsort_edge_func edge_func, oscap_cmp_func cmp_func, void *userdata);
+
+OSCAP_HIDDEN_END;
+
+#endif // OSCAP_TSORT_H_
+
