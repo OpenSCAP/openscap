68b15fa63775a36a0e76f9cdf8426cae325c1088
Daniel Kopecek <dkopecek@redhat.com>
2010-06-15 15:28:46 +0200

    [tests] vgtest-probes: don't create result files

--- tests/OVAL/probes/vgtest-probes.sh
+++ tests/OVAL/probes/vgtest-probes.sh
@@ -3,10 +3,10 @@
 export OVAL_PROBE_DIR="`pwd`/../src/OVAL/probes/"
 echo ""
 echo "---------------- Valgrind checks -----------------"
-./vgrun.sh "./test_probes OVAL/probes/file-set2.xml file-set2.result.xml"
-./vgrun.sh "./test_probes OVAL/probes/test_probes_file.xml"
-./vgrun.sh "./test_probes OVAL/probes/test_probes_textfilecontent54.xml"
-./vgrun.sh "./test_probes OVAL/probes/test_probes_family.xml"
+./vgrun.sh "./test_probes OVAL/probes/file-set2.xml /dev/null"
+./vgrun.sh "./test_probes OVAL/probes/test_probes_file.xml /dev/null"
+./vgrun.sh "./test_probes OVAL/probes/test_probes_textfilecontent54.xml /dev/null"
+./vgrun.sh "./test_probes OVAL/probes/test_probes_family.xml /dev/null"
 echo "--------------------------------------------------"
 
 exit 0
