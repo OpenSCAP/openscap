cc66a8a441f7a135cb875b901761d62178bea1f4
Tomas Heinrich <theinric@redhat.com>
2010-07-25 21:25:55 +0200

    [OVAL] third part of supporting functionality for variable bindings

--- src/OVAL/oval_probe.c
+++ src/OVAL/oval_probe.c
@@ -136,42 +136,154 @@ oval_subtype_t oval_str2subtype(const char *str)
         return (d == NULL ? OVAL_SUBTYPE_UNKNOWN : d->type);
 }
 
-static struct oval_string_map *_obj_collect_var_refs(struct oval_object *obj)
-{
-	struct oval_string_map *vm;
-	struct oval_object_content_iterator *cont_itr;
+static void _obj_collect_var_refs(struct oval_object *obj, struct oval_string_map *vm);
+static void _var_collect_var_refs(struct oval_variable *var, struct oval_string_map *vm);
 
-	vm = oval_string_map_new();
+static void _comp_collect_var_refs(struct oval_component *comp, struct oval_string_map *vm)
+{
+	struct oval_object *obj;
+	struct oval_variable *var;
+	struct oval_component_iterator *cmp_itr;
+
+	switch (oval_component_get_type(comp)) {
+	case OVAL_COMPONENT_OBJECTREF:
+		obj = oval_component_get_object(comp);
+		_obj_collect_var_refs(obj, vm);
+		break;
+	case OVAL_COMPONENT_VARREF:
+		var = oval_component_get_variable(comp);
+		_var_collect_var_refs(var, vm);
+		break;
+	case OVAL_FUNCTION_ARITHMETIC:
+	case OVAL_FUNCTION_BEGIN:
+	case OVAL_FUNCTION_CONCAT:
+	case OVAL_FUNCTION_END:
+	case OVAL_FUNCTION_ESCAPE_REGEX:
+	case OVAL_FUNCTION_REGEX_CAPTURE:
+	case OVAL_FUNCTION_SPLIT:
+	case OVAL_FUNCTION_SUBSTRING:
+	case OVAL_FUNCTION_TIMEDIF:
+		cmp_itr = oval_component_get_function_components(comp);
+		while (oval_component_iterator_has_more(cmp_itr)) {
+			struct oval_component *cmp;
+
+			cmp = oval_component_iterator_next(cmp_itr);
+			_comp_collect_var_refs(cmp, vm);
+		}
+		oval_component_iterator_free(cmp_itr);
+		break;
+	default:
+		break;
+	}
+}
+
+static void _var_collect_var_refs(struct oval_variable *var, struct oval_string_map *vm)
+{
+	char *var_id;
+
+	var_id = oval_variable_get_id(var);
+	oval_string_map_put(vm, var_id, var);
+
+	if (oval_variable_get_type(var) == OVAL_VARIABLE_LOCAL) {
+		struct oval_component *comp;
+
+		comp = oval_variable_get_component(var);
+		_comp_collect_var_refs(comp, vm);
+	}
+}
+
+static void _ent_collect_var_refs(struct oval_entity *ent, struct oval_string_map *vm)
+{
+	if (oval_entity_get_varref_type(ent) == OVAL_ENTITY_VARREF_ATTRIBUTE) {
+		struct oval_variable *var;
+
+		var = oval_entity_get_variable(ent);
+		_var_collect_var_refs(var, vm);
+	}
+}
+
+static void _ste_collect_var_refs(struct oval_state *ste, struct oval_string_map *vm)
+{
+	struct oval_state_content_iterator *cont_itr;
+
+	cont_itr = oval_state_get_contents(ste);
+	while (oval_state_content_iterator_has_more(cont_itr)) {
+		struct oval_state_content *cont;
+		struct oval_entity *ent;
+
+		cont = oval_state_content_iterator_next(cont_itr);
+		ent = oval_state_content_get_entity(cont);
+		_ent_collect_var_refs(ent, vm);
+	}
+	oval_state_content_iterator_free(cont_itr);
+}
+
+static void _set_collect_var_refs(struct oval_setobject *set, struct oval_string_map *vm)
+{
+	struct oval_setobject_iterator *subset_itr;
+	struct oval_object_iterator *obj_itr;
+	struct oval_state_iterator *ste_itr;
+
+	switch (oval_setobject_get_type(set)) {
+	case OVAL_SET_AGGREGATE:
+		subset_itr = oval_setobject_get_subsets(set);
+		while (oval_setobject_iterator_has_more(subset_itr)) {
+			struct oval_setobject *subset;
+
+			subset = oval_setobject_iterator_next(subset_itr);
+			_set_collect_var_refs(subset, vm);
+		}
+		oval_setobject_iterator_free(subset_itr);
+		break;
+	case OVAL_SET_COLLECTIVE:
+		obj_itr = oval_setobject_get_objects(set);
+		while (oval_object_iterator_has_more(obj_itr)) {
+			struct oval_object *obj;
+
+			obj = oval_object_iterator_next(obj_itr);
+			_obj_collect_var_refs(obj, vm);
+		}
+		oval_object_iterator_free(obj_itr);
+		ste_itr = oval_setobject_get_filters(set);
+		while (oval_state_iterator_has_more(ste_itr)) {
+			struct oval_state *ste;
+
+			ste = oval_state_iterator_next(ste_itr);
+			_ste_collect_var_refs(ste, vm);
+		}
+		oval_state_iterator_free(ste_itr);
+		break;
+	default:
+		break;
+	}
+}
+
+static void _obj_collect_var_refs(struct oval_object *obj, struct oval_string_map *vm)
+{
+	struct oval_object_content_iterator *cont_itr;
 
 	cont_itr = oval_object_get_object_contents(obj);
 	while (oval_object_content_iterator_has_more(cont_itr)) {
 		struct oval_object_content *cont;
 		struct oval_entity *ent;
+		struct oval_setobject *set;
 
 		cont = oval_object_content_iterator_next(cont_itr);
 
 		switch (oval_object_content_get_type(cont)) {
 		case OVAL_OBJECTCONTENT_ENTITY:
 			ent = oval_object_content_get_entity(cont);
-			if (oval_entity_get_varref_type(ent) == OVAL_ENTITY_VARREF_ATTRIBUTE) {
-				struct oval_variable *var;
-				char *var_id;
-
-				var = oval_entity_get_variable(ent);
-				var_id = oval_variable_get_id(var);
-				oval_string_map_put(vm, var_id, var);
-			}
+			_ent_collect_var_refs(ent, vm);
 			break;
 		case OVAL_OBJECTCONTENT_SET:
-			// todo: recursion
+			set = oval_object_content_get_setobject(cont);
+			_set_collect_var_refs(set, vm);
 			break;
 		default:
 			break;
 		}
 	}
 	oval_object_content_iterator_free(cont_itr);
-
-	return vm;
 }
 
 static void _syschar_add_bindings(struct oval_syschar *sc, struct oval_string_map *vm)
@@ -201,8 +313,6 @@ static void _syschar_add_bindings(struct oval_syschar *sc, struct oval_string_ma
 		oval_syschar_add_variable_binding(sc, binding);
 	}
 	oval_collection_iterator_free(var_itr);
-
-	oval_string_map_free(vm, NULL);
 }
 
 struct oval_syschar *oval_probe_object_query(oval_probe_session_t *psess, struct oval_object *object, int flags)
@@ -220,7 +330,8 @@ struct oval_syschar *oval_probe_object_query(oval_probe_session_t *psess, struct
                 return(NULL);
         }
 
-	vm = _obj_collect_var_refs(object);
+	vm = oval_string_map_new();
+	_obj_collect_var_refs(object, vm);
         o_sys = NULL;
 
         if (ph->func(type, ph->uptr, PROBE_HANDLER_ACT_EVAL, object, &o_sys, flags) != 0) {
@@ -229,6 +340,7 @@ struct oval_syschar *oval_probe_object_query(oval_probe_session_t *psess, struct
         }
 
 	_syschar_add_bindings(o_sys, vm);
+	oval_string_map_free(vm, NULL);
 
         return(o_sys);
 }
