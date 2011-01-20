4ba89a0d69435d00abbd5afeea41ad978cb84c51
Tomas Heinrich <theinric@redhat.com>
2011-01-20 16:12:54 +0100

    [probes] interface: add support for 'type' entity

--- src/OVAL/probes/unix/interface.c
+++ src/OVAL/probes/unix/interface.c
@@ -33,19 +33,23 @@
  */
 
 /*
- * interface probe:
+ * interface probe
  *
- *   interface_object:
- *     string name
+ *   reference:
+ *     http://oval.mitre.org/language/version5.6/ovaldefinition/documentation/unix-definitions-schema.html#interface_object
+ *     http://oval.mitre.org/language/version5.6/ovalsc/documentation/unix-system-characteristics-schema.html#interface_item
  *
- *   interface_item:
- *     string name
- *     interfacetype type
- *     string hardware_addr
- *     string inet_addr
- *     string broadcast_addr
- *     string netmask
- *     [0..*] string flag
+ *   object:
+ *     name oval-def:EntityObjectStringType 1 1
+ *
+ *   item:
+ *     name           oval-sc:EntityItemStringType    0 1
+ *     type           unix-sc:EntityItemInterfaceType 0 1
+ *     hardware_addr  oval-sc:EntityItemStringType    0 1
+ *     inet_addr      oval-sc:EntityItemStringType    0 1
+ *     broadcast_addr oval-sc:EntityItemStringType    0 1
+ *     netmask        oval-sc:EntityItemStringType    0 1
+ *     flag           oval-sc:EntityItemStringType    0 unbounded
  */
 
 #include <config.h>
@@ -62,14 +66,18 @@
 #include <sys/ioctl.h>
 #include <string.h>
 #include <net/if.h>
+#include <net/if_arp.h>
 
 static int fd=-1;
 
-static char *get_mac(const struct ifaddrs *ifa)
+static void get_l2_info(const struct ifaddrs *ifa, char **mp, char **tp)
 {
 	struct ifreq ifr;
 	unsigned char mac[6];
 	static char mac_buf[20];
+
+	*mp = mac_buf;
+	*tp = "";
 
 	memset(&ifr, 0, sizeof(struct ifreq));
 	strcpy(ifr.ifr_name, ifa->ifa_name);
@@ -78,19 +86,40 @@ static char *get_mac(const struct ifaddrs *ifa)
 		snprintf(mac_buf, sizeof(mac_buf),
 			"%02X:%02X:%02X:%02X:%02X:%02X",
 			mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
+		switch (ifr.ifr_hwaddr.sa_family) {
+		case ARPHRD_ETHER:
+			*tp = "ARPHRD_ETHER";
+			break;
+		case ARPHRD_FDDI:
+			*tp = "ARPHRD_FDDI";
+			break;
+		case ARPHRD_LOOPBACK:
+			*tp = "ARPHRD_LOOPBACK";
+			break;
+		case ARPHRD_PPP:
+			*tp = "ARPHRD_PPP";
+			break;
+		case ARPHRD_PRONET:
+			*tp = "ARPHRD_PRONET";
+			break;
+		case ARPHRD_SLIP:
+			*tp = "ARPHRD_SLIP";
+			break;
+		case ARPHRD_VOID:
+			*tp = "ARPHRD_VOID";
+			break;
+		}
 	} else
 		mac_buf[0] = 0;
-
-	return mac_buf;
 }
 
 static int get_ifs(SEXP_t *name_ent, SEXP_t *probe_out)
 {
 	struct ifaddrs *ifaddr, *ifa;
 	int family, rc=1;
-	char host[NI_MAXHOST], broad[NI_MAXHOST], mask[NI_MAXHOST], *mac;
+	char host[NI_MAXHOST], broad[NI_MAXHOST], mask[NI_MAXHOST], *mac, *type;
 	SEXP_t *item;
-	SEXP_t *r0, *r1, *r2, *r3, *r4;
+	SEXP_t *r0, *r1, *r2, *r3, *r4, *r5;
 
 	if (getifaddrs(&ifaddr) == -1) {
 		SEXP_t *msg;
@@ -134,7 +163,7 @@ static int get_ifs(SEXP_t *name_ent, SEXP_t *probe_out)
 		}
 		SEXP_free(sname);
 
-		mac = get_mac(ifa);
+		get_l2_info(ifa, &mac, &type);
 		rc = getnameinfo(ifa->ifa_addr, (family == AF_INET) ?
 			sizeof(struct sockaddr_in) :
 			sizeof(struct sockaddr_in6), host, NI_MAXHOST,
@@ -187,6 +216,8 @@ static int get_ifs(SEXP_t *name_ent, SEXP_t *probe_out)
 		item = probe_item_creat("interface_item", NULL,
 				"name", NULL,
                                 r0 = SEXP_string_new (ifa->ifa_name, strlen (ifa->ifa_name)),
+				"type", NULL,
+				r5 = SEXP_string_new(type, strlen(type)),
 				"hardware_addr", NULL,
                                 r1 = SEXP_string_new (mac, strlen (mac)),
 				"inet_addr", NULL,
@@ -198,7 +229,7 @@ static int get_ifs(SEXP_t *name_ent, SEXP_t *probe_out)
 				NULL);
 
 		probe_cobj_add_item(probe_out, item);
-		SEXP_vfree(r0, r1, r2, r3, r4, item, NULL);
+		SEXP_vfree(r0, r1, r2, r3, r4, r5, item, NULL);
 	}
 
 	rc = 0;
