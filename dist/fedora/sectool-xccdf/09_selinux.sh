61bb29abacfbc8cd543da1e6e58925126aebf453
Peter Vrabec <pvrabec@redhat.com>
2012-02-15 09:30:11 +0100

    provide sample profiles in "sectool" content

--- dist/fedora/sectool-xccdf/09_selinux.sh
+++ dist/fedora/sectool-xccdf/09_selinux.sh
@@ -1,14 +1,24 @@
 #!/usr/bin/env bash
 
-SELINUX_MODE=`/usr/sbin/getenforce`
+SELINUX_STATE=`/usr/sbin/getenforce | tr '[A-Z]' '[a-z]'`
 
-if [[ $SELINUX_MODE != "Enforcing" ]]
+if [[ -z "$XCCDF_VALUE_EXPECTED_STATE" ]]; then
+    XCCDF_VALUE_EXPECTED_STATE="enforcing"
+    echo "WARNING: Using default expected state!"
+else
+    XCCDF_VALUE_EXPECTED_STATE="$(echo $XCCDF_VALUE_EXPECTED_STATE | tr '[A-Z]' '[a-z]')"
+fi
+
+if [[ $SELINUX_STATE != $XCCDF_VALUE_EXPECTED_STATE ]]
 then
-	echo "Selinux is in "$SELINUX_MODE" mode."
-	echo "Using Enforing mode is highly recommended. See selinux manual page for switching to Enforcing mode."
+	echo "Selinux is in "$SELINUX_MODE" state."
+	echo "Expected state: "$XCCDF_VALUE_EXPECTED_STATE
+
+	if [[ $XCCDF_VALUE_EXPECTED_STATE == "enforcing" ]]; then
+	    echo "Using Enforing state is highly recommended. See selinux manual page for switching to Enforcing state."
+	fi
 
 	exit $XCCDF_RESULT_FAIL
 fi
 
 exit $XCCDF_RESULT_PASS
-
