8970772a00b61beedf7edfd5c0cc4a2932509ed3
Martin Preisler <mpreisle@redhat.com>
2012-04-10 17:21:11 +0200

    Test import/export of XCCDF 1.1 and 1.2 separately

--- tests/API/XCCDF/test_api_xccdf.sh
+++ tests/API/XCCDF/test_api_xccdf.sh
@@ -15,17 +15,17 @@
 
 # Test cases.
 
-function test_api_xccdf_export {
+function test_api_xccdf11_export {
     require "egrep" || return 255
     local ret_val=0
 
-    ./test_api_xccdf --export $srcdir/scap-rhel5-xccdf.xml \
-	scap-rhel5-xccdf.xml.out
+    ./test_api_xccdf --export $srcdir/scap-rhel5-xccdf11.xml \
+	scap-rhel5-xccdf11.xml.out
     
     if [ $? -eq 0 ]; then
 	local IGNORE_ATTRS="weight|schemaLocation|lang"
-	$srcdir/../../xmldiff.pl $srcdir/scap-rhel5-xccdf.xml \
-	    scap-rhel5-xccdf.xml.out | \
+	$srcdir/../../xmldiff.pl $srcdir/scap-rhel5-xccdf11.xml \
+	    scap-rhel5-xccdf11.xml.out | \
 	    egrep -v "[aA]ttribute '($IGNORE_ATTRS)'"
 	if [ $? -neq 0 ]; then
 	    echo "Exported file differs from what is expected!"
@@ -34,6 +34,30 @@ function test_api_xccdf_export {
     else
 	echo "Cannot export xccdf content!"
 	ret_val=1
+    fi
+
+    return $ret_val
+}
+
+function test_api_xccdf12_export {
+    require "egrep" || return 255
+    local ret_val=0
+
+    ./test_api_xccdf --export $srcdir/scap-rhel5-xccdf12.xml \
+        scap-rhel5-xccdf12.xml.out
+
+    if [ $? -eq 0 ]; then
+        local IGNORE_ATTRS="weight|schemaLocation|lang"
+        $srcdir/../../xmldiff.pl $srcdir/scap-rhel5-xccdf12.xml \
+            scap-rhel5-xccdf12.xml.out | \
+            egrep -v "[aA]ttribute '($IGNORE_ATTRS)'"
+        if [ $? -neq 0 ]; then
+            echo "Exported file differs from what is expected!"
+            ret_val=1
+        fi
+    else
+        echo "Cannot export xccdf content!"
+        ret_val=1
     fi
 
     return $ret_val
@@ -49,6 +73,7 @@ function text_api_xccdf_import {
 test_init "test_api_xccdf.log"
 
 #test_run "text_api_xccdf_import" text_api_xccdf_import
-test_run "text_api_xccdf_export" test_api_xccdf_export
+test_run "text_api_xccdf11_export" test_api_xccdf11_export
+test_run "text_api_xccdf12_export" test_api_xccdf12_export
 
 test_exit
