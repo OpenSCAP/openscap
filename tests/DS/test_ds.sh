895d277637d1489bb4ea6e08e61258ab133f85ef
Martin Preisler <mpreisle@redhat.com>
2012-07-17 16:09:07 +0200

    First version of the 'oscap ds rds_create' test, exports and validates
    Right now the validation will fail due to issues in the RDS code. This
    will be fixed soon.

--- tests/DS/test_ds.sh
+++ tests/DS/test_ds.sh
@@ -47,11 +47,38 @@ function test_sds {
     return "$ret_val"
 }
 
+function test_rds
+{
+    local ret_val=0;
+
+    local SDS_FILE="${srcdir}/$1"
+    local XCCDF_RESULT_FILE="${srcdir}/$2"
+    local OVAL_RESULT_FILE="${srcdir}/$3"
+    local DS_TARGET_DIR="`mktemp -d`"
+    local DS_FILE="$DS_TARGET_DIR/rds.xml"
+
+    local OSCAP_DIR=`cd ../../utils/.libs; pwd`
+
+    $OSCAP_DIR/oscap ds rds_create "$SDS_FILE" "$DS_FILE" "$XCCDF_RESULT_FILE" "$OVAL_RESULT_FILE"
+
+    if [ $? -ne 0 ]; then
+        ret_val=1
+    fi
+
+    #pushd "$DS_TARGET_DIR"
+    #$OSCAP_DIR/oscap ds sds_split "`basename $DS_FILE`" "$DS_TARGET_DIR"
+    #rm sds.xml
+    #popd
+
+    return "$ret_val"
+}
+
 # Testing.
 test_init "test_ds.log"
 
 test_run "sds_simple_xccdf" test_sds sds_simple/scap-fedora14-xccdf.xml
 test_run "sds_multiple_oval" test_sds sds_multiple_oval/multiple-oval-xccdf.xml
+test_run "rds_simple" test_rds rds_simple/sds.xml rds_simple/results-xccdf.xml rds_simple/results-oval.xml
 
 test_exit
 
