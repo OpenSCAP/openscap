81ef39fc871531490dec97f6c93457ec1419d8fe
Tomas Heinrich <theinric@redhat.com>
2012-08-10 15:30:52 +0200

    tests: test support for older schemas

--- tests/probes/textfilecontent54/test_probes_textfilecontent54.sh
+++ tests/probes/textfilecontent54/test_probes_textfilecontent54.sh
@@ -48,10 +48,56 @@ function test_probes_textfilecontent54 {
     return $ret_val
 }
 
+LST_VALID="
+	tfc54-def-5.4-valid.xml
+	tfc54-def-5.5-valid.xml
+	tfc54-def-5.6-valid.xml
+	tfc54-def-5.7-valid.xml
+	tfc54-def-5.8-valid.xml
+	tfc54-def-5.9-valid.xml
+	tfc54-def-5.10-valid.xml
+	tfc54-def-5.10.1-valid.xml
+"
+
+LST_INVALID="
+	tfc54-def-5.4-invalid.xml
+"
+
+function tfc54_validation {
+	local ret=0
+
+	probecheck "textfilecontent54" || return 255
+
+	echo "*** Validating and evaluating correct OVAL content:"
+	for i in $LST_VALID; do
+		echo "Validating definitions '$i'."
+		../../../utils/.libs/oscap oval validate-xml "$i" \
+			|| { echo "Validation failed."; ret=1; continue; }
+		echo "Evaluating definitions '$i'."
+		r="$(basename $i .xml)-results.xml"
+		../../../utils/.libs/oscap oval eval --results "$r" "$i" \
+			|| { echo "Evaluation failed."; ret=1; continue; }
+		echo "Validating results '$r'."
+		../../../utils/.libs/oscap oval validate-xml --results "$r" \
+			|| { echo "Validation failed."; ret=1; }
+	done
+
+	echo "*** Validating incorrect OVAL content:"
+	for i in $LST_INVALID; do
+		echo "Validating content '$i'."
+		../../../utils/.libs/oscap oval validate-xml "$i" \
+			&& { echo "Validation incorrectly succeeded."; ret=1; }
+	done
+
+	return $ret
+}
+
 # Testing.
 
 test_init "test_probes_textfilecontent54.log"
 
 test_run "test_probes_textfilencontent54" test_probes_textfilecontent54
+export OSCAP_FULL_VALIDATION=1
+test_run "textfilecontent54 validation" tfc54_validation
 
 test_exit
