1a5d3891c69be6e52c7dc27645143ac1794bcb11
Martin Preisler <mpreisle@redhat.com>
2012-08-20 13:15:26 +0200

    Added python binding import test and a stubbed out perl import test

---
+++ tests/bindings/test_perl.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# Author:
+#   Martin Preisler <mpreisle@redhat.com>
+
+. ${srcdir}/../test_common.sh
+
+# Test Cases.
+
+function test_perl_import {
+
+    #echo "require openscap" | perl
+    #return $?
+    # FIXME: We currently skip the test, this should be uncommented when
+    #        perl bindings are working again
+    return 255
+}
+
+# Testing.
+test_init "test_perl.log"
+
+test_run "perl_import" test_perl_import
+
+test_exit
