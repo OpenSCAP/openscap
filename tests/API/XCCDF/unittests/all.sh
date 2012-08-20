7c84c7a7cca9c6b34b891c6fa0a7cd39c209d3eb
Simon Lukasik <slukasik@redhat.com>
2012-08-20 13:48:33 +0200

    tests: Test for trac#162 issue.
    Signed-off-by: Martin Preisler <mpreisle@redhat.com>

--- tests/API/XCCDF/unittests/all.sh
+++ tests/API/XCCDF/unittests/all.sh
@@ -5,4 +5,5 @@
 test_init test_api_xccdf_unittests.log
 test_run "xccdf:complex-check -- NAND is working properly" ./test_xccdf_shall_pass $srcdir/test_xccdf_complex_check_nand.xccdf.xml
 test_run "xccdf:complex-check -- single negation" ./test_xccdf_shall_pass $srcdir/test_xccdf_complex_check_single_negate.xccdf.xml
+test_run "Certain id's of xccdf_items may overlap" ./test_xccdf_shall_pass $srcdir/test_xccdf_overlaping_IDs.xccdf.xml
 test_exit
