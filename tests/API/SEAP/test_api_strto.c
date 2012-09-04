6a08103bd0b1241b526ff4bfda7dd942d62db7b8
Daniel Kopecek <dkopecek@redhat.com>
2012-09-04 10:57:45 +0200

    [tests] SEAP/generic: added strto_uint16_hex test

--- tests/API/SEAP/test_api_strto.c
+++ tests/API/SEAP/test_api_strto.c
@@ -11,9 +11,6 @@
 
 int main(void)
 {
-  uint8_t num_out, num_exp;
-  char *input;
-  size_t input_len;
 
 #define TEST_strto_uint8_hex(input, expected_result) do { \
     if (strto_uint8_hex(input, strlen(input), NULL) != (expected_result)) {\
@@ -34,6 +31,39 @@ int main(void)
   TEST_strto_uint8_hex("0a", 10);
   TEST_strto_uint8_hex("00", 0);
   TEST_strto_uint8_hex("01", 1);
+
+#define TEST_strto_uint16_hex(input, expected_result) do { \
+    if (strto_uint16_hex(input, strlen(input), NULL) != (expected_result)) {\
+      fprintf(stderr, "strto_uint16_hex(%s) != %"PRIu16"\n", input, expected_result);\
+	return 1;\
+    }\
+  } while (0)
+
+  TEST_strto_uint16_hex("0", 0);
+  TEST_strto_uint16_hex("1", 1);
+  TEST_strto_uint16_hex("ff", 255);
+  TEST_strto_uint16_hex("FF", 255);
+  TEST_strto_uint16_hex("Ff", 255);
+  TEST_strto_uint16_hex("fF", 255);
+  TEST_strto_uint16_hex("10", 16);
+  TEST_strto_uint16_hex("11", 17);
+  TEST_strto_uint16_hex("a", 10);
+  TEST_strto_uint16_hex("0a", 10);
+  TEST_strto_uint16_hex("00", 0);
+  TEST_strto_uint16_hex("01", 1);
+
+  TEST_strto_uint16_hex("fFfF", 0xffff);
+  TEST_strto_uint16_hex("FFFF", 0xffff);
+  TEST_strto_uint16_hex("ffff", 0xffff);
+  TEST_strto_uint16_hex("aaaa", 0xaaaa);
+  TEST_strto_uint16_hex("0101", 0x0101);
+  TEST_strto_uint16_hex("1010", 0x1010);
+  TEST_strto_uint16_hex("1111", 0x1111);
+  TEST_strto_uint16_hex("0000", 0x0000);
+  TEST_strto_uint16_hex("000", 0x000);
+  TEST_strto_uint16_hex("8000", 0x8000);
+  TEST_strto_uint16_hex("0", 0x0);
+  TEST_strto_uint16_hex("1234", 0x1234);
 
   return 0;
 }
