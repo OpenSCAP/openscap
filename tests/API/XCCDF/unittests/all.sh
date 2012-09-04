ad099bb686f86ef12baa2e0953f6e8f61130d896
Simon Lukasik <slukasik@redhat.com>
2012-09-04 11:03:15 +0200

    tests: Make sure this file is not executable
    Tests with oval are designed to depend on this externality.

--- tests/API/XCCDF/unittests/all.sh
+++ tests/API/XCCDF/unittests/all.sh
@@ -8,6 +8,7 @@ test_run "xccdf:complex-check -- NAND is working properly" ./test_xccdf_shall_pa
 test_run "xccdf:complex-check -- single negation" ./test_xccdf_shall_pass $srcdir/test_xccdf_complex_check_single_negate.xccdf.xml
 test_run "Certain id's of xccdf_items may overlap" ./test_xccdf_shall_pass $srcdir/test_xccdf_overlaping_IDs.xccdf.xml
 
+test_run "Assert for environment" [ ! -x $srcdir/not_executable ]
 test_run "Escaping of xml &amp within xccdf:value" $srcdir/test_xccdf_xml_escaping_value.sh
 test_run "xccdf:check-content-ref without @name" $srcdir/test_xccdf_check_content_ref_without_name_attr.sh
 test_exit
