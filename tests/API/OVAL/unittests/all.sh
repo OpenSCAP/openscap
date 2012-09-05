05d07b6fce8d7f8ec877bb50d2c7d117db1e23eb
Petr Lautrbach <plautrba@redhat.com>
2012-09-05 15:00:33 +0200

    [OVAL] - test for count function (#9)

--- tests/API/OVAL/unittests/all.sh
+++ tests/API/OVAL/unittests/all.sh
@@ -7,5 +7,6 @@ test_init test_api_oval_unittests.log
 test_run "empty filename(pattern match)" $srcdir/test_empty_filename.sh
 test_run "deprecated definition" $srcdir/test_deprecated_def.sh
 #test_run "test applicability_check element" $srcdir/test_applicability_check.sh
+test_run "count function"  $srcdir/test_count_function.sh
 test_exit
 
