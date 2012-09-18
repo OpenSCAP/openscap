e7061909dd1407e9a601ba8b2b6062a70e552956
Simon Lukasik <slukasik@redhat.com>
2012-09-18 11:41:05 +0200

    tests: trac#206: Assert that complex-check/@system is not exported.

--- tests/API/XCCDF/unittests/test_xccdf_check_processing_complex_priority.sh
+++ tests/API/XCCDF/unittests/test_xccdf_check_processing_complex_priority.sh
@@ -28,5 +28,7 @@ for args in "" "--profile xccdf_moc.elpmaxe.www_profile_1"; do
 	assert_exists_once '//rule-result/complex-check/check[@system="http://oval.mitre.org/XMLSchema/oval-definitions-5"]/check-content-ref'
 	assert_exists_once '//rule-result/complex-check/check[@system="http://oval.mitre.org/XMLSchema/oval-definitions-5"]/check-content-ref[@name="oval:moc.elpmaxe.www:def:1"]'
 	assert_exists_once '//rule-result/complex-check/check[@system="http://oval.mitre.org/XMLSchema/oval-definitions-5"]/check-content-ref[@href="test_xccdf_check_content_ref_without_name_attr.oval.xml"]'
+	assert_exists_once '//Rule/complex-check[not(@system)]'
+	assert_exists_once '//rule-result/complex-check[not(@system)]'
 	rm -rf $result
 done
