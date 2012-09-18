a76e734a64c0b17327feab578d7300061457ff39
Simon Lukasik <slukasik@redhat.com>
2012-09-18 12:52:37 +0200

    tests: trac#186: assert that none check is used when no profile is selected.

--- tests/API/XCCDF/unittests/test_xccdf_refine_rule.sh
+++ tests/API/XCCDF/unittests/test_xccdf_refine_rule.sh
@@ -16,6 +16,11 @@ echo "Result file = $result"
 $OSCAP xccdf validate-xml $result
 
 [ "$(xpath $result 'count(//refine-rule[@weight])')" == "0" ]
-[ "$(xpath $result 'count(//refine-rule[not(@weight)])')" == "1" ]
+assert_exists_once() { [ "$(xpath $result 'count('$1')')" == "1" ]; }
+assert_exists_once '//refine-rule[not(@weight)]'
+assert_exists_once '//rule-result'
+assert_exists_once '//rule-result/result'
+assert_exists_once '//rule-result/result[text()="notchecked"]'
+assert_exists_once '//rule-result[not(check)]'
 
 rm -rf $result
