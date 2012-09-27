905ee0dd103ed2638a4f7cdeea9ed9031e4c0403
Simon Lukasik <slukasik@redhat.com>
2012-09-27 15:59:19 +0200

    tests: Assert that stdout is empty.

--- tests/API/XCCDF/unittests/test_xccdf_check_multi_check.sh
+++ tests/API/XCCDF/unittests/test_xccdf_check_multi_check.sh
@@ -6,7 +6,7 @@ set -o pipefail
 result=`mktemp`
 stderr=`mktemp`
 
-$OSCAP xccdf eval --results $result $srcdir/test_xccdf_check_multi_check.xccdf.xml 2> $stderr
+$OSCAP xccdf eval --results $result $srcdir/test_xccdf_check_multi_check.xccdf.xml 2>&1 > $stderr
 
 echo "Stderr file = $stderr"
 echo "Result file = $result"
