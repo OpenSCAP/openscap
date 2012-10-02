OpenSCAP/openscap

commit ff96d86ccc3580c0055cf41402ab9d3979c9de78

Do export UNKNOWN result, when @multi-check creates zero rule-results
Another hic-sunt-leones corner-case, this happens when referenced
OVAL file contains no <definition/> elements.

We believe that such case happens only by accident and if
the scanner was creating zero <rule-results/> it would make
the problem only worse and hide the cause.

We like to put the lights on.

src/XCCDF_POLICY/xccdf_policy.c
@@ -869,7 +869,7 @@ _xccdf_policy_rule_evaluate(struct xccdf_policy * policy, const struct xccdf_rul
 				if (!oscap_string_iterator_has_more(name_it)) {
 					// Super special case when oval file contains no definitions
 					// thus multi-check shall yield zero rule-results.
-					report = _xccdf_policy_report_rule(policy, "urn:xccdf:system:callback:output", rule, 0);
+					report = _xccdf_policy_report_rule_result(policy, result, rule, check, XCCDF_RESULT_UNKNOWN, "No definitions found for @multi-check.");
 				}
 				while (oscap_string_iterator_has_more(name_it)) {
 					const char *name = oscap_string_iterator_next(name_it);

commit d4bc3b0c7b16cf779cd183d199872ead46153784

Do not xccdf_check_free, when you assign it to the result.
Caused by:
    ff96d86ccc3580c0055cf41402ab9d3979c9de78
Addressing:
Program received signal SIGSEGV, Segmentation fault.
__strlen_sse2 () at ../sysdeps/x86_64/strlen.S:43
43pcmpeqb(%rax), %xmm0

src/XCCDF_POLICY/xccdf_policy.c
@@ -870,6 +870,11 @@ _xccdf_policy_rule_evaluate(struct xccdf_policy * policy, const struct xccdf_rul
 					// Super special case when oval file contains no definitions
 					// thus multi-check shall yield zero rule-results.
 					report = _xccdf_policy_report_rule_result(policy, result, rule, check, XCCDF_RESULT_UNKNOWN, "No definitions found for @multi-check.");
+					oscap_string_iterator_free(name_it);
+					oscap_stringlist_free(names);
+					xccdf_check_content_ref_iterator_free(content_it);
+					oscap_list_free(bindings, (oscap_destruct_func) xccdf_value_binding_free);
+					return report;
 				}
 				while (oscap_string_iterator_has_more(name_it)) {
 					const char *name = oscap_string_iterator_next(name_it);