73fa3e03b81ac62f68f92cf3e9aa40ad63fc63eb
Martin Preisler <mpreisle@redhat.com>
2012-10-09 16:10:13 +0200

    Changed cpe-oval.xml used for tests to contain always failing OVAL def
    OVAL changes by Tomas Heinrich.

--- tests/API/XCCDF/applicability/all.sh
+++ tests/API/XCCDF/applicability/all.sh
@@ -57,6 +57,7 @@ test_run "test_api_xccdf_applicability_cpe_applicable_rule" test_api_xccdf_cpe_e
 test_run "test_api_xccdf_applicability_cpe_applicable_embedded_rule" test_api_xccdf_embedded_cpe_eval applicable-rule-embedded-xccdf.xml 0
 test_run "test_api_xccdf_applicability_cpe_nonexistant_platforms_rule" test_api_xccdf_cpe_eval nonexistant-platforms-rule-xccdf.xml cpe-dict.xml 1
 test_run "test_api_xccdf_applicability_cpe2_applicable_embedded_rule" test_api_xccdf_embedded_cpe_eval cpe2-applicable-rule-embedded-xccdf.xml 0
+test_run "test_api_xccdf_applicability_cpe2_negated_applicable_embedded_rule" test_api_xccdf_embedded_cpe_eval cpe2-negated-applicable-rule-embedded-xccdf.xml 1
 test_run "test_api_xccdf_applicability_cpe2_and_embedded_rule" test_api_xccdf_embedded_cpe_eval cpe2-and-rule-embedded-xccdf.xml 0
 
 test_exit
