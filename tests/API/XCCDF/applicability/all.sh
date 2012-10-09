b6e8cd26c0231a20a78c39b9411c120ad81acf9d
Martin Preisler <mpreisle@redhat.com>
2012-10-09 15:31:19 +0200

    Added the first few CPE2 XCCDF applicability tests

--- tests/API/XCCDF/applicability/all.sh
+++ tests/API/XCCDF/applicability/all.sh
@@ -56,5 +56,7 @@ test_init "test_api_xccdf_applicability.log"
 test_run "test_api_xccdf_applicability_cpe_applicable_rule" test_api_xccdf_cpe_eval applicable-rule-xccdf.xml cpe-dict.xml 0
 test_run "test_api_xccdf_applicability_cpe_applicable_embedded_rule" test_api_xccdf_embedded_cpe_eval applicable-rule-embedded-xccdf.xml 0
 test_run "test_api_xccdf_applicability_cpe_nonexistant_platforms_rule" test_api_xccdf_cpe_eval nonexistant-platforms-rule-xccdf.xml cpe-dict.xml 1
+test_run "test_api_xccdf_applicability_cpe2_applicable_embedded_rule" test_api_xccdf_embedded_cpe_eval cpe2-applicable-rule-embedded-xccdf.xml 0
+test_run "test_api_xccdf_applicability_cpe2_and_embedded_rule" test_api_xccdf_embedded_cpe_eval cpe2-and-rule-embedded-xccdf.xml 0
 
 test_exit
