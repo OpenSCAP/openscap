OpenSCAP/openscap

commit 5baf373909b919d15e91fedb200867aaccd8675a

get rid of oscap_report in oval_agent_eval()

src/OVAL/oval_agent.c
@@ -187,6 +187,26 @@ int oval_agent_get_definition_result(oval_agent_session_t *ag_sess, const char *
 	return 0;
 }
 
+struct oval_result_definition * oval_agent_get_result_definition(oval_agent_session_t *ag_sess, const char *id) {
+	struct oval_results_model *rmodel;
+	struct oval_result_system_iterator *rsystem_it;
+	struct oval_result_system *rsystem;
+	struct oval_result_definition *rdef;
+
+	rmodel = oval_agent_get_results_model(ag_sess);
+	rsystem_it = oval_results_model_get_systems(rmodel);
+	if (!oval_result_system_iterator_has_more(rsystem_it)) {
+		oval_result_system_iterator_free(rsystem_it);
+                return NULL;
+	}
+
+	rsystem = oval_result_system_iterator_next(rsystem_it);
+	oval_result_system_iterator_free(rsystem_it);
+	rdef = oval_result_system_get_definition(rsystem, id);
+
+	return rdef;
+}
+
 int oval_agent_reset_session(oval_agent_session_t * ag_sess) {
 	ag_sess->cur_var_model = NULL;
 	oval_definition_model_clear_external_variables(ag_sess->def_model);
@@ -220,7 +240,7 @@ int oval_agent_abort_session(oval_agent_session_t *ag_sess)
 	return oval_probe_session_abort(ag_sess->psess);
 }
 
-int oval_agent_eval_system(oval_agent_session_t * ag_sess, oscap_reporter cb, void *arg) {
+int oval_agent_eval_system(oval_agent_session_t * ag_sess, agent_reporter cb, void *arg) {
 	struct oval_definition *oval_def;
 	struct oval_definition_iterator *oval_def_it;
 	char   *id;
@@ -240,21 +260,9 @@ int oval_agent_eval_system(oval_agent_session_t * ag_sess, oscap_reporter cb, vo
 
 		/* callback */
                 if (cb != NULL) {
-			struct oscap_reporter_message * msg = oscap_reporter_message_new_fmt(
-				OSCAP_REPORTER_FAMILY_OVAL, /* FAMILY */
-				0,                           /* CODE */
-				"%s",
-				oval_definition_get_description(oval_def));
-			oscap_reporter_message_set_user1str(msg, id);
-			ret = oval_agent_get_definition_result(ag_sess, id, &result);
-			if (ret == -1) {
-				oscap_reporter_message_free(msg);
-				goto cleanup;
-			}
-
-			oscap_reporter_message_set_user2num(msg, result);
-			oscap_reporter_message_set_user3str(msg, oval_definition_get_title(oval_def));
-			ret = oscap_reporter_report(cb, msg, arg);
+			struct oval_result_definition * res_def = oval_agent_get_result_definition(ag_sess, id);
+			ret = cb(res_def,arg);
+			/* stop? */
 			if (ret!=0)
 				goto cleanup;
 		}

src/OVAL/public/oval_agent_api.h
@@ -86,6 +86,12 @@ int oval_agent_eval_definition(oval_agent_session_t *, const char *);
 int oval_agent_get_definition_result(oval_agent_session_t *, const char *, oval_result_t *);
 
 /**
+ * Get the OVAL result definition from an agent session
+ * @return NULL if not found
+ */
+struct oval_result_definition * oval_agent_get_result_definition(oval_agent_session_t *ag_sess, const char *id);
+
+/**
  * Clean resuls that were generated in this agent session
  */
 int oval_agent_reset_session(oval_agent_session_t * ag_sess);
@@ -95,11 +101,13 @@ int oval_agent_reset_session(oval_agent_session_t * ag_sess);
  */
 int oval_agent_abort_session(oval_agent_session_t *ag_sess);
 
+typedef int (*agent_reporter)(const struct oval_result_definition * res_def, void *arg);
+
 /**
  * Probe and evaluate all definitions from the content, call the callback functions upon single evaluation
  * @return 0 on success; -1 error; 1 warning
  */
-int oval_agent_eval_system(oval_agent_session_t * ag_sess, oscap_reporter cb, void *arg);
+int oval_agent_eval_system(oval_agent_session_t * ag_sess, agent_reporter cb, void *arg);
 
 /**
  * Get a result model from agent session

utils/oscap-oval.c
@@ -189,14 +189,18 @@ static int oval_gen_report(const char *infile, const char *outfile)
     return app_xslt(infile, "oval-results-report.xsl", outfile, NULL);
 }
 
-static int app_oval_callback(const struct oscap_reporter_message *msg, void *arg)
+static int app_oval_callback(const struct oval_result_definition * res_def, void *arg)
 {
 
-	if (VERBOSE >= 0)
-		printf("Definition %s: %s\n",
-		       oscap_reporter_message_get_user1str(msg),
-		       oval_result_get_text(oscap_reporter_message_get_user2num(msg)));
-	switch ((oval_result_t) oscap_reporter_message_get_user2num(msg)) {
+	if (VERBOSE <= 0)
+		return 0;
+
+	struct oval_definition *oval_def = oval_result_definition_get_definition(res_def);
+	oval_result_t result =  oval_result_definition_get_result(res_def);
+
+	printf("Definition %s: %s\n", oval_definition_get_title(oval_def), oval_result_get_text(result));
+
+	switch (result) {
 	case OVAL_RESULT_TRUE:
 		((struct oval_usr *)arg)->result_true++;
 		break;

commit 516e6b6c62f136679767b686221108f72d56584d

Remove unused variable.
Addressing:
src/OVAL/oval_agent.c: In function 'oval_agent_eval_system':
src/OVAL/oval_agent.c:247:16: warning: unused variable 'result' [-Wunused-variable]

Caused by:
5baf373909b919d15e91fedb200867aaccd8675a

src/OVAL/oval_agent.c
@@ -244,7 +244,6 @@ int oval_agent_eval_system(oval_agent_session_t * ag_sess, agent_reporter cb, vo
 	struct oval_definition_iterator *oval_def_it;
 	char   *id;
 	int ret = 0;
-	oval_result_t result;
 
 	oval_def_it = oval_definition_model_get_definitions(ag_sess->def_model);
 	while (oval_definition_iterator_has_more(oval_def_it)) {