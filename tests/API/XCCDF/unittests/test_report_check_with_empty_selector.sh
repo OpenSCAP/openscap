d8e58ddae3595b4a88d51ab1c7daee0a5543ab28
Simon Lukasik <slukasik@redhat.com>
2012-10-24 17:34:24 +0200

    tests: Workaround trac#245 for distcheck.

--- tests/API/XCCDF/unittests/test_report_check_with_empty_selector.sh
+++ tests/API/XCCDF/unittests/test_report_check_with_empty_selector.sh
@@ -8,7 +8,17 @@ name=$(basename $0 .sh)
 result=$(mktemp -t ${name}.out.XXXXXX)
 stderr=$(mktemp -t ${name}.out.XXXXXX)
 
+	# Workaround trac#245 for distcheck
+	oval=${name}.oval.xml.result.xml
+	ln -s $srcdir/$oval $oval
+	# Workaround end
+
 $OSCAP xccdf generate --format html report --output $result $srcdir/${name}.xccdf.xml.result.xml 2> $stderr
+
+	# Workaround
+	rm -rf $oval
+	unset oval
+	# Workaround end
 
 echo "Stderr file = $stderr"
 echo "Result file = $result"
