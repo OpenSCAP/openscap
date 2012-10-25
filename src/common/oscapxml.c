OpenSCAP/openscap

commit 10181fcaf104901c734fbf37f385ec9eb4e5c5b6

fix validation - libxml workaround

src/common/oscapxml.c
@@ -143,6 +143,7 @@ int oscap_validate_xml(const char *xmlfile, const char *schemafile, xml_reporter
 	xmlSchemaParserCtxtPtr parser_ctxt = NULL;
 	xmlSchemaPtr schema = NULL;
 	xmlSchemaValidCtxtPtr ctxt = NULL;
+	xmlDocPtr doc = NULL;
 	struct ctxt context = { reporter, arg, (void*) xmlfile };
 
         if (xmlfile == NULL) {
@@ -183,7 +184,11 @@ int oscap_validate_xml(const char *xmlfile, const char *schemafile, xml_reporter
 
 	xmlSchemaSetValidStructuredErrors(ctxt, oscap_xml_validity_handler, &context);
 
-	result = xmlSchemaValidateFile(ctxt, xmlfile, 0);
+	doc = xmlReadFile(xmlfile, NULL, 0);
+	if (!doc)
+		goto cleanup;
+
+	result = xmlSchemaValidateDoc(ctxt, doc);
 
 	/*
 	 * xmlSchemaValidateFile() returns "-1" if document is not well formed

commit 415b47708dbb72322bcf696a30faecc8905dca1e

Workarounds shall not leak.
Addressing
==1733== 9,764 (176 direct, 9,588 indirect) bytes in 1 blocks are definitely lost in loss record 36 of 36
==1733==    at 0x4A074CD: malloc (vg_replace_malloc.c:236)
==1733==    by 0x3E9C251E84: xmlNewDoc (tree.c:1153)
==1733==    by 0x3E9C2FCF40: xmlSAX2StartDocument (SAX2.c:982)
==1733==    by 0x3E9C24DF7D: xmlParseDocument (parser.c:10285)
==1733==    by 0x3E9C24E0C4: xmlDoRead (parser.c:14612)
==1733==    by 0x4C5248B: oscap_validate_xml (oscapxml.c:187)
==1733==    by 0x409CCB: app_evaluate_xccdf (oscap-xccdf.c:502)
==1733==    by 0x4064D3: oscap_module_process (oscap-tool.c:258)
==1733==    by 0x40556E: main (oscap.c:75)

Caused by:
10181fcaf104901c734fbf37f385ec9eb4e5c5b6

src/common/oscapxml.c
@@ -203,6 +203,7 @@ int oscap_validate_xml(const char *xmlfile, const char *schemafile, xml_reporter
 	*/
 
 cleanup:
+	if (doc) xmlFreeDoc(doc);
 	if (ctxt)        xmlSchemaFreeValidCtxt(ctxt);
 	if (schema)      xmlSchemaFree(schema);
 	if (parser_ctxt) xmlSchemaFreeParserCtxt(parser_ctxt);