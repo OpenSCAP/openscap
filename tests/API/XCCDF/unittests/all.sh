57c7416475cb41f52a5dee7da750084d5e3804ec
Simon Lukasik <slukasik@redhat.com>
2012-11-05 11:45:59 +0100

    tests: trac#257: xccdf:fix/@disruption and @complexity exporting.

--- tests/API/XCCDF/unittests/all.sh
+++ tests/API/XCCDF/unittests/all.sh
@@ -19,6 +19,7 @@ test_run "check/@multi-check simple" $srcdir/test_xccdf_check_multi_check2.sh
 test_run "check/@multi-check that has zero definitions" $srcdir/test_xccdf_check_multi_check_zero_definitions.sh
 test_run "xccdf:check-content-ref without @name" $srcdir/test_xccdf_check_content_ref_without_name_attr.sh
 test_run "xccdf:refine-rule/@weight shall not be exported" $srcdir/test_xccdf_refine_rule.sh
+test_run "xccdf:fix/@distruption|@complexity shall not be exported" $srcdir/test_xccdf_fix_attr_export.sh
 test_run "xccdf:complex-check/@operator=AND -- notchecked" $srcdir/test_xccdf_complex_check_and_notchecked.sh
 test_run "Check Processing Algorithm -- complex-check priority" $srcdir/test_xccdf_check_processing_complex_priority.sh
 test_run "Check Processing Algorithm -- bad refine must select check without @selector" $srcdir/test_xccdf_check_processing_selector_bad.sh
---
+++ tests/API/XCCDF/unittests/test_xccdf_fix_attr_export.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -e
+set -o pipefail
+
+name=$(basename $0 .sh)
+
+result=$(mktemp -t ${name}.out.XXXXXX)
+stdout=$(mktemp -t ${name}.out.XXXXXX)
+stderr=$(mktemp -t ${name}.out.XXXXXX)
+
+$OSCAP xccdf eval --results $result $srcdir/${name}.xccdf.xml > $stdout 2> $stderr
+
+echo "Stdout file = $stdout"
+echo "Stderr file = $stderr"
+echo "Result file = $result"
+[ -f $stderr ]; [ ! -s $stderr ]; rm -rf $stdout
+
+$OSCAP xccdf validate-xml $result
+
+assert_exists() { [ "$($XPATH $result 'count('"$2"')')" == "$1" ]; }
+
+assert_exists 2 '//fix'
+assert_exists 1 '//Rule/fix'
+assert_exists 1 '//rule-result/fix'
+assert_exists 1 '//Rule/fix/@id'
+assert_exists 1 '//Rule/fix/@*'
+assert_exists 1 '//rule-result/fix/@id'
+assert_exists 1 '//rule-result/fix/@*'
+
+rm -rf $result
