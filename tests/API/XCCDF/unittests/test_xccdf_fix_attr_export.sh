469c12e952a1698322759101db9adf7874c9dc1f
Simon Lukasik <slukasik@redhat.com>
2012-11-06 11:31:33 +0100

    tests: trac257: verify that fix/@* attributes are exported correctly.

--- tests/API/XCCDF/unittests/test_xccdf_fix_attr_export.sh
+++ tests/API/XCCDF/unittests/test_xccdf_fix_attr_export.sh
@@ -18,12 +18,30 @@ $OSCAP xccdf validate-xml $result
 
 assert_exists() { [ "$($XPATH $result 'count('"$2"')')" == "$1" ]; }
 
-assert_exists 2 '//fix'
-assert_exists 1 '//Rule/fix'
-assert_exists 1 '//rule-result/fix'
-assert_exists 1 '//Rule/fix/@id'
-assert_exists 1 '//Rule/fix/@*'
-assert_exists 1 '//rule-result/fix/@id'
-assert_exists 1 '//rule-result/fix/@*'
+assert_exists 6 '//fix'
+assert_exists 6 '//fix/@id'
+assert_exists 3 '//Rule/fix'
+assert_exists 3 '//rule-result/fix'
+assert_exists 1 '//Rule/fix[@id="fix-empty"]'
+assert_exists 1 '//rule-result/fix[@id="fix-empty"]'
+assert_exists 1 '//Rule/fix[@id="fix-unknown"]'
+assert_exists 1 '//rule-result/fix[@id="fix-unknown"]'
+assert_exists 1 '//Rule/fix[@id="fix-restrict"]'
+assert_exists 1 '//rule-result/fix[@id="fix-restrict"]'
+
+assert_exists 2 '//fix[@id="fix-empty"]'
+assert_exists 2 '//fix[@id="fix-empty"]/@*'
+
+assert_exists 2 '//fix[@id="fix-unknown"]'
+assert_exists 6 '//fix[@id="fix-unknown"]/@*'
+assert_exists 2 '//fix[@id="fix-unknown" and @disruption="unknown"]'
+assert_exists 2 '//fix[@id="fix-unknown" and @complexity="unknown"]'
+assert_exists 0 '//fix[@id="fix-unknown" and @strategy="unknown"]'
+
+assert_exists 2 '//fix[@id="fix-restrict"]'
+assert_exists 8 '//fix[@id="fix-restrict"]/@*'
+assert_exists 2 '//fix[@id="fix-restrict" and @disruption="medium"]'
+assert_exists 2 '//fix[@id="fix-restrict" and @complexity="high"]'
+assert_exists 2 '//fix[@id="fix-restrict" and @strategy="restrict"]'
 
 rm -rf $result
