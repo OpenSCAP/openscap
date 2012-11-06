2b02b5c2293906a3ff54e7b4aad815006d5324e1
Simon Lukasik <slukasik@redhat.com>
2012-11-06 10:01:48 +0100

    tests: Do not care about stdout, but unlink stderr.

--- tests/API/XCCDF/unittests/test_xccdf_fix_attr_export.sh
+++ tests/API/XCCDF/unittests/test_xccdf_fix_attr_export.sh
@@ -6,15 +6,13 @@ set -o pipefail
 name=$(basename $0 .sh)
 
 result=$(mktemp -t ${name}.out.XXXXXX)
-stdout=$(mktemp -t ${name}.out.XXXXXX)
 stderr=$(mktemp -t ${name}.out.XXXXXX)
 
-$OSCAP xccdf eval --results $result $srcdir/${name}.xccdf.xml > $stdout 2> $stderr
+$OSCAP xccdf eval --results $result $srcdir/${name}.xccdf.xml 2> $stderr
 
-echo "Stdout file = $stdout"
 echo "Stderr file = $stderr"
 echo "Result file = $result"
-[ -f $stderr ]; [ ! -s $stderr ]; rm -rf $stdout
+[ -f $stderr ]; [ ! -s $stderr ]; rm -rf $stderr
 
 $OSCAP xccdf validate-xml $result
 
