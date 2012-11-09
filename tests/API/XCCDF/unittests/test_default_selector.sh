375cb786f3ab2739110d7b9d88d99e822fba42cf
Simon Lukasik <slukasik@redhat.com>
2012-11-09 10:59:05 +0100

    tests: Extend the test for export-oval-variables.
    Each new assertion puts more requirements on the result xml.
    Each such requirement enhance the value of the test.
    
    The set of xml files accepted by previous version of the test
    is vastly bigger then the set accepted by newer version.

--- tests/API/XCCDF/unittests/test_default_selector.sh
+++ tests/API/XCCDF/unittests/test_default_selector.sh
@@ -15,13 +15,24 @@ echo "Result file = $result"
 [ -f $stderr ]; [ ! -s $stderr ]
 
 assert_exists() { [ $($XPATH $result 'count('$2')') == "$1" ]; }
-assert_exists 3 '//variable/value[text()="100"]'
+test_variable() {
+	assert_exists 3 '/oval_variables/variables/variable'
+	assert_exists 3 '//variable'
+	assert_exists 3 '//variable/*'
+	assert_exists 3 '//variable/value'
+	assert_exists 0 '//variable/value/@*'
+	assert_exists 1 '//variable[@id="oval:ssg:var:1"]/value'
+	assert_exists 1 '//variable[@id="oval:ssg:var:2"]/value'
+	assert_exists 1 '//variable[@id="oval:ssg:var:3"]/value'
+	assert_exists 3 '//variable/value[text()="100"]'
+}
 
+test_variable
 rm -rf $result
 
 # Ensure that it works even for export-oval-variables (trac#267)
 $OSCAP xccdf export-oval-variables $srcdir/${name}.xccdf.xml 2>&1 > $stderr
 [ -f $stderr ]; [ ! -s $stderr ]; rm $stderr
-assert_exists 3 '//variable/value[text()="100"]'
+test_variable
 rm $result
 
