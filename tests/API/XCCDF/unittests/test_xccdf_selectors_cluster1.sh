a3c81334f8c4d44d01af05fee27ba3e1847c62d6
Simon Lukasik <slukasik@redhat.com>
2012-11-20 16:00:42 +0100

    tests: trac#194: Use a selector with cluster-id to disable group.

---
+++ tests/API/XCCDF/unittests/test_xccdf_selectors_cluster1.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -e
+set -o pipefail
+
+name=$(basename $0 .sh)
+
+result=$(mktemp -t ${name}.out.XXXXXX)
+output=$(mktemp -t ${name}.out.XXXXXX)
+
+$OSCAP xccdf eval --profile xccdf_moc.elpmaxe.www_profile_1 --results $result $srcdir/${name}.xccdf.xml 2>&1 > $output
+
+echo "Output file = $output"
+echo "Result file = $result"
+[ -f $output ]; [ "`cat $output`" == "XCCDF Results are exported correctly." ]; rm -rf $output
+
+$OSCAP xccdf validate-xml $result
+
+assert_exists() { [ $($XPATH $result 'count('$2')') == "$1" ]; }
+assert_exists 1 '//Profile'
+assert_exists 2 '//Profile/select'
+assert_exists 1 '//Profile/select[@selected="true"]'
+assert_exists 1 '//Profile/select[@selected="false"]'
+assert_exists 1 '//rule-result'
+assert_exists 1 '//rule-result/result'
+assert_exists 1 '//rule-result/result[text()="notselected"]'
+assert_exists 1 '//score'
+assert_exists 1 '//score[text()="0.000000"]'
+rm -rf $result
