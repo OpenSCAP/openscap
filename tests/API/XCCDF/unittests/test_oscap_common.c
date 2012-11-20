d3e9a2d05ee189bb4241a9c12da7deeccd75fd2b
Simon Lukasik <slukasik@redhat.com>
2012-11-20 12:43:32 +0100

    tests: Test for oscap_htable_iterator.

--- tests/API/XCCDF/unittests/test_oscap_common.c
+++ tests/API/XCCDF/unittests/test_oscap_common.c
@@ -102,11 +102,145 @@ static void _test_empty_list_filter_has_more(void)
 	oscap_stringlist_free(names);
 }
 
+static void _test_hit_empty(void)
+{
+	struct oscap_htable *h = oscap_htable_new();
+	struct oscap_htable_iterator *hit = oscap_htable_iterator_new(h);
+	assume(!oscap_htable_iterator_has_more(hit));
+	assume(!oscap_htable_iterator_has_more(hit));
+	assume(!oscap_htable_iterator_has_more(hit));
+	oscap_htable_iterator_free(hit);
+	oscap_htable_free0(h);
+}
+
+static void _test_hit_single_item(void)
+{
+	static const char *key = "id-12345";
+	static const char *value = "openscap!";
+	struct oscap_htable *h = oscap_htable_new();
+	assume(oscap_htable_add(h, key, (char *) value));
+	struct oscap_htable_iterator *hit = oscap_htable_iterator_new(h);
+	assume(oscap_htable_iterator_has_more(hit));
+	const struct oscap_htable_item *item = oscap_htable_iterator_next(hit);
+	assume(item != NULL);
+	assume(strcmp(item->key, key) == 0);
+	assume(strcmp(item->value, value) == 0);
+	assume(!oscap_htable_iterator_has_more(hit));
+	assume(!oscap_htable_iterator_has_more(hit));
+	oscap_htable_iterator_free(hit);
+	oscap_htable_free0(h);
+}
+
+static void _test_hit_multiple_items(void)
+{
+	static const int n = 9;
+	bool seen[n];
+	struct oscap_htable *h = oscap_htable_new();
+	for (int i = 0; i < n; i++) {
+		char key[10];
+		snprintf(key, 10, "%d", i);
+		assume(oscap_htable_add(h, key, NULL));
+		seen[i] = false;
+	}
+
+	struct oscap_htable_iterator *hit = oscap_htable_iterator_new(h);
+	assume(oscap_htable_iterator_has_more(hit));
+	int i = 0;
+	while (oscap_htable_iterator_has_more(hit)) {
+		const char *key = oscap_htable_iterator_next_key(hit);
+		assume(key != NULL);
+		assume(strlen(key) == 1);
+		seen[key[0] - '0'] = true;
+		i++;
+	}
+	assume(i == n);
+	oscap_htable_iterator_free(hit);
+	for (i = 0; i < n; i++) {
+		assume(seen[i]);
+	}
+	oscap_htable_free0(h);
+}
+
+static int _htable_cmp(const char *s1, const char *s2)
+{
+        if (s1 == NULL)
+                return -1;
+        if (s2 == NULL)
+                return 1;
+        return strcmp(s1, s2);
+}
+
+static void _test_hit_empty1(void)
+{
+	struct oscap_htable *h = oscap_htable_new1(_htable_cmp, 1);
+	struct oscap_htable_iterator *hit = oscap_htable_iterator_new(h);
+	assume(!oscap_htable_iterator_has_more(hit));
+	assume(!oscap_htable_iterator_has_more(hit));
+	assume(!oscap_htable_iterator_has_more(hit));
+	oscap_htable_iterator_free(hit);
+	oscap_htable_free0(h);
+}
+
+static void _test_hit_single_item1(void)
+{
+	static const char *key = "id-12345";
+	static const char *value = "openscap!";
+	struct oscap_htable *h = oscap_htable_new1(_htable_cmp, 1);
+	assume(oscap_htable_add(h, key, (char *) value));
+	struct oscap_htable_iterator *hit = oscap_htable_iterator_new(h);
+	assume(oscap_htable_iterator_has_more(hit));
+	const struct oscap_htable_item *item = oscap_htable_iterator_next(hit);
+	assume(item != NULL);
+	assume(strcmp(item->key, key) == 0);
+	assume(strcmp(item->value, value) == 0);
+	assume(!oscap_htable_iterator_has_more(hit));
+	assume(!oscap_htable_iterator_has_more(hit));
+	oscap_htable_iterator_free(hit);
+	oscap_htable_free0(h);
+}
+
+static void _test_hit_multiple_items1(void)
+{
+	static const int n = 9;
+	bool seen[n];
+	struct oscap_htable *h = oscap_htable_new1(_htable_cmp, 1);
+	for (int i = 0; i < n; i++) {
+		char key[10];
+		snprintf(key, 10, "%d", i);
+		assume(oscap_htable_add(h, key, NULL));
+		seen[i] = false;
+	}
+
+	struct oscap_htable_iterator *hit = oscap_htable_iterator_new(h);
+	assume(oscap_htable_iterator_has_more(hit));
+	int i = 0;
+	while (oscap_htable_iterator_has_more(hit)) {
+		const char *key = oscap_htable_iterator_next_key(hit);
+		assume(key != NULL);
+		assume(strlen(key) == 1);
+		seen[key[0] - '0'] = true;
+		i++;
+	}
+	assume(i == n);
+	oscap_htable_iterator_free(hit);
+	for (i = 0; i < n; i++) {
+		assume(seen[i]);
+	}
+	oscap_htable_free0(h);
+}
+
 int main(int argc, char *argv[])
 {
 	_test_first_item_is_not_skipped();
 	_test_not_matching_last_item_is_not_returned();
 	_test_empty_list_has_more();
 	_test_empty_list_filter_has_more();
+
+	_test_hit_empty();
+	_test_hit_single_item();
+	_test_hit_multiple_items();
+	_test_hit_empty1();
+	_test_hit_single_item1();
+	_test_hit_multiple_items1();
 	return 0;
 }
