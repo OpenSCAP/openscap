0c51deb518ef10e2a6d57ef75654234013fd2075
Daniel Kopecek <dkopecek@redhat.com>
2012-12-06 12:38:38 +0100

    confgen.sh: don't overwrite configure.ac in case of a failure

--- confgen.sh
+++ confgen.sh
@@ -2,12 +2,14 @@
 D="$(pwd)"
 
 echo -n "ac_probes.sh... "
-./ac_probes/ac_probes.sh "$D/ac_probes/configure.ac.tpl" "$D/ac_probes/" "$D/src/OVAL/probes/" > configure.ac
+C="$(./ac_probes/ac_probes.sh "$D/ac_probes/configure.ac.tpl" "$D/ac_probes/" "$D/src/OVAL/probes/")"
 
 ret=$?
 if [ $ret -ne 0 ]; then
     echo "failed: $ret"
+    exit $ret
 else
+    echo "$C" > configure.ac
     echo "ok"
 fi
 
@@ -17,6 +19,7 @@ echo -n "autogen.sh... "
 ret=$?
 if [ $ret -ne 0 ]; then
     echo "failed: $ret"
+    exit $ret
 else
     echo "ok"
 fi
