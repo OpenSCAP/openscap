d2dc338727be6acfc35754fa9ed3e70552e6fa65
Simon Lukasik <slukasik@redhat.com>
2012-12-13 17:23:01 +0100

    tests: Do not combine && with set -e
    It has a very special semantics. See `man bash` for more info.

--- tests/API/OVAL/unittests/test_applicability_check.sh
+++ tests/API/OVAL/unittests/test_applicability_check.sh
@@ -13,5 +13,6 @@ set -o pipefail
 [ $($xpath $result 'count(/oval_results/oval_definitions/definitions/definition[@id="oval:gov.irs.rhel5:def:1"]/criteria/extend_definition[@applicability_check="true"])') == "1" ]
 [ $($xpath $result 'count(/oval_results/results/system/definitions/definition[@definition_id="oval:gov.irs.rhel5:def:1"]/criteria[@applicability_check="true"])') == "1" ]
 [ $($xpath $result 'count(/oval_results/results/system/definitions/definition[@definition_id="oval:gov.irs.rhel5:def:1"]/criteria/criterion[@applicability_check="true"])') == "1" ]
-[ $($xpath $result 'count(/oval_results/results/system/definitions/definition[@definition_id="oval:gov.irs.rhel5:def:1"]/criteria/extend_definition[@applicability_check="true"])') == "1" ] && rm $result
+[ $($xpath $result 'count(/oval_results/results/system/definitions/definition[@definition_id="oval:gov.irs.rhel5:def:1"]/criteria/extend_definition[@applicability_check="true"])') == "1" ]
+rm $result
 
