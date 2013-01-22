7847fcf901b503d9403cbffa42533b53ac5e9ac7
Martin Preisler <mpreisle@redhat.com>
2013-01-22 15:56:25 +0100

    Added hybrid DS + external file tailoring tests

--- tests/API/XCCDF/tailoring/all.sh
+++ tests/API/XCCDF/tailoring/all.sh
@@ -51,6 +51,28 @@ function test_api_xccdf_tailoring_ds {
     return 1
 }
 
+function test_api_xccdf_tailoring_ds_hybrid {
+    local INPUT=$srcdir/$1
+    local TAILORING=$srcdir/$2
+    local PROFILE=$3
+    local EXPECTED_PASS=$4
+
+    local TMP_RESULTS=`mktemp`
+    $OSCAP xccdf eval --tailoring-file $TAILORING --profile $PROFILE --results $TMP_RESULTS $INPUT
+    if [ "$?" != "0" ]; then
+        return 1
+    fi
+
+    local PASS_COUNT=$($XPATH $TMP_RESULTS 'count(//result[text()="pass"])')
+    rm -f $TMP_RESULTS
+
+    if [ "$PASS_COUNT" == "$EXPECTED_PASS" ]; then
+        return 0
+    fi
+
+    return 1
+}
+
 # Testing.
 
 test_init "test_api_xccdf_tailoring.log"
@@ -61,5 +83,8 @@ test_run "test_api_xccdf_tailoring_override" test_api_xccdf_tailoring simple-xcc
 test_run "test_api_xccdf_tailoring_ds_default" test_api_xccdf_tailoring_ds simple-ds.xml scap_org.open-scap_cref_simple-tailoring.xml xccdf_org.open-scap_profile_default 1
 test_run "test_api_xccdf_tailoring_ds_unselecting" test_api_xccdf_tailoring_ds simple-ds.xml scap_org.open-scap_cref_simple-tailoring.xml xccdf_org.open-scap_profile_unselecting 0
 test_run "test_api_xccdf_tailoring_ds_override" test_api_xccdf_tailoring_ds simple-ds.xml scap_org.open-scap_cref_simple-tailoring.xml xccdf_org.open-scap_profile_override 1
+test_run "test_api_xccdf_tailoring_ds_hybrid_default" test_api_xccdf_tailoring_ds_hybrid simple-ds.xml simple-tailoring.xml xccdf_org.open-scap_profile_default 1
+test_run "test_api_xccdf_tailoring_ds_hybrid_unselecting" test_api_xccdf_tailoring_ds_hybrid simple-ds.xml simple-tailoring.xml xccdf_org.open-scap_profile_unselecting 0
+test_run "test_api_xccdf_tailoring_ds_hybrid_override" test_api_xccdf_tailoring_ds_hybrid simple-ds.xml simple-tailoring.xml xccdf_org.open-scap_profile_override 1
 
 test_exit
