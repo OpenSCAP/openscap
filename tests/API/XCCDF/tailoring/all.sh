cf0bb83ba7eff2bec84e46206cbf9e45c51c80d7
Martin Preisler <mpreisle@redhat.com>
2013-01-22 15:34:43 +0100

    Added tests for tailoring inside a datastream

--- tests/API/XCCDF/tailoring/all.sh
+++ tests/API/XCCDF/tailoring/all.sh
@@ -28,6 +28,29 @@ function test_api_xccdf_tailoring {
 
     return 1
 }
+
+function test_api_xccdf_tailoring_ds {
+    local INPUT=$srcdir/$1
+    local TAILORING_ID=$2
+    local PROFILE=$3
+    local EXPECTED_PASS=$4
+
+    local TMP_RESULTS=`mktemp`
+    $OSCAP xccdf eval --tailoring-id $TAILORING_ID --profile $PROFILE --results $TMP_RESULTS $INPUT
+    if [ "$?" != "0" ]; then
+        return 1
+    fi
+
+    local PASS_COUNT=$($XPATH $TMP_RESULTS 'count(//result[text()="pass"])')
+    rm -f $TMP_RESULTS
+
+    if [ "$PASS_COUNT" == "$EXPECTED_PASS" ]; then
+        return 0
+    fi
+
+    return 1
+}
+
 # Testing.
 
 test_init "test_api_xccdf_tailoring.log"
@@ -35,5 +58,8 @@ test_init "test_api_xccdf_tailoring.log"
 test_run "test_api_xccdf_tailoring_default" test_api_xccdf_tailoring simple-xccdf.xml simple-tailoring.xml xccdf_org.open-scap_profile_default 1
 test_run "test_api_xccdf_tailoring_unselecting" test_api_xccdf_tailoring simple-xccdf.xml simple-tailoring.xml xccdf_org.open-scap_profile_unselecting 0
 test_run "test_api_xccdf_tailoring_override" test_api_xccdf_tailoring simple-xccdf.xml simple-tailoring.xml xccdf_org.open-scap_profile_override 1
+test_run "test_api_xccdf_tailoring_ds_default" test_api_xccdf_tailoring_ds simple-ds.xml scap_org.open-scap_cref_simple-tailoring.xml xccdf_org.open-scap_profile_default 1
+test_run "test_api_xccdf_tailoring_ds_unselecting" test_api_xccdf_tailoring_ds simple-ds.xml scap_org.open-scap_cref_simple-tailoring.xml xccdf_org.open-scap_profile_unselecting 0
+test_run "test_api_xccdf_tailoring_ds_override" test_api_xccdf_tailoring_ds simple-ds.xml scap_org.open-scap_cref_simple-tailoring.xml xccdf_org.open-scap_profile_override 1
 
 test_exit
