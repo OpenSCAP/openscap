7853691ecff7afc9981955e74987e51d88bfd7d3
Simon Lukasik <slukasik@redhat.com>
2013-01-28 16:28:29 +0100

    Introduce a helper module for iterating through minidom.
    This shall be usefull when resolving the Text Substitution
    of XCCDF.
    
    Note that in some cases we store snippets of xml as plain text,
    and which might need to be modified later during processing.

---
+++ src/common/xml_iterate.c
@@ -0,0 +1,93 @@
+/*
+ * Copyright 2013 Red Hat Inc., Durham, North Carolina.
+ * All Rights Reserved.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <string.h>
+#include "debug_priv.h"
+#include "xml_iterate.h"
+
+
+static int xml_element_dfs_callback(xmlNode *node, xml_iterate_callback user_fn, void *user_data)
+{
+	int result = user_fn(node, user_data);
+	if (result == 1)
+		return result;
+	xmlNodePtr child = node->children;
+	while (child != NULL) {
+		int res = xml_element_dfs_callback(child, user_fn, user_data);
+		if (res == 1)
+			return res;
+		if (result == 0)
+			result = res;
+		child = child->next;
+	}
+	return result;
+}
+
+int xml_iterate_dfs(const char *input_text, char **output_text, xml_iterate_callback user_fn, void *user_data)
+{
+	char *input_document = NULL;
+	xmlDocPtr doc = NULL;
+	xmlNode *root = NULL;
+	int res = 0;
+
+	if (user_fn == NULL)
+		return 1;
+
+	input_document = oscap_sprintf("<x xmlns='http://www.w3.org/1999/xhtml'>%s</x>", input_text);
+	if ((doc = xmlParseMemory(input_document, strlen(input_document))) == NULL) {
+		dW("Could not xmlParseMemory: '%s'\n", input_document);
+		free(input_document);
+		return 1;
+	}
+	if ((root = xmlDocGetRootElement(doc)) == NULL) {
+		dW("Could not xmlDocGetRootElement: '%s'\n", input_document);
+		xmlFreeDoc(doc);
+		free(input_document);
+		return 1;
+	}
+	free(input_document);
+
+	res = xml_element_dfs_callback(root, user_fn, user_data);
+	if (output_text != NULL) {
+		// We cannot simply xmlDumpMemory, because we need to skip the upper <x/> ellement.
+		if ((root = xmlDocGetRootElement(doc)) == NULL) {
+			dW("Could not get xmlDocGetRootElement of result document.\n");
+			xmlFreeDoc(doc);
+			return 1;
+		}
+		xmlBuffer *buff = xmlBufferCreate();
+		xmlNode *child = root->children;
+		while (child != NULL) {
+			int size = xmlNodeDump(buff, doc, child, 0, 0);
+			if (size == 0) {
+				dW("xmlBufNodeDump returns zero.\n");
+			}
+			child = child->next;
+		}
+		/* Sadly, we cannot use xmlBufferDetach since we want to support older libxml. */
+		*output_text = oscap_strdup((const char *) xmlBufferContent(buff));
+		xmlBufferFree(buff);
+	}
+	xmlFreeDoc(doc);
+	return res;
+}
---
+++ src/common/xml_iterate.h
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2013 Red Hat Inc., Durham, North Carolina.
+ * All Rights Reserved.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Authors:
+ *   Simon Lukasik <slukasik@redhat.com>
+ *
+ */
+
+#pragma once
+#ifndef _OSCAP_XML_ITERATE_H
+#define _OSCAP_XML_ITERATE_H
+
+#include "util.h"
+#include <libxml/tree.h>
+
+OSCAP_HIDDEN_START;
+
+/**
+ * A callback function to be called on each xmlNode.
+ * @param node the current xml node
+ * @param user_data
+ * @returns the result of operation
+ * @retval 0 indicates success of callback call
+ * @retval 1 indicates hard failure of callback call and process is immediatelly terminated
+ * @retval else idicates warn and walking through tree continues
+ */
+typedef int (*xml_iterate_callback) (xmlNode *node, void *user_data);
+
+/**
+ * Parse the input_text as XML and iterate through all nodes in minidom
+ * using DFS order. Calls user_fn with user_data on each node. The user_fn
+ * is allowed to modify the minidom tree.
+ * @param input_text the string which shall be parsed
+ * @param output_text the output document after tree walking. This attribute
+ * might be NULL to skip the export (which might be usefull in case when
+ * user_fn does not modify the tree). The caller shall dispose *output_text.
+ * @param user_fn Function which is called on each node
+ * @param user_data Data supplied to the user_fn
+ * @returns 0 on success, 1 on failure.
+ */
+int xml_iterate_dfs(const char *input_text, char **output_text, xml_iterate_callback user_fn, void *user_data);
+
+OSCAP_HIDDEN_END;
+
+#endif
