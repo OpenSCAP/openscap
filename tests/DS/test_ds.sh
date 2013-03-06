b513e77ea60186c186b4aeb105821d6b28af6c64
Martin Preisler <mpreisle@redhat.com>
2013-03-06 16:13:14 +0100

    Added test for plain-text extended-component in datastream

--- tests/DS/test_ds.sh
+++ tests/DS/test_ds.sh
@@ -188,6 +188,7 @@ test_run "sds_simple" test_sds sds_simple scap-fedora14-xccdf.xml 0
 test_run "sds_multiple_oval" test_sds sds_multiple_oval multiple-oval-xccdf.xml 0
 test_run "sds_subdir" test_sds sds_subdir subdir/scap-fedora14-xccdf.xml 1
 test_run "sds_extended_component" test_sds sds_extended_component fake-check-xccdf.xml 0
+test_run "sds_extended_component_plain_text" test_sds sds_extended_component_plain_text fake-check-xccdf.xml 0
 
 test_run "eval_simple" test_eval eval_simple/sds.xml
 test_run "eval_invalid" test_invalid_eval eval_invalid/sds.xml
