925ae5b3ec3f9a75b96b4f9fc72acfab1dd34e24
Simon Lukasik <slukasik@redhat.com>
2013-03-12 15:21:43 +0100

    tests: Assert for TestResults exported by oscap-xccdf-remediate
    This covers multiple scenarios:
     - appending new TestResult to the bottom of the document
     - using the very last test result, when --result-id is not supplied
     - creation of new TestResult/@id based of the input TestResult
     - creation when the new TestResult/@id already exists
     - when there is nothing to remediate (result=notchecked)
     - when there is wrong OVAL to verify remediation (result=error)
     - successful run (result=fixed)

--- tests/API/XCCDF/unittests/test_remediate_simple.sh
+++ tests/API/XCCDF/unittests/test_remediate_simple.sh
@@ -4,25 +4,75 @@ set -e
 set -o pipefail
 
 name=$(basename $0 .sh)
+
+# Check the input data.
+result=$srcdir/${name}.xccdf.xml
+assert_exists 3 '//TestResult'
+assert_exists 1 '//TestResult[@id="xccdf_org.open-scap_testresult_default-profile"]'
+assert_exists 1 '//TestResult[@id="xccdf_org.open-scap_testresult_default-profile001"]'
+assert_exists 1 '//TestResult[@id="xccdf_org.open-scap_testresult_default-profile002"]'
+
 stderr=$(mktemp -t ${name}.out.XXXXXX)
+tmpdir=$(mktemp -d -t ${name}.out.XXXXXX)
+oval=test_remediation_simple.oval.xml
+cp $srcdir/$oval $tmpdir # Accomodate scanning by xccdf placed in the same temp dir.
+result=$(mktemp --tmpdir=$tmpdir ${name}.out.XXXXXX)
 echo "Stderr file = $stderr"
+echo "Result file = $stderr"
 rm -f test_file
 
-$OSCAP xccdf remediate $srcdir/${name}.xccdf.xml 2> $stderr
+$OSCAP xccdf remediate --results $result  $srcdir/${name}.xccdf.xml 2> $stderr
 [ -f $stderr ]; [ ! -s $stderr ]; :> $stderr
 [ ! -f test_file ]
+$OSCAP xccdf validate $result
+assert_exists 4 '//TestResult'
+assert_exists 1 '//TestResult[@id="xccdf_org.open-scap_testresult_default-profile002001"]'
+assert_exists 1 '//TestResult[@id="xccdf_org.open-scap_testresult_default-profile002001"]/rule-result'
+assert_exists 1 '//TestResult[@id="xccdf_org.open-scap_testresult_default-profile002001"]/rule-result/result'
+assert_exists 1 '//TestResult[@id="xccdf_org.open-scap_testresult_default-profile002001"]/rule-result/result[text()="notchecked"]'
 
-$OSCAP xccdf remediate --result-id xccdf_org.open-scap_testresult_default-profile002 $srcdir/${name}.xccdf.xml 2> $stderr
+
+$OSCAP xccdf remediate --result-id xccdf_org.open-scap_testresult_default-profile002 --results $result $result 2> $stderr
 [ -f $stderr ]; [ ! -s $stderr ]; :> $stderr
 [ ! -f test_file ]
+$OSCAP xccdf validate $result
+assert_exists 5 '//TestResult'
+assert_exists 1 '//TestResult[@id="xccdf_org.open-scap_testresult_default-profile002002"]'
+assert_exists 1 '//TestResult[@id="xccdf_org.open-scap_testresult_default-profile002002"]/rule-result'
+assert_exists 1 '//TestResult[@id="xccdf_org.open-scap_testresult_default-profile002002"]/rule-result/result'
+assert_exists 1 '//TestResult[@id="xccdf_org.open-scap_testresult_default-profile002002"]/rule-result/result[text()="notchecked"]'
+
 
 ret=0
-$OSCAP xccdf remediate --result-id xccdf_org.open-scap_testresult_default-profile001 $srcdir/${name}.xccdf.xml 2> $stderr || ret=$?
+$OSCAP xccdf remediate --result-id xccdf_org.open-scap_testresult_default-profile001 --results $result $result 2> $stderr || ret=$?
 [ $ret -eq 2 ]
 [ -f $stderr ]; [ ! -s $stderr ]; :> $stderr
 [ -f test_file ]; rm test_file
+$OSCAP xccdf validate $result
+assert_exists 6 '//TestResult'
+assert_exists 1 '//TestResult[@id="xccdf_org.open-scap_testresult_default-profile001001"]'
+assert_exists 1 '//TestResult[@id="xccdf_org.open-scap_testresult_default-profile001001"]/rule-result'
+assert_exists 1 '//TestResult[@id="xccdf_org.open-scap_testresult_default-profile001001"]/rule-result/result'
+assert_exists 1 '//TestResult[@id="xccdf_org.open-scap_testresult_default-profile001001"]/rule-result/result[text()="error"]'
+assert_exists 1 '//TestResult[@id="xccdf_org.open-scap_testresult_default-profile001001"]/rule-result/fix'
+assert_exists 2 '//TestResult[@id="xccdf_org.open-scap_testresult_default-profile001001"]/rule-result/message'
+assert_exists 1 '//TestResult[@id="xccdf_org.open-scap_testresult_default-profile001001"]/rule-result/message[text()="Fix execution comleted and returned: 0"]'
+assert_exists 1 '//TestResult[@id="xccdf_org.open-scap_testresult_default-profile001001"]/rule-result/message[text()="Failed to verify applied fix: Checking engine returns: notchecked"]'
 
-$OSCAP xccdf remediate --result-id xccdf_org.open-scap_testresult_default-profile $srcdir/${name}.xccdf.xml 2> $stderr
+
+$OSCAP xccdf remediate --result-id xccdf_org.open-scap_testresult_default-profile --results $result $result 2> $stderr
 [ -f $stderr ]; [ ! -s $stderr ]; rm $stderr
 [ -f test_file ]; rm test_file
+$OSCAP xccdf validate $result
+assert_exists 7 '//TestResult'
+assert_exists 1 '//TestResult[@id="xccdf_org.open-scap_testresult_default-profile003"]'
+assert_exists 1 '//TestResult[@id="xccdf_org.open-scap_testresult_default-profile003"]/rule-result'
+assert_exists 1 '//TestResult[@id="xccdf_org.open-scap_testresult_default-profile003"]/rule-result/result'
+assert_exists 1 '//TestResult[@id="xccdf_org.open-scap_testresult_default-profile003"]/rule-result/result[text()="fixed"]'
+assert_exists 1 '//TestResult[@id="xccdf_org.open-scap_testresult_default-profile003"]/rule-result/fix'
+assert_exists 1 '//TestResult[@id="xccdf_org.open-scap_testresult_default-profile003"]/rule-result/message'
+assert_exists 1 '//TestResult[@id="xccdf_org.open-scap_testresult_default-profile003"]/rule-result/message[text()="Fix execution comleted and returned: 0"]'
 
+rm $result
+rm $tmpdir/$oval
+rmdir $tmpdir
