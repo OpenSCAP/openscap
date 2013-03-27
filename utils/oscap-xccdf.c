5663d0243d2871c12eb8a60a2aa7b89678a80690
Simon Lukasik <slukasik@redhat.com>
2013-03-27 15:08:37 +0100

    Allow --cpe argument when generating fix from source XCCDF.
    For now this option is intentionally left undocumented.
    In most cases we still rely on xsl/fix.xslt. Which does
    not process CPE. This option shall be documented once
    the xsl/fix.xslt is gone.
    
    This option will be used by Anaconda however, to allow
    different Anaconda_Fixes for different Anaconda versions.

--- utils/oscap-xccdf.c
+++ utils/oscap-xccdf.c
@@ -767,10 +767,12 @@ int app_generate_fix(const struct oscap_action *action)
 	struct xccdf_session *session = xccdf_session_new(action->f_xccdf);
 	if (session == NULL)
 		goto cleanup;
+	xccdf_session_set_user_cpe(session, action->cpe);
 	if (xccdf_session_load_xccdf(session) != 0)
 		goto cleanup;
+	if (xccdf_session_load_cpe(session) != 0)
+		goto cleanup;
 
-	// TODO: We may need CPE dict for CPE applicability
 	if (!xccdf_session_set_profile_id(session, action->profile)) {
 		fprintf(stderr, "Profile \"%s\" was not found.\n", action->profile);
 		goto cleanup;
