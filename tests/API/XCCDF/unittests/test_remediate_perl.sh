f34a783a5c0a800ad34f99828a6b59cedb50ea05
Simon Lukasik <slukasik@redhat.com>
2013-04-03 10:54:11 +0200

    tests: Simple test for perl fix

---
+++ tests/API/XCCDF/unittests/test_remediate_perl.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -e
+set -o pipefail
+
+name=$(basename $0 .sh)
+stderr=$(mktemp -t ${name}.out.XXXXXX)
+result=$(mktemp -t ${name}.out.XXXXXX)
+echo "Stderr file = $stderr"
+echo "Result file = $stderr"
+rm -f test_file
+
+$OSCAP xccdf remediate --results $result $srcdir/${name}.xccdf.xml 2> $stderr
+[ -f $stderr ]; [ ! -s $stderr ]; rm $stderr
+[ -f test_file ]; rm test_file
+$OSCAP xccdf validate $result
+assert_exists 2 '//TestResult'
+assert_exists 1 '//TestResult[@id="xccdf_org.open-scap_testresult_default-profile001"]'
+assert_exists 1 '//TestResult[@id="xccdf_org.open-scap_testresult_default-profile001"]/rule-result'
+assert_exists 1 '//TestResult[@id="xccdf_org.open-scap_testresult_default-profile001"]/rule-result/result'
+assert_exists 1 '//TestResult[@id="xccdf_org.open-scap_testresult_default-profile001"]/rule-result/result[text()="fixed"]'
+assert_exists 1 '//TestResult[@id="xccdf_org.open-scap_testresult_default-profile001"]/rule-result/fix'
+assert_exists 1 '//TestResult[@id="xccdf_org.open-scap_testresult_default-profile001"]/rule-result/message'
+assert_exists 1 '//TestResult[@id="xccdf_org.open-scap_testresult_default-profile001"]/rule-result/message[text()="Fix execution comleted and returned: 0"]'
+
+rm $result
