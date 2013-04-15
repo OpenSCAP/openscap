addc83da7cfcffc2e8a7c634c838b62fbbd76f26
Richard W.M. Jones <rjones@redhat.com>
2013-04-15 15:02:44 +0100

    Add ./run script.
    In libguestfs & libvirt we have very successfully use a ./run script
    in the top level directory to set environment variables so the program
    can be run from the local directory.
    
    eg:
      ./run utils/oscap oval eval [...]
    or:
      ./run gdb --args utils/oscap oval eval [...]
    or from another project that uses openscap libraries:
      ../openscap/run ./another-program
    
    The same idea appears in the utils/oscap-local.sh script, but ./run is
    more flexible since it can also be used for tests, or for running any
    external program that happens to use the openscap libraries.
    
    As well as being easier to type.
    
    Signed-off-by: Martin Preisler <mpreisle@redhat.com>

--- utils/oscap-local.sh
+++ utils/oscap-local.sh
@@ -1,27 +1,8 @@
-#
-# Use this to run local oscap tool build
+echo "Use the following command instead:"
+echo
+echo "  ../run" "$@"
+echo
+echo "where '../run' is the path to the script called 'run' in the"
+echo "top level build directory."
 
-# Usage example:
-# $ ./oscap-local.sh gdb --args .libs/oscap xccdf eval ...
-if [ $# -eq 0 ]; then
-	echo "Usage: \"$ ./oscap-local.sh .libs/oscap xccdf eval ...\"";
-fi
-
-# parent directory of the script, this is so that you can cd anywhere
-# and still be able to run oscap or various tests, etc...
-PARENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-PREFIX="$PARENT_DIR/.."
-
-# Preload testing library that accept various environment variables
-export LD_PRELOAD="$PREFIX/src/.libs/libopenscap_testing.so"
-export LD_LIBRARY_PATH="$PREFIX/src/.libs"
-
-# Set paths to probes, schemas, transformations and cpes
-export OVAL_PROBE_DIR="$PREFIX/src/OVAL/probes"
-export OSCAP_SCHEMA_PATH="$PREFIX/schemas"
-export OSCAP_XSLT_PATH="$PREFIX/xsl"
-export OSCAP_CPE_PATH="$PREFIX/cpe"
-
-# Execution part
-exec "$@"
-
+exit 1
