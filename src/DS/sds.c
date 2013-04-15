OpenSCAP/openscap

commit 094fa1710dde941ff88e311350739a30ddc0ef71

Avoid exporting duplicate components in the same batch run
We may still export component which is duplicate to the previously
added one, however that is hard to spot.

In future, we may need to have deduplication algorithm, which will
go through the DOM structure of each two components. Noteworthy,
the plain-text comparison seems not be feasible since namespaces
may be named differently and so on.

src/DS/sds.c
@@ -33,6 +33,7 @@
 #include "common/alloc.h"
 #include "common/_error.h"
 #include "common/util.h"
+#include "common/list.h"
 
 #include <sys/stat.h>
 #include <time.h>
@@ -757,6 +758,7 @@ static int ds_sds_compose_add_xccdf_dependencies(xmlDocPtr doc, xmlNodePtr datas
 	xmlNodeSetPtr nodeset = xpathObj->nodesetval;
 	if (nodeset != NULL)
 	{
+		struct oscap_htable *exported = oscap_htable_new();
 		char* filepath_cpy = oscap_strdup(filepath);
 		const char* dir = dirname(filepath_cpy);
 
@@ -770,6 +772,12 @@ static int ds_sds_compose_add_xccdf_dependencies(xmlDocPtr doc, xmlNodePtr datas
 			if (xmlHasProp(node, BAD_CAST "href"))
 			{
 				char* href = (char*)xmlGetProp(node, BAD_CAST "href");
+				if (oscap_htable_get(exported, href) != NULL) {
+					// This path has been already exported. Do not export duplicate.
+					xmlFree(href);
+					continue;
+				}
+				oscap_htable_add(exported, href, "");
 				char* real_path = (strcmp(dir, "") == 0 || strcmp(dir, ".") == 0) ?
 					oscap_strdup(href) : oscap_sprintf("%s/%s", dir, href);
 
@@ -805,6 +813,7 @@ static int ds_sds_compose_add_xccdf_dependencies(xmlDocPtr doc, xmlNodePtr datas
 					oscap_free(uri);
 					oscap_free(cref_id);
 					oscap_free(real_path);
+					oscap_htable_free0(exported);
 					xmlFree(href);
 					return -1;
 				}
@@ -815,13 +824,13 @@ static int ds_sds_compose_add_xccdf_dependencies(xmlDocPtr doc, xmlNodePtr datas
 				xmlSetProp(catalog_uri, BAD_CAST "uri", BAD_CAST uri);
 
 				oscap_free(uri);
-				oscap_free(real_path);
 				xmlFree(href);
 
 				xmlAddChild(catalog, catalog_uri);
 			}
 		}
 
+		oscap_htable_free0(exported);
 		oscap_free(filepath_cpy);
 	}
 

commit a9918ec4fb7284697b73a16f4c60fcaed84367b0

Fix memory leak
Introduced by:
094fa1710dde941ff88e311350739a30ddc0ef71

src/DS/sds.c
@@ -822,12 +822,12 @@ static int ds_sds_compose_add_xccdf_dependencies(xmlDocPtr doc, xmlNodePtr datas
 
 				oscap_free(cref_id);
 				oscap_free(uri);
+				oscap_free(real_path);
 				xmlFree(href);
 
 				if (ret < 0) {
 					// oscap_seterr has already been called
 					xmlFreeDoc(component_doc);
-					oscap_free(real_path);
 					oscap_htable_free0(exported);
 					return -1;
 				}