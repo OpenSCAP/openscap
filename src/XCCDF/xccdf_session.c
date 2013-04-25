OpenSCAP/openscap

commit d4ab011b3e25258b5f344429694e767a6ad7d9d8

Refactor SCE initialization to xccdf_session.

src/XCCDF/xccdf_session.c
@@ -37,6 +37,9 @@
 #include "common/_error.h"
 #include "DS/public/scap_ds.h"
 #include "XCCDF_POLICY/public/xccdf_policy.h"
+#ifdef ENABLE_SCE
+#include <sce_engine_api.h>
+#endif
 #include "public/xccdf_session.h"
 
 static void _oval_content_resources_free(struct oval_content_resource **resources);
@@ -59,6 +62,10 @@ struct xccdf_session *xccdf_session_new(const char *filename)
 
 void xccdf_session_free(struct xccdf_session *session)
 {
+#ifdef ENABLE_SCE
+	if (session->sce.parameters != NULL)
+		sce_parameters_free(session->sce.parameters);
+#endif
 	oscap_free(session->oval.product_cpe);
 	_xccdf_session_free_oval_agents(session);
 	_oval_content_resources_free(session->oval.custom_resources);
@@ -562,6 +569,23 @@ int xccdf_session_load_oval(struct xccdf_session *session)
 			xccdf_policy_model_register_engine_oval(session->xccdf.policy_model, tmp_sess);
 	}
 	return 0;
+}
+
+int xccdf_session_load_sce(struct xccdf_session *session)
+{
+#ifdef ENABLE_SCE
+	char *xccdf_pathcopy;
+	if (session->sce.parameters != NULL)
+		sce_parameters_free(session->sce.parameters);
+
+	session->sce.parameters = sce_parameters_new();
+	xccdf_pathcopy = oscap_strdup(session->xccdf.file);
+	sce_parameters_set_xccdf_directory(session->sce.parameters, dirname(xccdf_pathcopy));
+	oscap_free(xccdf_pathcopy);
+	return !xccdf_policy_model_register_engine_sce(session->xccdf.policy_model, session->sce.parameters);
+#else
+	return 0;
+#endif
 }
 
 OSCAP_GENERIC_GETTER(struct xccdf_policy_model *, xccdf_session, policy_model, xccdf.policy_model)

src/XCCDF/Makefile.am
@@ -21,6 +21,7 @@ libxccdf_la_CPPFLAGS  = @xml2_CFLAGS@ \
 			-I$(top_srcdir)/src \
 			-I$(top_srcdir)/src/XCCDF/public \
 			-I$(top_srcdir)/src/XCCDF_POLICY/public \
+			-I$(top_srcdir)/src/SCE/public \
 			-I$(top_srcdir)/src/common/public \
 			-I$(top_srcdir)/src/CPE/public \
 			-I$(top_srcdir)/lib

src/XCCDF/public/xccdf_session.h
@@ -74,6 +74,11 @@ struct xccdf_session {
 		xccdf_policy_engine_eval_fn user_eval_fn;///< Custom OVAL engine callback
 		char *product_cpe;			///< CPE of scanner product.
 	} oval;
+#ifdef ENABLE_SCE
+	struct {
+		struct sce_parameters *parameters;	///< Script Check Engine parameters
+	} sce;
+#endif
 	char *user_cpe;					///< Path to CPE dictionary required by user
 	oscap_document_type_t doc_type;		///< Document type of the session file (see filename member) used.
 	bool validate;				///< False value indicates to skip any XSD validation.
@@ -199,6 +204,14 @@ int xccdf_session_load_cpe(struct xccdf_session *session);
  * @memberof xccdf_session
  */
 int xccdf_session_load_oval(struct xccdf_session *session);
+
+/**
+ * Load Script Check Engine (if available) to the XCCDF session.
+ * @memberof xccdf_session
+ * @param session XCCDF Session
+ * @returns zero on success
+ */
+int xccdf_session_load_sce(struct xccdf_session *session);
 
 /**
  * Get policy_model of the session. The @ref xccdf_session_load_xccdf shall be run

utils/oscap-xccdf.c
@@ -391,15 +391,11 @@ int app_evaluate_xccdf(const struct oscap_action *action)
 
 	struct xccdf_policy *policy = NULL;
 	struct xccdf_policy_model *policy_model = NULL;
-	char * xccdf_pathcopy = NULL;
 	char* f_results = NULL;
 
 	char** oval_result_files = NULL;
 	int result = OSCAP_ERROR;
 	float base_score = 0;
-#ifdef ENABLE_SCE
-	struct sce_parameters* sce_parameters = 0;
-#endif
 	int priority = LOG_NOTICE;
 
 	/* syslog message */
@@ -453,16 +449,8 @@ int app_evaluate_xccdf(const struct oscap_action *action)
 	if (xccdf_session_load_oval(session) != 0)
 		goto cleanup;
 
-	// register sce system
-	xccdf_pathcopy =  strdup(session->xccdf.file);
-
-#ifdef ENABLE_SCE
-	sce_parameters = sce_parameters_new();
-	sce_parameters_set_xccdf_directory(sce_parameters, dirname(xccdf_pathcopy));
-	sce_parameters_allocate_session(sce_parameters);
-
-	xccdf_policy_model_register_engine_sce(policy_model, sce_parameters);
-#endif
+	if (xccdf_session_load_sce(session) != 0)
+		goto cleanup;
 
 	/* Perform evaluation */
 	struct xccdf_result *ritem = xccdf_policy_evaluate(policy);
@@ -568,7 +556,7 @@ int app_evaluate_xccdf(const struct oscap_action *action)
 #ifdef ENABLE_SCE
 	/* Export SCE results */
 	if (action->sce_results == true) {
-		struct sce_check_result_iterator * it = sce_session_get_check_results(sce_parameters_get_session(sce_parameters));
+		struct sce_check_result_iterator * it = sce_session_get_check_results(sce_parameters_get_session(session->sce.parameters));
 
 		while(sce_check_result_iterator_has_more(it))
 		{
@@ -725,11 +713,6 @@ cleanup:
 
 	/* syslog message */
 	syslog(priority, "Evaluation finnished. Return code: %d, Base score %f.", result, base_score);
-
-#ifdef ENABLE_SCE
-	sce_parameters_free(sce_parameters);
-#endif
-	free(xccdf_pathcopy);
 
 	if (oval_result_files)
 	{

commit 49223a7467c5a190cec25b19083e3a0096bc356c

rhbz#953069: Allocate SCE session before scanning
This regression was introduced by
d4ab011b3e25258b5f344429694e767a6ad7d9d8

src/XCCDF/xccdf_session.c
@@ -749,6 +749,7 @@ int xccdf_session_load_sce(struct xccdf_session *session)
 	session->sce.parameters = sce_parameters_new();
 	xccdf_pathcopy = oscap_strdup(session->xccdf.file);
 	sce_parameters_set_xccdf_directory(session->sce.parameters, dirname(xccdf_pathcopy));
+	sce_parameters_allocate_session(session->sce_parameters);
 	oscap_free(xccdf_pathcopy);
 	return !xccdf_policy_model_register_engine_sce(session->xccdf.policy_model, session->sce.parameters);
 #else