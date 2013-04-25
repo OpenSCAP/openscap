OpenSCAP/openscap

commit 49223a7467c5a190cec25b19083e3a0096bc356c

rhbz#953069: Allocate SCE session before scanning
This regression was introduced by
d4ab011b3e25258b5f344429694e767a6ad7d9d8

src/XCCDF/xccdf_session.c
@@ -749,6 +749,7 @@ int xccdf_session_load_sce(struct xccdf_session *session)
 	session->sce.parameters = sce_parameters_new();
 	xccdf_pathcopy = oscap_strdup(session->xccdf.file);
 	sce_parameters_set_xccdf_directory(session->sce.parameters, dirname(xccdf_pathcopy));
+	sce_parameters_allocate_session(session->sce_parameters);
 	oscap_free(xccdf_pathcopy);
 	return !xccdf_policy_model_register_engine_sce(session->xccdf.policy_model, session->sce.parameters);
 #else

commit 4f3b152cd3ceab90ede2b59bff231c320b9a82a7

Fixed a typo that caused broken build with --enable-sce
Build was broken in 49223a7467c5a190cec25b19083e3a0096bc356c

src/XCCDF/xccdf_session.c
@@ -749,7 +749,7 @@ int xccdf_session_load_sce(struct xccdf_session *session)
 	session->sce.parameters = sce_parameters_new();
 	xccdf_pathcopy = oscap_strdup(session->xccdf.file);
 	sce_parameters_set_xccdf_directory(session->sce.parameters, dirname(xccdf_pathcopy));
-	sce_parameters_allocate_session(session->sce_parameters);
+	sce_parameters_allocate_session(session->sce.parameters);
 	oscap_free(xccdf_pathcopy);
 	return !xccdf_policy_model_register_engine_sce(session->xccdf.policy_model, session->sce.parameters);
 #else