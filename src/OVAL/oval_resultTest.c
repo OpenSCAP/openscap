239d1a23ff4c5bb643eb7abe12ca01311feaf803
Simon Lukasik <slukasik@redhat.com>
2013-07-01 13:44:12 +0200

    Include collectVarRefs_impl.h because of oval_ste_collect_var_refs
    Addressing:
    oval_resultTest.c: In function '_oval_result_test_initialize_bindings':
    oval_resultTest.c:1508:3: warning: implicit declaration of function 'oval_ste_collect_var_refs'

--- src/OVAL/oval_resultTest.c
+++ src/OVAL/oval_resultTest.c
@@ -47,6 +47,7 @@
 #include "oval_results_impl.h"
 #include "oval_collection_impl.h"
 #include "oval_string_map_impl.h"
+#include "collectVarRefs_impl.h"
 #include "public/oval_types.h"
 #include "common/util.h"
 #include "common/debug_priv.h"
