efedf5fb2575ec4f24e74617445e5095756a3494
Simon Lukasik <slukasik@redhat.com>
2013-07-02 09:08:27 +0200

    Refactor: Extract iterator's implementation to generic module.

--- src/OVAL/oval_resultDefinitionIterator.c
+++ src/OVAL/oval_resultDefinitionIterator.c
@@ -30,74 +30,33 @@
  * multiple result definition elements that are distinguished only by
  * variable_instance attribute.
  *
- * This module implements only sub set of iterator functionality for iterating
- * through map/list structure. Limits: It supposes that list (slave_col) is
- * never empty.
  */
 
 #ifdef HAVE_CONFIG_H
 #include <config.h>
 #endif
 
-#include "common/alloc.h"
 #include "public/oval_results.h"
 #include "oval_results_impl.h"
+#include "adt/oval_smc_iterator_impl.h"
 #include "adt/oval_string_map_impl.h"
-
-struct oval_result_definition_iterator {
-	struct oval_collection *master_col;		///< list of lists
-	struct oval_iterator *master_it;		///< iterating through master_col
-	struct oval_iterator *slave_it;			///< iterating through a single item of master_col
-};
 
 struct oval_result_definition_iterator *oval_result_definition_iterator_new(struct oval_string_map *mapping)
 {
-	if (mapping == NULL)
-		return NULL;
-
-	struct oval_result_definition_iterator *it = oscap_calloc(1, sizeof(struct oval_result_definition_iterator));
-
-	it->master_col = oval_string_map_collect_values(mapping, NULL);
-	it->master_it = oval_collection_iterator(it->master_col);
-	it->slave_it = NULL;
-	return it;
+	return (struct oval_result_definition_iterator *) oval_smc_iterator_new(mapping);
 }
 
 void oval_result_definition_iterator_free(struct oval_result_definition_iterator *it)
 {
-	if (it == NULL)
-		return;
-	oval_collection_free(it->master_col);
-	oval_collection_iterator_free(it->master_it);
-	oval_collection_iterator_free(it->slave_it);
-	oscap_free(it);
+	return oval_smc_iterator_free((struct oval_smc_iterator *) it);
 }
 
 bool oval_result_definition_iterator_has_more(struct oval_result_definition_iterator *it)
 {
-	return it != NULL &&
-		(oval_collection_iterator_has_more(it->master_it) ||
-		(it->slave_it &&
-			oval_collection_iterator_has_more(it->slave_it)));
+	return oval_smc_iterator_has_more((struct oval_smc_iterator *) it);
 }
 
 struct oval_result_definition *oval_result_definition_iterator_next(struct oval_result_definition_iterator *it)
 {
-	if (it == NULL)
-		return NULL;
-	if (it->slave_it == NULL || !oval_collection_iterator_has_more(it->slave_it)) {
-		// Rewind master and find new slave
-		if (it->slave_it != NULL) {
-			oval_collection_iterator_free(it->slave_it);
-			it->slave_it = NULL;
-		}
-		if (!oval_collection_iterator_has_more(it->master_it)) {
-			assert(false);
-			return NULL;
-		}
-		struct oval_collection *slave_col = (struct oval_collection *) oval_collection_iterator_next(it->master_it);
-		it->slave_it = oval_collection_iterator(slave_col);
-	}
-	assert(oval_collection_iterator_has_more(it->slave_it));
-	return (struct oval_result_definition *) oval_collection_iterator_next(it->slave_it);
+	return (struct oval_result_definition *) oval_smc_iterator_next((struct oval_smc_iterator *) it);
 }
