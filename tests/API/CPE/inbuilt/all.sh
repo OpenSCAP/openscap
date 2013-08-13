f82faae3fdce3f919d1b1728c341b31e8d827b5f
Simon Lukasik <slukasik@redhat.com>
2013-08-13 15:35:14 +0200

    tests: trac#333: Make sure that validate-xml behaves as validate

--- tests/API/CPE/inbuilt/all.sh
+++ tests/API/CPE/inbuilt/all.sh
@@ -48,6 +48,8 @@ function validate_inbuilt_dict(){
 	[ -f $path ]
 	$OSCAP cpe validate $path 2>&1 > $output
 	[ -f $output ]; [ ! -s $output ]
+	$OSCAP cpe validate-xml $path 2>&1 > $output
+	[ -f $output ]; [ ! -s $output ]
 	rm $output
 }
 
