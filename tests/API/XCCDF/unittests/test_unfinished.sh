411e5b11604b5dcfa35ea8584db91146bcbe4c9f
Simon Lukasik <slukasik@redhat.com>
2013-10-02 11:06:51 +0200

    tests: workaround trac#346, disable test on RPM systems with older libxml2
    While this is not exactly friendly for our non-rpm friends,
    we welcome patches which will extend the workaround or
    address the trac#346.

--- tests/API/XCCDF/unittests/test_unfinished.sh
+++ tests/API/XCCDF/unittests/test_unfinished.sh
@@ -3,6 +3,16 @@
 set -e
 set -o pipefail
 
+if [ -f /bin/rpm ] && /bin/rpm -q libxml2; then
+	# workaround trac#346: skip this test
+	version="`/bin/rpm -q --qf %{VERSION} libxml2`";
+	if echo $version | \
+			awk -F. '{print ($1 == 2 && ($2 < 7 || ($2 == 7 && $3 <= 6))) ? "true" : "false"}' | \
+			grep true; then
+		exit 0;
+	fi
+fi
+
 name=$(basename $0 .sh)
 stderr=$(mktemp -t ${name}.err.XXXXXX)
 echo "Stderr file = $stderr"
