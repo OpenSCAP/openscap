cf9d0b5ab11fcdfde3b13c033baeae8dee2f60ae
Simon Lukasik <slukasik@redhat.com>
2013-10-02 13:54:25 +0200

    tests: rhbz#1013011: Assert for export ind-def:pid/@xsi:nil

--- tests/API/OVAL/unittests/all.sh
+++ tests/API/OVAL/unittests/all.sh
@@ -2,6 +2,15 @@
 
 . $srcdir/../../../test_common.sh
 
+assert_exists() {
+        real_cnt="$($XPATH $result 'count('"$2"')')"
+        if [ "$real_cnt" != "$1" ]; then
+                echo "Failed: expected count: $1, real count: $real_cnt, xpath: '$2'"
+                return 1
+        fi
+}
+export -f assert_exists
+
 test_init test_api_oval_unittests.log
 test_run "empty filename(pattern match)" $srcdir/test_empty_filename.sh
 test_run "deprecated definition" $srcdir/test_deprecated_def.sh
@@ -9,5 +18,6 @@ test_run "applicability_check element" $srcdir/test_applicability_check.sh
 test_run "count function"  $srcdir/test_count_function.sh
 test_run "partial matches"  $srcdir/test_item_not_exist.sh
 test_run "empty variable evaluation" $srcdir/test_oval_empty_variable_evaluation.sh
+test_run "export of xsi:nil on pid entity of env.var.58_object" $srcdir/test_xsinil_envv58_pid.sh
 test_run "anyxml element" $srcdir/test_anyxml.sh
 test_exit
---
+++ tests/API/OVAL/unittests/test_xsinil_envv58_pid.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e -o pipefail
+
+name=$(basename $0 .sh)
+result=$(mktemp ${name}.out.XXXXXX)
+echo "Result file: $result"
+
+echo "Evaluating content."
+$OSCAP oval eval --results $result $srcdir/${name}.oval.xml
+echo "Validating results."
+$OSCAP oval validate-xml --results --schematron $result
+echo "Testing results values."
+assert_exists 1 '/oval_results'
+assert_exists 1 '/oval_results/oval_definitions'
+assert_exists 1 '/oval_results/oval_definitions/objects'
+assert_exists 1 '/oval_results/oval_definitions/objects/*'
+assert_exists 1 '/oval_results/oval_definitions/objects/*/ind-def:pid'
+assert_exists 0 '/oval_results/oval_definitions/objects/*/ind-def:pid/*'
+assert_exists 2 '/oval_results/oval_definitions/objects/*/ind-def:pid/@*'
+assert_exists 1 '/oval_results/oval_definitions/objects/*/ind-def:pid[@datatype="int"]'
+assert_exists 1 '/oval_results/oval_definitions/objects/*/ind-def:pid[@xsi:nil="true"]'
+
+rm $result
