0f425fbae4d86a36531de08170271834e523fb01
Simon Lukasik <slukasik@redhat.com>
2013-12-04 10:44:12 +0100

    tests: Make sure that 'temp != temPX' for case insensitive equals

--- tests/API/OVAL/unittests/all.sh
+++ tests/API/OVAL/unittests/all.sh
@@ -13,5 +13,6 @@ test_run "export of xsi:nil on pid entity of env.var.58_object" $srcdir/test_xsi
 test_run "Import content without proper namespaces" $srcdir/test_xmlns_missing.sh
 test_run "evr_string comparison is superior to rpmvercmp" $srcdir/test_evr_string_comparison.sh
 test_run "evr_string comparison regards missing epoch in content" $srcdir/test_evr_string_missing_epoch.sh
+test_run "insensitive_equals on properties" $srcdir/test_envvar_insensitive_equals.sh
 test_run "anyxml element" $srcdir/test_anyxml.sh
 test_exit
