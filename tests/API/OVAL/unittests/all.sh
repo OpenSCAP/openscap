963506730cce99b7072d35cf81ce8b5af0fe96ce
Simon Lukasik <slukasik@redhat.com>
2013-12-05 17:53:29 +0100

    tests: Assert for 'superset of' operation on ipv4_address type
    These tests are inspirated by their ipv6 equivalent.

--- tests/API/OVAL/unittests/all.sh
+++ tests/API/OVAL/unittests/all.sh
@@ -15,5 +15,6 @@ test_run "evr_string comparison is superior to rpmvercmp" $srcdir/test_evr_strin
 test_run "evr_string comparison regards missing epoch in content" $srcdir/test_evr_string_missing_epoch.sh
 test_run "insensitive_equals on properties" $srcdir/test_envvar_insensitive_equals.sh
 test_run "superset of" $srcdir/test_super_set_of.sh
+test_run "ipv4_address: 'superset of' operation" $srcdir/test_ipv4_super_set_of.sh
 test_run "anyxml element" $srcdir/test_anyxml.sh
 test_exit
