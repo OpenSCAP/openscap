21373456a89236def3e574e66c0cb0a694f8ab03
Martin Preisler <mpreisle@redhat.com>
2013-12-12 17:14:40 +0100

    Added a test for `oscap xccdf generate custom` with XCCDF 1.1 content

--- tests/API/XCCDF/report/all.sh
+++ tests/API/XCCDF/report/all.sh
@@ -28,11 +28,36 @@ function test_generate_report {
     return 1
 }
 
+function test_generate_report_custom {
+    local INPUT=$srcdir/$1
+    local EXPECTED_CONTENT=$2
+
+    local GENERATED_CONTENT=$($OSCAP xccdf generate custom --stylesheet $OSCAP_XSLT_PATH/xccdf-report.xsl "$INPUT")
+    if [ "$?" != "0" ]; then
+        return 1
+    fi
+
+    echo "$GENERATED_CONTENT" | grep "$EXPECTED_CONTENT"
+    if [ "$?" == "0" ]; then
+        return 0
+    fi
+
+    echo "Generated content does not contain '$EXPECTED_CONTENT'!"
+    echo "Generated content:"
+    echo "$GENERATED_CONTENT"
+
+    return 1
+}
+
 # Testing.
 
 test_init "test_api_xccdf_report.log"
 
 test_run "test_api_xccdf_report_xccdf11" test_generate_report results-xccdf11.xml xccdf_moc.elpmaxe.www_rule_1
 test_run "test_api_xccdf_report_xccdf12" test_generate_report results-xccdf12.xml xccdf_moc.elpmaxe.www_rule_1
+test_run "test_api_xccdf_report_custom_xccdf11" test_generate_report_custom results-xccdf11.xml xccdf_moc.elpmaxe.www_rule_1
+# This does not work because the xccdf-report.xsl is made for XCCDF 1.1, the namespace workaround
+# (see xccdf_ns_xslt_workaround(..) for details) is not used when using custom stylesheets.
+#test_run "test_api_xccdf_report_custom_xccdf12" test_generate_report_custom results-xccdf12.xml xccdf_moc.elpmaxe.www_rule_1
 
 test_exit
