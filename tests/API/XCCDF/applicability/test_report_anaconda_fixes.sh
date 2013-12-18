693c88f985c173a7048ced2747b985f7c089bb9e
Simon Lukasik <slukasik@redhat.com>
2013-12-18 13:09:37 +0100

    [tests] trac#362: Ensure that new files are created with sane permissions

--- tests/API/XCCDF/applicability/test_report_anaconda_fixes.sh
+++ tests/API/XCCDF/applicability/test_report_anaconda_fixes.sh
@@ -38,13 +38,18 @@ grep "$line1" $result
 grep "$line2" $result
 grep -v "$line1" $result | grep -v "$line2" | grep -v "$line3"
 [ "`grep -v "$line1" $result | grep -v "$line2" | sed 's/\W//g'`"x == x ]
-:> $result
+
+# To test permissions of newly created files
+rm $result
+result=./${name}.out
+[ -f $result ] && rm $result
 
 $OSCAP xccdf generate fix --template urn:redhat:anaconda:pre \
 	--cpe $srcdir/cpe-dict.xml \
 	--profile xccdf_moc.elpmaxe.www_profile_1 \
 	--output $result $srcdir/${name}.xccdf.xml 2>&1 > $stderr
 [ -f $stderr ]; [ ! -s $stderr ]; :> $stderr
+stat $result | grep 'Access: (..../-rw.------)'
 grep "$line1" $result
 grep "$line2" $result
 grep "$line3" $result
