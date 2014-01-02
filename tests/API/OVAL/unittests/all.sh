027e977ca7edda8d3415a9e5fbbe04df7724f30a
Simon Lukasik <slukasik@redhat.com>
2014-01-02 14:23:10 +0100

    [tests] trac#366: Asserts for OVAL float comparison

--- tests/API/OVAL/unittests/all.sh
+++ tests/API/OVAL/unittests/all.sh
@@ -13,6 +13,7 @@ test_run "export of xsi:nil on pid entity of env.var.58_object" $srcdir/test_xsi
 test_run "Import content without proper namespaces" $srcdir/test_xmlns_missing.sh
 test_run "evr_string comparison is superior to rpmvercmp" $srcdir/test_evr_string_comparison.sh
 test_run "evr_string comparison regards missing epoch in content" $srcdir/test_evr_string_missing_epoch.sh
+test_run "float comparison" $srcdir/test_float_comparison.sh
 test_run "insensitive_equals on properties" $srcdir/test_envvar_insensitive_equals.sh
 test_run "ipv6_address: 'superset of' operation" $srcdir/test_ipv6_super_set_of.sh
 test_run "ipv6_address: 'subset of' operation" $srcdir/test_ipv6_subset_of.sh
---
+++ tests/API/OVAL/unittests/test_float_comparison.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+set -e -o pipefail
+
+name=$(basename $0 .sh)
+result=$(mktemp ${name}.out.XXXXXX)
+echo "result file: $result"
+stderr=$(mktemp ${name}.err.XXXXXX)
+echo "stderr file: $stderr"
+
+echo "Analysing syschar content."
+$OSCAP oval analyse --results $result $srcdir/$name.oval.xml $srcdir/$name.syschar.xml 2> $stderr
+[ -f $stderr ]; [ ! -s $stderr ]; rm $stderr
+[ -f $result ]
+
+assert_exists 1 '/oval_results'
+assert_exists 1 '/oval_results/generator'
+assert_exists 1 '/oval_results/generator/oval:product_name'
+assert_exists 1 '/oval_results/generator/oval:product_name[text()="cpe:/a:open-scap:oscap"]'
+assert_exists 1 '/oval_results/generator/oval:schema_version'
+assert_exists 1 '/oval_results/generator/oval:schema_version[text()="5.8"]'
+assert_exists 1 '/oval_results/generator/oval:timestamp'
+assert_exists 1 '/oval_results/directives'
+assert_exists 1 '/oval_results/oval_definitions'
+assert_exists 1 '/oval_results/results'
+assert_exists 1 '/oval_results/results/system'
+assert_exists 1 '/oval_results/results/system/definitions'
+assert_exists 6 '/oval_results/results/system/definitions/definition'
+assert_exists 1 '/oval_results/results/system/definitions/definition[@definition_id="oval:x:def:1"]'
+assert_exists 1 '/oval_results/results/system/definitions/definition[@definition_id="oval:x:def:1"][@result="true"]'
+assert_exists 1 '/oval_results/results/system/definitions/definition[@definition_id="oval:x:def:2"]'
+assert_exists 1 '/oval_results/results/system/definitions/definition[@definition_id="oval:x:def:2"][@result="true"]'
+assert_exists 1 '/oval_results/results/system/definitions/definition[@definition_id="oval:x:def:3"]'
+assert_exists 1 '/oval_results/results/system/definitions/definition[@definition_id="oval:x:def:3"][@result="true"]'
+assert_exists 1 '/oval_results/results/system/definitions/definition[@definition_id="oval:x:def:4"]'
+assert_exists 1 '/oval_results/results/system/definitions/definition[@definition_id="oval:x:def:4"][@result="true"]'
+assert_exists 1 '/oval_results/results/system/definitions/definition[@definition_id="oval:x:def:5"]'
+assert_exists 1 '/oval_results/results/system/definitions/definition[@definition_id="oval:x:def:5"][@result="true"]'
+assert_exists 1 '/oval_results/results/system/definitions/definition[@definition_id="oval:x:def:6"]'
+assert_exists 1 '/oval_results/results/system/definitions/definition[@definition_id="oval:x:def:6"][@result="true"]'
+
+rm $result
