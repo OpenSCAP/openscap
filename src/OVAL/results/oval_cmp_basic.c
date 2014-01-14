OpenSCAP/openscap

commit 9601ce1032fca9893b8e04d12f6bbab2c9f86729

Refactor: Extract function: oval_int_cmp.

src/OVAL/results/oval_cmp_basic.c
@@ -38,6 +38,31 @@
 #include "common/debug_priv.h"
 #include "oval_cmp_basic_impl.h"
 
+oval_result_t oval_int_cmp(const int state, const int syschar, oval_operation_t operation)
+{
+	switch (operation) {
+	case OVAL_OPERATION_EQUALS:
+		return state == syschar ? OVAL_RESULT_TRUE : OVAL_RESULT_FALSE;
+	case OVAL_OPERATION_NOT_EQUAL:
+		return state != syschar ? OVAL_RESULT_TRUE : OVAL_RESULT_FALSE;
+	case OVAL_OPERATION_GREATER_THAN:
+		return syschar > state ? OVAL_RESULT_TRUE : OVAL_RESULT_FALSE;
+	case OVAL_OPERATION_GREATER_THAN_OR_EQUAL:
+		return syschar >= state ? OVAL_RESULT_TRUE : OVAL_RESULT_FALSE;
+	case OVAL_OPERATION_LESS_THAN:
+		return syschar < state ? OVAL_RESULT_TRUE : OVAL_RESULT_FALSE;
+	case OVAL_OPERATION_LESS_THAN_OR_EQUAL:
+		return syschar <= state ? OVAL_RESULT_TRUE : OVAL_RESULT_FALSE;
+	case OVAL_OPERATION_BITWISE_AND:
+		return (syschar & state) == state ? OVAL_RESULT_TRUE : OVAL_RESULT_FALSE;
+	case OVAL_OPERATION_BITWISE_OR:
+		return (syschar | state) == state ? OVAL_RESULT_TRUE : OVAL_RESULT_FALSE;
+	default:
+		oscap_seterr(OSCAP_EFAMILY_OVAL, "Invalid type of operation in integer evaluation: %d.", operation);
+	}
+	return OVAL_RESULT_ERROR;
+}
+
 static inline int cmp_float(double a, double b)
 {
 	double relative_err;

src/OVAL/results/oval_cmp_basic_impl.h
@@ -29,6 +29,8 @@
 
 OSCAP_HIDDEN_START;
 
+oval_result_t oval_int_cmp(const int state, const int syschar, oval_operation_t operation);
+
 oval_result_t oval_float_cmp(const double state_val, const double sys_val, oval_operation_t operation);
 
 oval_result_t oval_string_cmp(const char *state, const char *syschar, oval_operation_t operation);

src/OVAL/results/oval_resultTest.c
@@ -226,27 +226,7 @@ static oval_result_t evaluate(char *sys_data, char *state_data, oval_datatype_t 
 			             sys_data, sizeof(intmax_t)*8, strerror(errno));
 			return OVAL_RESULT_ERROR;
 		}
-
-		if (operation == OVAL_OPERATION_EQUALS) {
-			return ((state_val == syschar_val) ? OVAL_RESULT_TRUE : OVAL_RESULT_FALSE);
-		} else if (operation == OVAL_OPERATION_NOT_EQUAL) {
-			return ((state_val != syschar_val) ? OVAL_RESULT_TRUE : OVAL_RESULT_FALSE);
-		} else if (operation == OVAL_OPERATION_GREATER_THAN) {
-			return ((syschar_val > state_val) ? OVAL_RESULT_TRUE : OVAL_RESULT_FALSE);
-		} else if (operation == OVAL_OPERATION_GREATER_THAN_OR_EQUAL) {
-			return ((syschar_val >= state_val) ? OVAL_RESULT_TRUE : OVAL_RESULT_FALSE);
-		} else if (operation == OVAL_OPERATION_LESS_THAN) {
-			return ((syschar_val < state_val) ? OVAL_RESULT_TRUE : OVAL_RESULT_FALSE);
-		} else if (operation == OVAL_OPERATION_LESS_THAN_OR_EQUAL) {
-			return ((syschar_val <= state_val) ? OVAL_RESULT_TRUE : OVAL_RESULT_FALSE);
-		} else if (operation == OVAL_OPERATION_BITWISE_AND) {
-			return (((syschar_val & state_val) == state_val) ? OVAL_RESULT_TRUE : OVAL_RESULT_FALSE);
-		} else if (operation == OVAL_OPERATION_BITWISE_OR) {
-			return (((syschar_val | state_val) == state_val) ? OVAL_RESULT_TRUE : OVAL_RESULT_FALSE);
-		} else {
-			oscap_seterr(OSCAP_EFAMILY_OVAL, "Invalid type of operation in integer evaluation: %d.", operation);
-			return OVAL_RESULT_ERROR;
-		}
+		return oval_int_cmp(state_val, syschar_val, operation);
 	} else if (state_data_type == OVAL_DATATYPE_FLOAT) {
 		double state_val, sys_val;
 

commit 7c451866eb4b1212da2ddab9ff23219c47b8f408

rhbz#1052142: partition_test fails with huge values in *space* system data
Introduced by:
9601ce1032fca9893b8e04d12f6bbab2c9f86729

src/OVAL/results/oval_cmp_basic.c
@@ -51,7 +51,7 @@ oval_result_t oval_boolean_cmp(const bool state, const bool syschar, oval_operat
 	return OVAL_RESULT_ERROR;
 }
 
-oval_result_t oval_int_cmp(const int state, const int syschar, oval_operation_t operation)
+oval_result_t oval_int_cmp(const intmax_t state, const intmax_t syschar, oval_operation_t operation)
 {
 	switch (operation) {
 	case OVAL_OPERATION_EQUALS:

src/OVAL/results/oval_cmp_basic_impl.h
@@ -31,7 +31,7 @@ OSCAP_HIDDEN_START;
 
 oval_result_t oval_boolean_cmp(const bool state, const bool syschar, oval_operation_t operation);
 
-oval_result_t oval_int_cmp(const int state, const int syschar, oval_operation_t operation);
+oval_result_t oval_int_cmp(const intmax_t state, const intmax_t syschar, oval_operation_t operation);
 
 oval_result_t oval_float_cmp(const double state_val, const double sys_val, oval_operation_t operation);
 