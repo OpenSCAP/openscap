034307ef3dd1092cb833383601cba7da8f166254
Simon Lukasik <slukasik@redhat.com>
2014-03-13 12:31:45 +0100

    tests: trac#373: Ensure refina-value gets inherited with the tailoring.

--- tests/API/XCCDF/unittests/test_inherit_selector.sh
+++ tests/API/XCCDF/unittests/test_inherit_selector.sh
@@ -12,8 +12,17 @@ $OSCAP xccdf eval --profile test2 --export-variable $srcdir/${name}.xccdf.xml 2>
 
 echo "Stderr file = $stderr"
 echo "Result file = $result"
-[ -f $stderr ]; [ ! -s $stderr ]; rm $stderr
+[ -f $stderr ]; [ ! -s $stderr ]
 
+assert_exists 1 '//variable[@id="oval:ssg:var:1"]/value[text()="600"]'
+assert_exists 1 '//variable[@id="oval:ssg:var:2"]/value[text()="300"]'
+assert_exists 1 '//variable[@id="oval:ssg:var:3"]/value[text()="600"]'
+:> $result
+
+$OSCAP xccdf eval --profile test2_tailored --export-variables \
+	--tailoring-file $srcdir/${name}.tailoring.xml $srcdir/${name}.xccdf.xml 2> $stderr
+
+[ -f $stderr ]; [ ! -s $stderr ]; rm $stderr
 assert_exists 1 '//variable[@id="oval:ssg:var:1"]/value[text()="600"]'
 assert_exists 1 '//variable[@id="oval:ssg:var:2"]/value[text()="300"]'
 assert_exists 1 '//variable[@id="oval:ssg:var:3"]/value[text()="600"]'
