3e5beef70e00748b721f5c3a9e9ac81329e48997
Simon Lukasik <slukasik@redhat.com>
2014-04-28 15:55:53 +0200

    tests: Take only one of the installed versions for comparison.
    Addressing:
    version=2.7.62.7.6 on multilib systems

--- tests/API/XCCDF/unittests/test_unfinished.sh
+++ tests/API/XCCDF/unittests/test_unfinished.sh
@@ -5,7 +5,7 @@ set -o pipefail
 
 if [ -f /bin/rpm ] && /bin/rpm -q libxml2; then
 	# workaround trac#346: skip this test
-	version="`/bin/rpm -q --qf %{VERSION} libxml2`";
+	version="`/bin/rpm -q --qf '%{VERSION}\n' libxml2 | tail -n 1`";
 	if echo $version | \
 			awk -F. '{print ($1 == 2 && ($2 < 7 || ($2 == 7 && $3 < 7))) ? "true" : "false"}' | \
 			grep true; then
