38f2ea71da60d3a3b6f2b7c2d4fe5aa570711130
Simon Lukasik <slukasik@redhat.com>
2014-04-28 13:36:36 +0200

    tests: Skip test when libxml 2.7.6-14 is around
    Addressing false negative:
    $ echo 2.7.6-14.el6 |  awk -F. '{print ($1 == 2 && ($2 < 7 || ($2 == 7 && $3 <= 6))) ? "true" : "false"}'
    false
    
    Fixed:
    $ echo 2.7.6-14.el6 |  awk -F. '{print ($1 == 2 && ($2 < 7 || ($2 == 7 && $3 < 7))) ? "true" : "false"}'
    true

--- tests/API/XCCDF/unittests/test_unfinished.sh
+++ tests/API/XCCDF/unittests/test_unfinished.sh
@@ -7,7 +7,7 @@ if [ -f /bin/rpm ] && /bin/rpm -q libxml2; then
 	# workaround trac#346: skip this test
 	version="`/bin/rpm -q --qf %{VERSION} libxml2`";
 	if echo $version | \
-			awk -F. '{print ($1 == 2 && ($2 < 7 || ($2 == 7 && $3 <= 6))) ? "true" : "false"}' | \
+			awk -F. '{print ($1 == 2 && ($2 < 7 || ($2 == 7 && $3 < 7))) ? "true" : "false"}' | \
 			grep true; then
 		exit 0;
 	fi
