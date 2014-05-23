c2529f6fdc2016c28a8c27d509276850a111e4df
Martin Preisler <mpreisle@redhat.com>
2014-05-23 13:40:44 +0200

    libdbus code to query unit dbus path by systemd unit name

--- src/OVAL/probes/unix/linux/systemdunitproperty.c
+++ src/OVAL/probes/unix/linux/systemdunitproperty.c
@@ -31,6 +31,81 @@
 #endif
 
 #include <probe-api.h>
+#include <dbus/dbus.h>
+#include "common/debug_priv.h"
+
+static char *get_path_by_unit(DBusConnection *conn, const char *unit)
+{
+	DBusMessage *msg = NULL;
+	DBusPendingCall *pending = NULL;
+	DBusBasicValue path;
+	char *ret = NULL;
+
+	msg = dbus_message_new_method_call(
+		"org.freedesktop.systemd1",
+		"/org/freedesktop/systemd1",
+		"org.freedesktop.systemd1.Manager",
+		// LoadUnit is similar to GetUnit except it will load the unit file
+		// if it hasn't been loaded yet.
+		"LoadUnit"
+	);
+	if (msg == NULL) {
+		dI("Failed to create dbus_message via dbus_message_new_method_call!\n");
+		goto cleanup;
+	}
+
+	DBusMessageIter args;
+
+	dbus_message_iter_init_append(msg, &args);
+	if (!dbus_message_iter_append_basic(&args, DBUS_TYPE_STRING, &unit)) {
+		dI("Failed to append unit '%s' string parameter to dbus message!\n", unit);
+		goto cleanup;
+	}
+
+	if (!dbus_connection_send_with_reply(conn, msg, &pending, -1)) {
+		dI("Failed to send message via dbus!\n");
+		goto cleanup;
+	}
+	if (pending == NULL) {
+		dI("Invalid dbus pending call!\n");
+		goto cleanup;
+	}
+
+	dbus_connection_flush(conn);
+	dbus_message_unref(msg); msg = NULL;
+
+	dbus_pending_call_block(pending);
+	msg = dbus_pending_call_steal_reply(pending);
+	if (msg == NULL) {
+		dI("Failed to steal dbus pending call reply.\n");
+		goto cleanup;
+	}
+	dbus_pending_call_unref(pending); pending = NULL;
+
+	if (!dbus_message_iter_init(msg, &args)) {
+		dI("Failed to initialize iterator over received dbus message.\n");
+		goto cleanup;
+	}
+
+	if (dbus_message_iter_get_arg_type(&args) != DBUS_TYPE_OBJECT_PATH) {
+		dI("Expected string argument in reply. Instead received: %s.\n", dbus_message_type_to_string(dbus_message_iter_get_arg_type(&args)));
+		goto cleanup;
+	}
+
+	dbus_message_iter_get_basic(&args, &path);
+	ret = oscap_strdup(path.str);
+	dbus_message_unref(msg); msg = NULL;
+
+cleanup:
+	if (pending != NULL)
+		dbus_pending_call_unref(pending);
+
+	if (msg != NULL)
+		dbus_message_unref(msg);
+
+	return ret;
+}
+
 
 int probe_main(probe_ctx *ctx, void *probe_arg)
 {
