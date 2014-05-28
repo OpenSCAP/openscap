d0d0fcb2a33a9964f10dc98ec37d1eb862e9738e
Martin Preisler <mpreisle@redhat.com>
2014-05-28 19:19:33 +0200

    Getting all properties is only required in the unit property probe, cleanup

--- src/OVAL/probes/unix/linux/systemdshared.h
+++ src/OVAL/probes/unix/linux/systemdshared.h
@@ -194,7 +194,6 @@ static char *get_property_by_unit_path(DBusConnection *conn, const char *unit_pa
 {
 	DBusMessage *msg = NULL;
 	DBusPendingCall *pending = NULL;
-	DBusBasicValue value;
 	char *ret = NULL;
 
 	msg = dbus_message_new_method_call(
@@ -257,107 +256,6 @@ static char *get_property_by_unit_path(DBusConnection *conn, const char *unit_pa
 	ret = dbus_value_to_string(&value_iter);
 
 	dbus_message_unref(msg); msg = NULL;
-
-cleanup:
-	if (pending != NULL)
-		dbus_pending_call_unref(pending);
-
-	if (msg != NULL)
-		dbus_message_unref(msg);
-
-	return ret;
-}
-
-static int get_all_properties_by_unit_path(DBusConnection *conn, const char *unit_path)
-{
-	int ret = 1;
-	DBusMessage *msg = NULL;
-	DBusPendingCall *pending = NULL;
-
-	msg = dbus_message_new_method_call(
-		"org.freedesktop.systemd1",
-		unit_path,
-		"org.freedesktop.DBus.Properties",
-		"GetAll"
-	);
-	if (msg == NULL) {
-		dI("Failed to create dbus_message via dbus_message_new_method_call!\n");
-		goto cleanup;
-	}
-
-	DBusMessageIter args, property_iter;
-
-	const char *interface = "org.freedesktop.systemd1.Unit";
-
-	dbus_message_iter_init_append(msg, &args);
-	if (!dbus_message_iter_append_basic(&args, DBUS_TYPE_STRING, &interface)) {
-		dI("Failed to append interface '%s' string parameter to dbus message!\n", interface);
-		goto cleanup;
-	}
-
-	if (!dbus_connection_send_with_reply(conn, msg, &pending, -1)) {
-		dI("Failed to send message via dbus!\n");
-		goto cleanup;
-	}
-	if (pending == NULL) {
-		dI("Invalid dbus pending call!\n");
-		goto cleanup;
-	}
-
-	dbus_connection_flush(conn);
-	dbus_message_unref(msg); msg = NULL;
-
-	dbus_pending_call_block(pending);
-	msg = dbus_pending_call_steal_reply(pending);
-	if (msg == NULL) {
-		dI("Failed to steal dbus pending call reply.\n");
-		goto cleanup;
-	}
-	dbus_pending_call_unref(pending); pending = NULL;
-
-	if (!dbus_message_iter_init(msg, &args)) {
-		dI("Failed to initialize iterator over received dbus message.\n");
-		goto cleanup;
-	}
-
-	if (dbus_message_iter_get_arg_type(&args) != DBUS_TYPE_ARRAY && dbus_message_iter_get_element_type(&args) != DBUS_TYPE_DICT_ENTRY) {
-		dI("Expected array of dict_entry argument in reply. Instead received: %s.\n", dbus_message_type_to_string(dbus_message_iter_get_arg_type(&args)));
-		goto cleanup;
-	}
-
-	dbus_message_iter_recurse(&args, &property_iter);
-	do {
-		DBusMessageIter dict_entry, value_variant;
-		dbus_message_iter_recurse(&property_iter, &dict_entry);
-
-		if (dbus_message_iter_get_arg_type(&dict_entry) != DBUS_TYPE_STRING) {
-			dI("Expected string as key in dict_entry. Instead received: %s.\n", dbus_message_type_to_string(dbus_message_iter_get_arg_type(&dict_entry)));
-			goto cleanup;
-		}
-
-		DBusBasicValue value;
-		dbus_message_iter_get_basic(&dict_entry, &value);
-		char *property_name = oscap_strdup(value.str);
-
-		dbus_message_iter_next(&dict_entry);
-
-		if (dbus_message_iter_get_arg_type(&dict_entry) != DBUS_TYPE_VARIANT) {
-			dI("Expected variant as value in dict_entry. Instead received: %s.\n", dbus_message_type_to_string(dbus_message_iter_get_arg_type(&dict_entry)));
-			goto cleanup;
-		}
-
-		dbus_message_iter_recurse(&dict_entry, &value_variant);
-
-		char *property_value = dbus_value_to_string(&value_variant);
-		printf("property '%s' = '%s'\n", property_name, property_value);
-		oscap_free(property_value);
-
-		oscap_free(property_name);
-	}
-	while (dbus_message_iter_next(&property_iter));
-
-	dbus_message_unref(msg); msg = NULL;
-	ret = 0;
 
 cleanup:
 	if (pending != NULL)
