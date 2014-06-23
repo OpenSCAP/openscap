faea6089329663aea64a03db9756feba72db05d4
Martin Preisler <mpreisle@redhat.com>
2014-06-23 18:18:07 +0200

    Added get_all_systemd_units to the shared systemd tests mini API
    This functionality is required for pattern matching across systemd
    units.

--- src/OVAL/probes/unix/linux/systemdshared.h
+++ src/OVAL/probes/unix/linux/systemdshared.h
@@ -105,6 +105,96 @@ cleanup:
 	return ret;
 }
 
+static int get_all_systemd_units(DBusConnection* conn)
+{
+	DBusMessage *msg = NULL;
+	DBusPendingCall *pending = NULL;
+	char ret = 1;
+
+	msg = dbus_message_new_method_call(
+		"org.freedesktop.systemd1",
+		"/org/freedesktop/systemd1",
+		"org.freedesktop.systemd1.Manager",
+		"ListUnits"
+	);
+	if (msg == NULL) {
+		dI("Failed to create dbus_message via dbus_message_new_method_call!\n");
+		goto cleanup;
+	}
+
+	DBusMessageIter args, unit_iter;
+
+	// the args should be empty for this call
+	dbus_message_iter_init_append(msg, &args);
+
+	if (!dbus_connection_send_with_reply(conn, msg, &pending, -1)) {
+		dI("Failed to send message via dbus!\n");
+		goto cleanup;
+	}
+	if (pending == NULL) {
+		dI("Invalid dbus pending call!\n");
+		goto cleanup;
+	}
+
+	dbus_connection_flush(conn);
+	dbus_message_unref(msg); msg = NULL;
+
+	dbus_pending_call_block(pending);
+	msg = dbus_pending_call_steal_reply(pending);
+	if (msg == NULL) {
+		dI("Failed to steal dbus pending call reply.\n");
+		goto cleanup;
+	}
+	dbus_pending_call_unref(pending); pending = NULL;
+
+	if (!dbus_message_iter_init(msg, &args)) {
+		dI("Failed to initialize iterator over received dbus message.\n");
+		goto cleanup;
+	}
+
+	if (dbus_message_iter_get_arg_type(&args) != DBUS_TYPE_ARRAY) {
+		dI("Expected array of structs in reply. Instead received: %s.\n", dbus_message_type_to_string(dbus_message_iter_get_arg_type(&args)));
+		goto cleanup;
+	}
+
+	dbus_message_iter_recurse(&args, &unit_iter);
+	do {
+		if (dbus_message_iter_get_arg_type(&unit_iter) != DBUS_TYPE_STRUCT) {
+			dI("Expected unit struct as elements in returned array. Instead received: %s.\n", dbus_message_type_to_string(dbus_message_iter_get_arg_type(&unit_iter)));
+			goto cleanup;
+		}
+
+		DBusMessageIter unit_name;
+		dbus_message_iter_recurse(&unit_iter, &unit_name);
+
+		if (dbus_message_iter_get_arg_type(&unit_name) != DBUS_TYPE_STRING) {
+			dI("Expected string as the first element in the unit struct. Instead received: %s.\n", dbus_message_type_to_string(dbus_message_iter_get_arg_type(&unit_name)));
+			goto cleanup;
+		}
+
+		DBusBasicValue value;
+		dbus_message_iter_get_basic(&unit_name, &value);
+		char *unit_name_s = oscap_strdup(value.str);
+		printf("%s\n", unit_name_s);
+
+		oscap_free(unit_name_s);
+	}
+	while (dbus_message_iter_next(&unit_iter));
+
+	dbus_message_unref(msg); msg = NULL;
+
+	ret = 0;
+
+cleanup:
+	if (pending != NULL)
+		dbus_pending_call_unref(pending);
+
+	if (msg != NULL)
+		dbus_message_unref(msg);
+
+	return ret;
+}
+
 static char *dbus_value_to_string(DBusMessageIter *iter)
 {
 	const int arg_type = dbus_message_iter_get_arg_type(iter);
