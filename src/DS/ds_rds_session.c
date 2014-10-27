3f3fa7aebb896134a27aec3fe2e59278b8b4aa9d
Šimon Lukašík <slukasik@redhat.com>
2014-10-27 09:57:17 +0100

    Introduce internal cache to ds_rds_session.
    This follows the very same patter as ds_sds_session.

--- src/DS/ds_rds_session.c
+++ src/DS/ds_rds_session.c
@@ -31,6 +31,7 @@
 #include "common/public/oscap.h"
 #include "common/util.h"
 #include "ds_rds_session.h"
+#include "ds_rds_session_priv.h"
 #include "rds_index_priv.h"
 #include "source/oscap_source_priv.h"
 #include "source/public/oscap_source.h"
@@ -39,6 +40,7 @@
 struct ds_rds_session {
 	struct oscap_source *source;            ///< Result DataStream raw representation
 	struct rds_index *index;                ///< Result DataStream index
+	struct oscap_htable *component_sources; ///< oscap_sources for parsed contents (arf:content)
 };
 
 struct ds_rds_session *ds_rds_session_new_from_source(struct oscap_source *source)
@@ -50,6 +52,7 @@ struct ds_rds_session *ds_rds_session_new_from_source(struct oscap_source *sourc
 	}
 	struct ds_rds_session *rds_session = (struct ds_rds_session *) oscap_calloc(1, sizeof(struct ds_rds_session));
 	rds_session->source = source;
+	rds_session->component_sources = oscap_htable_new();
 	return rds_session;
 }
 
@@ -57,6 +60,7 @@ void ds_rds_session_free(struct ds_rds_session *rds_session)
 {
 	if (rds_session != NULL) {
 		rds_index_free(rds_session->index);
+		oscap_htable_free(rds_session->component_sources, (oscap_destruct_func) oscap_source_free);
 		oscap_free(rds_session);
 	}
 }
@@ -72,6 +76,16 @@ struct rds_index *ds_rds_session_get_rds_idx(struct ds_rds_session *session)
 		xmlFreeTextReader(reader);
 	}
 	return session->index;
+}
+
+int ds_rds_session_register_component_source(struct ds_rds_session *session, const char *content_id, struct oscap_source *component)
+{
+	if (!oscap_htable_add(session->component_sources, content_id, component)) {
+		oscap_seterr(OSCAP_EFAMILY_OSCAP, "Content '%s' has already been register with Result DataStream session: %s",
+				content_id, oscap_source_readable_origin(session->source));
+		return -1;
+	}
+	return 0;
 }
 
 char *ds_rds_session_get_html_report(struct ds_rds_session *rds_session)
