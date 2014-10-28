6877a33d9074984244211211e042d4ef95edf346
Šimon Lukašík <slukasik@redhat.com>
2014-10-28 16:23:30 +0100

    Deprecate: ds_rds_decompose.

--- src/DS/public/scap_ds.h
+++ src/DS/public/scap_ds.h
@@ -126,7 +126,11 @@ int ds_sds_compose_from_xccdf(const char* xccdf_file, const char* target_datastr
  */
 int ds_sds_compose_add_component(const char *target_datastream, const char *datastream_id, const char *new_component, bool extended);
 
-int ds_rds_decompose(const char* input_file, const char* report_id, const char* request_id, const char* target_dir);
+/**
+ * @deprecated This function has been deprecated. Make a use of ds_rds_session
+ * instread. This function may be dropped from later versions of the library.
+ */
+OSCAP_DEPRECATED(int ds_rds_decompose(const char* input_file, const char* report_id, const char* request_id, const char* target_dir));
 
 /**
  * @brief takes given source data stream and XCCDF result file and makes a result data stream
