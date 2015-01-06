f33a34f29b33ab2dac66f46655e99c62f2fbdbf1
Šimon Lukašík <slukasik@redhat.com>
2015-01-06 17:45:37 +0100

    trac#434: Keep the relative path canonical
    Do not create things like: "./oval.xml"
    
    Addressing:
    Unable to parse XML at: './ssg-fedora-cpe-oval.xml' [oscap_source.c:187]
    Can't import OVAL definition model './ssg-fedora-cpe-oval.xml' for CPE applicability checking [cpe_session.c:108]

--- src/XCCDF_POLICY/xccdf_policy.c
+++ src/XCCDF_POLICY/xccdf_policy.c
@@ -773,7 +773,13 @@ static char *_cpe_get_oval_href(struct cpe_dict_model *dict, struct cpe_lang_mod
 		// we need to strdup because dirname potentially alters the string
 		origin_file = oscap_strdup(origin_file_c ? origin_file_c : "");
 		const char* prefix_dirname = dirname(origin_file);
-		oval_href = oscap_sprintf("%s/%s", prefix_dirname, oval_relative_href);
+		if (oscap_streq(prefix_dirname, ".")) {
+			// The path is relative. Do not overide it.
+			// Chances are that ds_sds_session expects the very same href
+			oval_href = oscap_strdup(oval_relative_href);
+		} else {
+			oval_href = oscap_sprintf("%s/%s", prefix_dirname, oval_relative_href);
+		}
 		oscap_free(origin_file);
 	}
 	return oval_href;
