404e2e3aec451ca5958591a81af12dd9e96a6133
Šimon Lukašík <slukasik@redhat.com>
2015-02-16 11:00:14 +0100

    declaration of ‘time’ shadows a global declaration
    Fixes #42.
    
    Addressing:
    result.c: In function ‘xccdf_rule_result_override’:
    result.c:1157: warning: declaration of ‘time’ shadows a global declaration
    /usr/include/time.h:186: warning: shadowed declaration is here

--- src/XCCDF/result.c
+++ src/XCCDF/result.c
@@ -1154,12 +1154,12 @@ xmlNode *xccdf_rule_result_to_dom(struct xccdf_rule_result *result, xmlDoc *doc,
 	return result_node;
 }
 
-bool xccdf_rule_result_override(struct xccdf_rule_result *rule_result, xccdf_test_result_type_t new_result, const char *time, const char *authority, struct oscap_text *remark)
+bool xccdf_rule_result_override(struct xccdf_rule_result *rule_result, xccdf_test_result_type_t new_result, const char *waiver_time, const char *authority, struct oscap_text *remark)
 {
 	struct xccdf_override *o= xccdf_override_new();
 	xccdf_override_set_old_result(o, xccdf_rule_result_get_result(rule_result));
 	xccdf_override_set_new_result(o, new_result);
-	xccdf_override_set_time(o, time);
+	xccdf_override_set_time(o, waiver_time);
 	xccdf_override_set_authority(o, authority);
 	xccdf_override_set_remark(o, remark);
 	if (!xccdf_rule_result_add_override(rule_result, o)) {
