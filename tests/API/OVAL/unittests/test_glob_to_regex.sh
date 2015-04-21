ba662809a5e253328b0aee3c20d9dbc402f4146c
Jan Černý <jcerny@redhat.com>
2015-04-21 15:48:10 +0200

    Removed overabundant "exit 1" - replaced by assert_exist.

--- tests/API/OVAL/unittests/test_glob_to_regex.sh
+++ tests/API/OVAL/unittests/test_glob_to_regex.sh
@@ -5,7 +5,8 @@ result=`mktemp`
 set -e
 set -o pipefail
 
-$OSCAP oval eval --results $result $srcdir/test_glob_to_regex.xml || exit 1
+$OSCAP oval eval --results $result $srcdir/test_glob_to_regex.xml
 
+assert_exists 2 '/oval_results/results/system/definitions/definition[@result="true"]'
 rm $result
 
