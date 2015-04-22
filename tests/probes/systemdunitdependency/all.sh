e2f19f8827633d72db6a59b436000129b94c6ddd
Jan Černý <jcerny@redhat.com>
2015-04-22 10:12:39 +0200

    Test oscap oval validate on OVAL 5.11 content.
    We need to test `oscap oval validate`
    and `oscap oval validate --schematron` on OVAL 5.11 content
    to avoid mistakes in OVAL 5.11 schematron.

--- tests/probes/systemdunitdependency/all.sh
+++ tests/probes/systemdunitdependency/all.sh
@@ -4,4 +4,5 @@
 
 test_init "test_probes_systemdunitdependency.log"
 test_run "systemdunitdependency general functionality" $srcdir/test_probes_systemdunitdependency.sh
+test_run "OVAL 5.11 validation" $srcdir/test_validation.sh
 test_exit
