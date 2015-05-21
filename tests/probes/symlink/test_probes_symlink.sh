576ca1d11775beeaa14350526282b9e456c81634
Jan Černý <jcerny@redhat.com>
2015-05-21 10:38:46 +0200

    Improved test fro symlink probe

--- tests/probes/symlink/test_probes_symlink.sh
+++ tests/probes/symlink/test_probes_symlink.sh
@@ -12,43 +12,45 @@ set -e -o pipefail
 function test_probes_symlink {
     probecheck "symlink" || return 255
 
-    DF="${srcdir}/test_probes_symlink.xml"
+    DF="test_probes_symlink.xml"
     RF="results.xml"
 
     rm -f $RF
-    rm -f some_file
-    rm -f file_to_remove
-    rm -f normal_symlink
-    rm -f broken_symlink
-    rm -f circular_symlink
 
-    touch some_file
-    touch file_to_remove
-    ln -s some_file normal_symlink
-    ln -s file_to_remove broken_symlink
-    rm -f file_to_remove
-    ln -s circular_symlink circular_symlink
+    tmpdir=$(mktemp -t -d "test_symlink.XXXXXX")
+
+    touch $tmpdir/some_file
+    touch $tmpdir/file_to_remove
+    ln -s $tmpdir/some_file $tmpdir/normal_symlink
+    ln -s $tmpdir/file_to_remove $tmpdir/broken_symlink
+    rm -f $tmpdir/file_to_remove
+    ln -s $tmpdir/circular_symlink $tmpdir/circular_symlink
+    touch $tmpdir/other_file
+    ln -s $tmpdir/other_file $tmpdir/middle_symlink
+    ln -s $tmpdir/middle_symlink $tmpdir/chained_symlink
+
+    bash ${srcdir}/test_probes_symlink.xml.sh $tmpdir > "$DF"
 
     $OSCAP oval eval --results $RF $DF
 
     result=$RF
 
     p='oval_results/results/system/oval_system_characteristics/'
-    assert_exists 5 $p'collected_objects/object'
-    assert_exists 1 $p'collected_objects/object[@flag="complete"]'
-    assert_exists 1 $p'system_data/unix-sys:symlink_item'
-    assert_exists 1 $p'system_data/unix-sys:symlink_item[@status="exists"]'
-    assert_exists 1 $p'system_data/unix-sys:symlink_item/unix-sys:filepath[text()="normal_symlink"]'
-    assert_exists 1 $p'system_data/unix-sys:symlink_item/unix-sys:canonical_path[text()="some_file"]'
+    assert_exists 6 $p'collected_objects/object'
+    assert_exists 2 $p'collected_objects/object[@flag="complete"]'
+    assert_exists 2 $p'system_data/unix-sys:symlink_item'
+    assert_exists 2 $p'system_data/unix-sys:symlink_item[@status="exists"]'
+    assert_exists 1 $p'system_data/unix-sys:symlink_item/unix-sys:filepath[text()="'$tmpdir/normal_symlink'"]'
+    assert_exists 1 $p'system_data/unix-sys:symlink_item/unix-sys:canonical_path[text()="'$tmpdir'/some_file"]'
+    assert_exists 1 $p'system_data/unix-sys:symlink_item/unix-sys:filepath[text()="'$tmpdir/chained_symlink'"]'
+    assert_exists 1 $p'system_data/unix-sys:symlink_item/unix-sys:canonical_path[text()="'$tmpdir'/other_file"]'
     assert_exists 2 $p'collected_objects/object[@flag="error"]'
     assert_exists 2 $p'collected_objects/object[@flag="does not exist"]'
     assert_exists 4 $p'collected_objects/object/message'
 
-    rm -f some_file
-    rm -f normal_symlink
-    rm -f broken_symlink
-    rm -f circular_symlink
+    rm -rf $tmpdir
     rm -f $RF
+    rm -f $DF
 }
 
 test_probes_symlink
--- tests/probes/symlink/test_probes_symlink.xml.sh
+++ tests/probes/symlink/test_probes_symlink.xml.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+
+cat <<EOF
 <?xml version="1.0"?>
 <oval_definitions xmlns:oval-def="http://oval.mitre.org/XMLSchema/oval-definitions-5" xmlns:unix="http://oval.mitre.org/XMLSchema/oval-definitions-5#unix" xmlns:oval="http://oval.mitre.org/XMLSchema/oval-common-5" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:ind-def="http://oval.mitre.org/XMLSchema/oval-definitions-5#independent" xmlns:unix-def="http://oval.mitre.org/XMLSchema/oval-definitions-5#unix" xmlns:lin-def="http://oval.mitre.org/XMLSchema/oval-definitions-5#linux" xmlns="http://oval.mitre.org/XMLSchema/oval-definitions-5" xsi:schemaLocation="http://oval.mitre.org/XMLSchema/oval-definitions-5#unix unix-definitions-schema.xsd http://oval.mitre.org/XMLSchema/oval-definitions-5#independent independent-definitions-schema.xsd http://oval.mitre.org/XMLSchema/oval-definitions-5#linux linux-definitions-schema.xsd http://oval.mitre.org/XMLSchema/oval-definitions-5 oval-definitions-schema.xsd http://oval.mitre.org/XMLSchema/oval-common-5 oval-common-schema.xsd">
   <generator>
@@ -20,6 +23,7 @@
           <criterion test_ref="oval:x:tst:3" comment="always pass"/>
           <criterion test_ref="oval:x:tst:4" comment="always pass"/>
           <criterion test_ref="oval:x:tst:5" comment="always pass"/>
+          <criterion test_ref="oval:x:tst:6" comment="always pass"/>
       </criteria>
     </definition>
   </definitions>
@@ -41,31 +45,43 @@
     <unix:symlink_test check="all" check_existence="all_exist" comment="x" id="oval:x:tst:5" version="1">
       <unix:object object_ref="oval:1:obj:5"/>
     </unix:symlink_test>
+    <unix:symlink_test check="all" check_existence="all_exist" comment="x" id="oval:x:tst:6" version="1">
+      <unix:object object_ref="oval:1:obj:6"/>
+      <unix:state state_ref="oval:1:ste:6"/>
+    </unix:symlink_test>
   </tests>
 
   <objects>
     <unix:symlink_object id="oval:1:obj:1" version="1">
-      <unix:filepath>normal_symlink</unix:filepath>
+        <unix:filepath>$1/normal_symlink</unix:filepath>
     </unix:symlink_object>
     <unix:symlink_object id="oval:1:obj:2" version="1">
-      <unix:filepath>broken_symlink</unix:filepath>
+        <unix:filepath>$1/broken_symlink</unix:filepath>
     </unix:symlink_object>
     <unix:symlink_object id="oval:1:obj:3" version="1">
         <unix:filepath>/etc/does_not_exist</unix:filepath>
     </unix:symlink_object>
     <unix:symlink_object id="oval:1:obj:4" version="1">
-      <unix:filepath>some_file</unix:filepath>
+        <unix:filepath>$1/some_file</unix:filepath>
     </unix:symlink_object>
     <unix:symlink_object id="oval:1:obj:5" version="1">
-      <unix:filepath>circular_symlink</unix:filepath>
+        <unix:filepath>$1/circular_symlink</unix:filepath>
+    </unix:symlink_object>
+    <unix:symlink_object id="oval:1:obj:6" version="1">
+        <unix:filepath>$1/chained_symlink</unix:filepath>
     </unix:symlink_object>
   </objects>
 
   <states>
     <unix:symlink_state id="oval:1:ste:1" version="1">
-      <unix:filepath>normal_symlink</unix:filepath>
-      <unix:canonical_path>some_file</unix:canonical_path>
+        <unix:filepath>$1/normal_symlink</unix:filepath>
+        <unix:canonical_path>$1/some_file</unix:canonical_path>
+    </unix:symlink_state>
+    <unix:symlink_state id="oval:1:ste:6" version="1">
+        <unix:filepath>$1/chained_symlink</unix:filepath>
+        <unix:canonical_path>$1/other_file</unix:canonical_path>
     </unix:symlink_state>
   </states>
 
 </oval_definitions>
+EOF
