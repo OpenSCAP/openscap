c11e78c6f253864f23c6609267d3f4565e0b1a8e
Jan Černý <jcerny@redhat.com>
2015-05-29 12:00:48 +0200

    Refactored test, fixed += in Makefile.am

--- tests/probes/file/test_probes_file.sh
+++ tests/probes/file/test_probes_file.sh
@@ -20,8 +20,10 @@ function test_probes_file {
 
     probecheck "file" || return 255
 
-    local ret_val=0;
-    local DF="$srcdir/test_probes_file.xml"
+    local def_id=$2
+    local tst_id=$3
+    local ret_val=1
+    local DF="$srcdir/$1"
     local RF="results.xml"
 
     [ -f $RF ] && rm -f $RF
@@ -29,32 +31,8 @@ function test_probes_file {
     $OSCAP oval eval --results $RF $DF
 
     if [ -f $RF ]; then
-	verify_results "def" $DF $RF 13 && verify_results "tst" $DF $RF 204
-	ret_val=$?
-    else
-	ret_val=1
-    fi
-
-    return $ret_val
-}
-
-function test_recurse {
-
-    probecheck "file" || return 255
-
-    local ret_val=0
-    local DF="$srcdir/test_recurse.xml"
-    local RF="results.xml"
-
-    [ -f $RF ] && rm -f $RF
-
-    $OSCAP oval eval --results $RF $DF
-
-    if [ -f $RF ]; then
-	verify_results "def" $DF $RF 1 && verify_results "tst" $DF $RF 1
-	ret_val=$?
-    else
-	ret_val=1
+        verify_results "def" $DF $RF $def_id && verify_results "tst" $DF $RF $tst_id
+        ret_val=$?
     fi
 
     return $ret_val
@@ -64,7 +42,7 @@ function test_recurse {
 
 test_init "test_probes_file.log"
 
-test_run "test_probes_file" test_probes_file
-test_run "test recurse_file_system=defined" test_recurse
+test_run "test_probes_file" test_probes_file test_probes_file.xml 13 204
+test_run "test recurse_file_system=defined" test_probes_file test_recurse.xml 1 1
 
 test_exit
