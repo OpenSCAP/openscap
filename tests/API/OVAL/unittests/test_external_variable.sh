0777d892e62e56c8a381d3ecac606293ccdc9b9d
Jan Černý <jcerny@redhat.com>
2015-06-18 12:37:16 +0200

    test handling possible_value elements in external_variable

--- tests/API/OVAL/unittests/test_external_variable.sh
+++ tests/API/OVAL/unittests/test_external_variable.sh
@@ -8,7 +8,7 @@ echo "result file: $result"
 stderr=$(mktemp ${name}.err.XXXXXX)
 echo "stderr file: $stderr"
 
-$OSCAP oval eval --results $result $srcdir/$name.oval.xml 2> $stderr
+$OSCAP oval eval --results $result --variables $srcdir/external_variables.xml $srcdir/$name.oval.xml 2> $stderr
 [ ! -s $stderr ] && rm $stderr
 [ -s $result ]
 
@@ -36,4 +36,19 @@ assert_exists 1 '/oval_results/oval_definitions/variables/external_variable[@id=
 assert_exists 1 '/oval_results/oval_definitions/variables/external_variable[@id="oval:x:var:4"]/possible_value[text()="0"]'
 assert_exists 1 '/oval_results/oval_definitions/variables/external_variable[@id="oval:x:var:4"]/possible_value[text()="1"]'
 assert_exists 1 '/oval_results/oval_definitions/variables/external_variable[@id="oval:x:var:4"]/possible_value[@hint="This restricts the variable value(s) to 1."]'
+
+CO='/oval_results/results/system/oval_system_characteristics/collected_objects'
+assert_exists 4 $CO'/object'
+assert_exists 1 $CO'/object[@id="oval:x:obj:1"]'
+assert_exists 1 $CO'/object[@id="oval:x:obj:2"]'
+assert_exists 1 $CO'/object[@id="oval:x:obj:3"]'
+assert_exists 1 $CO'/object[@flag="does not exist"]'
+assert_exists 1 $CO'/object[@id="oval:x:obj:3"]/message'
+assert_exists 1 $CO'/object[@id="oval:x:obj:3"]/message[text()="Referenced variable has no values (oval:x:var:3)."]'
+assert_exists 1 $CO'/object[@id="oval:x:obj:4"]'
+assert_exists 1 $CO'/object[@id="oval:x:obj:4"]/variable_value'
+assert_exists 1 $CO'/object[@id="oval:x:obj:4"]/variable_value[@variable_id="oval:x:var:4"]'
+assert_exists 1 $CO'/object[@id="oval:x:obj:4"]/variable_value[text()="1"]'
+assert_exists 1 $CO'/object[@id="oval:x:obj:4"]/reference'
+
 rm $result
