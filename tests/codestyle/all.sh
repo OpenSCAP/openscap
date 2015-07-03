40f4bc64381f53f3d41e5563bfbdeacea9c0641e
Michal Šrubař <xsruba03@stud.fit.vutbr.cz>
2015-07-03 17:51:51 +0200

    The original regex skiped all the files before */source/oscap_source.c.
    See https://fedorahosted.org/openscap/ticket/470

--- tests/codestyle/all.sh
+++ tests/codestyle/all.sh
@@ -21,7 +21,7 @@ function test_illicit_function_use {
 		echo "xmlTextReaderReadString is not allowed within OpenSCAP project. Its implementation in libxml does not play well with xmlWalkerReader."
 		return 1;
 	fi
-	whitelisted=$(echo $codebase | sed 's/\w.*\/source\/oscap_source.c / /g' -)
+	whitelisted=$(echo $codebase | sed 's/\S*\/source\/oscap_source.c / /g' -)
 	if grep xmlReadFile $whitelisted; then
 		echo "xmlReadFile is not allowed within OpenSCAP project. Please make a use of oscap_source facility."
 		return 1;
