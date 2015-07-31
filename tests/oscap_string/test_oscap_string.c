7fde23785b3827d2331dc7d50bf67f2360ba2628
Zbynek Moravec <zmoravec@redhat.com>
2015-07-31 16:51:33 +0200

    tests: oscap_string - add test for oscap_buffer_append_binary_data()

--- tests/oscap_string/test_oscap_string.c
+++ tests/oscap_string/test_oscap_string.c
@@ -2,9 +2,11 @@
 #include <stdlib.h>
 #include <string.h>
 #include "common/oscap_string.h"
+#include "common/oscap_buffer.h"
 
 int test_append_char(void);
 int test_append_string(void);
+int test_append_binary_data(void);
 
 int test_append_char()
 {
@@ -49,6 +51,49 @@ int test_append_string()
 	return retval;
 }
 
+
+int test_append_binary_data()
+{
+	#define ASSERT_LENGTH(STR, LENGTH) if (oscap_buffer_get_length(STR) != (LENGTH)) { fprintf(stderr, "Length of result does not match the required length.\n"); return 1;}
+
+	static const char* data[] = { 
+		"\0",
+		"\0A",
+		"ABC"
+	};
+
+	struct oscap_buffer *s = oscap_buffer_new();
+	ASSERT_LENGTH(s, 0);
+
+	oscap_buffer_append_binary_data(s, data[0], 1);
+	ASSERT_LENGTH(s, 1);
+
+	oscap_buffer_append_binary_data(s, data[1], 2);
+	ASSERT_LENGTH(s, 3);
+
+	oscap_buffer_append_binary_data(s, data[0], 0); // do not increase length
+	ASSERT_LENGTH(s, 3);
+
+	oscap_buffer_append_binary_data(s, data[2], 3);
+	ASSERT_LENGTH(s, 6);
+	
+	
+	// Check output data
+	int length = oscap_buffer_get_length(s); // length is valid due to previous ASSERT_LENGTH
+	static const char expected_result[] = {'\0', '\0', 'A', 'A', 'B', 'C'};
+	const char* result = oscap_buffer_get_raw(s);
+	for (int i = 0; i < length; ++i) {
+		char expected = expected_result[i];
+		char got = result[i];
+		if ( expected != got ) {
+			fprintf(stderr, "Invalid character at index %d, expected %x and got %x\n", i, expected, got);
+			return 1;
+		}
+	}
+
+	return 0;
+}
+
 int main (int argc, char *argv[])
 {
 	int retval = 0;
@@ -59,6 +104,10 @@ int main (int argc, char *argv[])
 	if ((retval = test_append_string()) != 0 ) {
 		return retval;
 	}
+
+	if ((retval = test_append_binary_data()) != 0 ) {
+		return retval;
+	}
+
 	return retval;
 }
-
