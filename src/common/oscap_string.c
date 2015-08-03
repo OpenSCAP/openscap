7e56359041e78aeafb1b590f9a629ca36d62d96b
Zbynek Moravec <zmoravec@redhat.com>
2015-08-03 18:00:37 +0200

    Move body of oscap_string to oscap_buffer

--- src/common/oscap_string.c
+++ src/common/oscap_string.c
@@ -24,81 +24,33 @@
 #include <config.h>
 #endif
 
-#include <stdlib.h>
-#include <string.h>
 #include "oscap_string.h"
+#include "oscap_buffer.h"
 
-#define INITIAL_CAPACITY 64
 
-/**
- * String with unlimited length
- * contains:
- * - pointer to data,
- * - actual length of string,
- * - capacity of allocated memory
- */
-struct oscap_string {
-	char *str;
-	size_t length;
-	size_t capacity;
-};
+typedef struct oscap_buffer oscap_string;
 
 struct oscap_string *oscap_string_new()
 {
-	struct oscap_string *s;
-	s = oscap_alloc(sizeof(struct oscap_string));
-	s->str = oscap_alloc(INITIAL_CAPACITY);
-	s->str[0] = '\0';
-	s->length = 0;
-	s->capacity = INITIAL_CAPACITY;
-	return s;
+	return (struct oscap_string*) oscap_buffer_new();
 }
 
 void oscap_string_free(struct oscap_string *s)
 {
-	if (s != NULL) {
-		oscap_free(s->str);
-		oscap_free(s);
-	}
+	oscap_buffer_free((struct oscap_buffer*)s);
 }
 
 void oscap_string_append_char(struct oscap_string *s, char c)
 {
-	if (s == NULL)
-		return;
-	if ((s->length + 1) + 1 > s->capacity) {
-		/* Aligning allocated memory to multiples of INITIAL_CAPACITY.
-		 * We pass to realloc the nearest greater muliple of INITIAL_CAPACITY
-		 * rather than only needed capacity in order to not fragment
-		 * the memory.
-		 */
-		s->capacity = (s->capacity / INITIAL_CAPACITY + 1) * INITIAL_CAPACITY;
-		s->str = oscap_realloc(s->str, s->capacity);
-	}
-	s->str[s->length++] = c;
-	s->str[s->length] = '\0';
+	oscap_buffer_append_char((struct oscap_buffer*)s,c);
 }
 
 void oscap_string_append_string(struct oscap_string *s, const char *t)
 {
-	if (s == NULL || t == NULL)
-		return;
-	const size_t append_length = strlen(t);
-	if (s->length + append_length + 1 > s->capacity)  {
-		/* Aligning allocated memory to multiples of INITIAL_CAPACITY.
-		 * We pass to realloc the nearest greater muliple of INITIAL_CAPACITY
-		 * rather than only needed capacity in order to not fragment
-		 * the memory.
-		 */
-		s->capacity = ((s->capacity + append_length - 1) / INITIAL_CAPACITY + 1) * INITIAL_CAPACITY;
-		s->str = oscap_realloc(s->str, s->capacity);
-	}
-	// Terminating null byte is included by strcpy
-	strcpy(s->str + s->length, t);
-	s->length += append_length;
+	oscap_buffer_append_string((struct oscap_buffer*)s,t);
 }
 
 const char *oscap_string_get_cstr(const struct oscap_string *s)
 {
-	return s->str;
+	return oscap_buffer_get_cstr((struct oscap_buffer*)s);
 }
