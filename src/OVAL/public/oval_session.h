76ac91b36eecd5424a39484c6cd9b657f1722ef3
Michal Šrubař <msrubar@redhat.com>
2015-08-05 08:51:44 +0200

    Introduce a new OVAL Session structure and module

---
+++ src/OVAL/public/oval_session.h
@@ -0,0 +1,66 @@
+/**
+ * @addtogroup OVAL
+ * @{
+ * @addtogroup OVALSESSION
+ * OVAL Session
+ *
+ * This is a high level API for OVAL Definition content evaluation, collection
+ * of OVAL System Characteristics and analysing of OVAL Definitions and
+ * collected OVAL System Characteristics.
+ * @{
+ *
+ * @file
+ */
+
+/*
+ * Copyright 2015 Red Hat Inc., Durham, North Carolina.
+ * All Rights Reserved.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Author:
+ * 		Michal Šrubař <msrubar@redhat.com>
+ */
+
+#ifndef OVAL_SESSION_H_
+#define OVAL_SESSION_H_
+
+/**
+ * @struct oval_session
+ * A structure encapsulating the context of OVAL operations.
+ */
+struct oval_session;
+
+/**
+ * Costructor of an \ref oval_session. It attempts to recognize a type of the
+ * filename which has to be a source datastream or OVAL Definitions.
+ *
+ * @memberof oval_session
+ * @param filename a path to an OVAL or source datastream file
+ *
+ * @returns a newly created \ref oval_session
+ * @retval NULL in case of an error (use \ref oscap_err_desc or \ref
+ * oscap_err_get_full_error to get more details)
+ */
+struct oval_session *oval_session_new(const char *filename);
+
+/**
+ * Destructor of an \ref oval_session.
+ * @memberof oval_session
+ * @param session an \ref oval_session to destroy
+ */
+void oval_session_free(struct oval_session *session);
+
+#endif
