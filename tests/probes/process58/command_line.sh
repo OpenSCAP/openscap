e7289b8450889cc40565bc9c5d53eeb26728edde
Zbynek Moravec <zmoravec@redhat.com>
2015-08-07 10:28:45 +0200

    tests: process58 do not compare command-line with output of ps
    ps's versions have different behaviour and it is causing problems with testing on different systems
    I have recorded output of ps on my fedora(should has some new version of ps) and test will compare
    recorded output with output from probe.
    
    There is some xpath bug that sometimes convert entities back and sometimes not. I've fixed it with sed

--- tests/probes/process58/command_line.sh
+++ tests/probes/process58/command_line.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-export LC_ALL=C
 set -e -o pipefail
 
 function clean_processes {
@@ -19,16 +18,16 @@ function get_process_cmdline() {
 	ps opid,cmd | grep -E "^\s*$PID\s+" | sed -E 's/^\s*'$PID'\s+(.*)$/\1/'
 }
 
-# Wait for command line of child
-function get_child_cmdline() {
+# Wait for process start
+# We can scan processes before process's exec() and get wrong command_line
+function wait_for_process() {
 	local PID="$1"
-	for i in `seq 1 10`;
+	for i in `seq 1 10`; # wait max 10 * 100ms
 		do
 			PROCESS_CMDLINE="`get_process_cmdline $PID`"
 			[[ "${PROCESS_CMDLINE}" == *${PROC}* ]] && break
 			sleep 0.1s
 		done
-	echo "${PROCESS_CMDLINE}"
 }
 
 # Actively wait for zombie process with specified PPID and return its PID
@@ -44,13 +43,24 @@ function get_zombie_pid_from_ppid() {
 	echo ${ZOMBIE_PID}
 }
 
-# Return XPATH expression to search process58_item in oval-result with corresponding "PID" and "command_line"
-function pid_command_line_xpath() {
-		local PID="$1"
-		local COMMAND_LINE="$2"
-		local ITEM_PATH='/oval_results/results/system/oval_system_characteristics/system_data/unix-sys:process58_item[@status="exists"]'
-		echo "${ITEM_PATH}/unix-sys:pid[text()=\"$PID\"]"\
-		"/../unix-sys:command_line[text()=\"${COMMAND_LINE}\"]"
+function get_command_line_node_text() {
+	local PID="$1"
+	local item='/oval_results/results/system/oval_system_characteristics/system_data/unix-sys:process58_item[@status="exists"]/unix-sys:pid[text()="'${PID}'"]'
+	assert_exists 1 "${item}"
+	xpath $result "${item}/../unix-sys:command_line/text()" 2>/dev/null
+}
+
+function assert_match_command_line() {
+	local PID="$1"
+	local EXPECTED_COMMAND_LINE="$2"
+	
+	# get text from element and fix xpath bug
+	cmdline="`get_command_line_node_text \"$PID\" | sed 's|&lt;|<|g' | sed 's|&gt;|>|g'`" 
+	
+	echo "$cmdline" | grep -qE "${EXPECTED_COMMAND_LINE}" || { # compare commands
+		echo "Failed: expected command-line of process with PID=${PID} is '${EXPECTED_COMMAND_LINE}' but real is: '$cmdline'"
+		exit 1
+	}
 }
 
 name=$(basename $0 .sh)
@@ -65,25 +75,29 @@ echo "stderr file: $stderr"
 	# Run process with simple params
 	"${PROC}" param1 param2 param3 &
 	PID=$!
-	CMDLINE="`get_child_cmdline $PID`"
-	[ -n "$PID" ]
-	[ -n "$CMDLINE" ]
+	[ -n "${PID}" ]
+	wait_for_process $PID
+	# "/bin/bash ./stopped_process.sh param1 param2 param3"
+	CMDLINE_REGEX='/(\w+/)+bash.*stopped_process\.sh param1 param2 param3$'
 
 
 	# Run zombie process (without full cmdline)
 	( : & exec "${PROC}" ) &
 	ZOMBIE_PPID=$!
 	ZOMBIE_PID=`get_zombie_pid_from_ppid ${ZOMBIE_PPID}`
-	ZOMBIE_CMDLINE="`get_process_cmdline ${ZOMBIE_PID}`"
 	[ -n "${ZOMBIE_PPID}" ]
-	[ -n "${ZOMBIE_CMDLINE}" ]
+	wait_for_process ${ZOMBIE_PID}
+	# "[command_line.sh] <defunct>"
+	ZOMBIE_CMDLINE_REGEX='^\[command_line.sh\] <defunct>$'
+
 
 	# Run process with special characters in parameters
 	"${PROC}" escaped "`echo -ne \"\e\n\E[1;33m\"`" "\\\n\e" &
 	ESCAPED_PID=$!
-	ESCAPED_CMDLINE="`get_child_cmdline ${ESCAPED_PID}`"
 	[ -n "${ESCAPED_PID}" ]
-	[ -n "${ESCAPED_CMDLINE}" ]
+	wait_for_process ${ESCAPED_PID}
+	# "/bin/bash ./stopped_process.sh escaped . .[1;33m \\n\e"
+	ESCAPED_CMDLINE_REGEX='/(\w+/)+bash.*stopped_process\.sh escaped \. \.\[1;33m \\\\n\\e$'
 
 ########################################################################
 ### Run evaluation:
@@ -97,8 +111,8 @@ echo "stderr file: $stderr"
 ### Check results:
 ########################################################################
 	[ -s $result ]
-	assert_exists 1 "`pid_command_line_xpath \"${PID}\" \"${CMDLINE}\"`"
-	assert_exists 1 "`pid_command_line_xpath \"${ZOMBIE_PID}\" \"${ZOMBIE_CMDLINE}\"`"
-	assert_exists 1 "`pid_command_line_xpath \"${ESCAPED_PID}\" \"${ESCAPED_CMDLINE}\"`"
+	assert_match_command_line "${PID}" "${CMDLINE_REGEX}"
+	assert_match_command_line "${ZOMBIE_PID}" "${ZOMBIE_CMDLINE_REGEX}"
+	assert_match_command_line "${ESCAPED_PID}" "${ESCAPED_CMDLINE_REGEX}"
 
 	rm $result
