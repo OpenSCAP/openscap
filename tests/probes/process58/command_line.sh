9a7f6942b704840d4ade4718fb9c14521833f8f5
Zbynek Moravec <zmoravec@redhat.com>
2015-08-09 20:41:27 +0200

    tests: process58 fix typo

--- tests/probes/process58/command_line.sh
+++ tests/probes/process58/command_line.sh
@@ -6,7 +6,7 @@ function clean_processes {
 	# Processes are in stopped state. SIGCONT cause their exiting
 	[ -n "${ZOMBIE_PPID}" ] && kill -SIGCONT ${ZOMBIE_PPID}
 	[ -n "${PID}" ] && kill -SIGCONT ${PID}
-	[ -n "${ESCAPED_PPID}" ] && kill -SIGCONT ${ESCAPED_PID}
+	[ -n "${ESCAPED_PID}" ] && kill -SIGCONT ${ESCAPED_PID}
 }
 trap clean_processes EXIT
 
