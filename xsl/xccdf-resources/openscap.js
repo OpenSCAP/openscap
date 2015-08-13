22771e2b2fd1f438d5113847c4394adb7fdedc25
Jan Černý <jcerny@redhat.com>
2015-08-13 10:14:37 +0200

    Replace tabs by spaces

--- xsl/xccdf-resources/openscap.js
+++ xsl/xccdf-resources/openscap.js
@@ -33,7 +33,7 @@ function toggleRuleDisplay(checkbox)
         $(".rule-overview-leaf-" + result).addClass("rule-result-filtered");
         $(".rule-detail-" + result).addClass("rule-result-filtered");
     }
-	stripeTreeTable();
+    stripeTreeTable();
 }
 
 function toggleResultDetails(button)
@@ -115,151 +115,151 @@ var original_treetable = null;
 $(document).ready( function() {
     $("#result-details").hide();
     $(".js-only").show();
-	$(".form-group select").val("default");
+    $(".form-group select").val("default");
     $(".toggle-rule-display").each(function(){
         toggleRuleDisplay(this);
     });
     original_treetable = $(".treetable").clone();
     $(".treetable").treetable({ column: 0, expandable: true, initialState : "expanded", indent : 0 });
     is_original = true;
-	stripeTreeTable();
+    stripeTreeTable();
 });
 
 function resetTreetable() {
-	if (!is_original) {
-		$(".treetable").remove();
-		$("#rule-overview").append(original_treetable.clone());
-		$(".treetable").treetable({ column: 0, expandable: true, clickableNodeNames: true, initialState : "expanded", indent : 0 });
-		$(".toggle-rule-display").each(function(){
-			toggleRuleDisplay(this);
-		});
-		is_original = true;
-	}
+    if (!is_original) {
+        $(".treetable").remove();
+        $("#rule-overview").append(original_treetable.clone());
+        $(".treetable").treetable({ column: 0, expandable: true, clickableNodeNames: true, initialState : "expanded", indent : 0 });
+        $(".toggle-rule-display").each(function(){
+            toggleRuleDisplay(this);
+        });
+        is_original = true;
+    }
 }
 
 function newGroupLine(group_name)
 {
-	return "<tr class=\"rule-overview-inner-node\" data-tt-id=\"" + group_name + "\">" +
-		"<td colspan=\"3\"><strong>" + group_name + "</strong></td></tr>";
+    return "<tr class=\"rule-overview-inner-node\" data-tt-id=\"" + group_name + "\">" +
+        "<td colspan=\"3\"><strong>" + group_name + "</strong></td></tr>";
 }
 
 var KeysEnum = {
-	DEFAULT: "default",
-	SEVERITY: "severity",
-	RESULT: "result",
-	NIST: "http://nvlpubs.nist.gov/nistpubs/SpecialPublications/NIST.SP.800-53r4.pdf",
-	DISA: "http://iase.disa.mil/stigs/cci/Pages/index.aspx"
+    DEFAULT: "default",
+    SEVERITY: "severity",
+    RESULT: "result",
+    NIST: "http://nvlpubs.nist.gov/nistpubs/SpecialPublications/NIST.SP.800-53r4.pdf",
+    DISA: "http://iase.disa.mil/stigs/cci/Pages/index.aspx"
 };
 
 /* This function returns an array of target groups indentifiers */
 function getTargetGroupsList(rule, key)
 {
-	switch(key) {
-	case KeysEnum.SEVERITY:
-		var severity = rule.children(".rule-severity").text();
-		return [severity];
-	case KeysEnum.RESULT:
-		var result = rule.children(".rule-result").text();
-		return [result];
-	default:
-		try {
-			var references = JSON.parse(rule.attr("data-references"));
-		} catch (err) {
-			return ["unknown"];
-		}
-		if (!references.hasOwnProperty(key))
-			return ["unknown"];
-		return references[key];
-	}
+    switch(key) {
+    case KeysEnum.SEVERITY:
+        var severity = rule.children(".rule-severity").text();
+        return [severity];
+    case KeysEnum.RESULT:
+        var result = rule.children(".rule-result").text();
+        return [result];
+    default:
+        try {
+            var references = JSON.parse(rule.attr("data-references"));
+        } catch (err) {
+            return ["unknown"];
+        }
+        if (!references.hasOwnProperty(key))
+            return ["unknown"];
+        return references[key];
+    }
 }
 
 function sortGroups(groups, key)
 {
-	switch(key) {
-	case KeysEnum.SEVERITY:
-		return ["high", "medium", "low"];
-	case KeysEnum.DISA:
-		return groups.sort(function(a, b){
-			return parseInt(a) - parseInt(b);
-		});
-	case KeysEnum.NIST:
-		return groups.sort(function(a, b){
-			var regex = /(\w\w)-(\d+)(.*)/;
-			var a_parts = regex.exec(a);
-			var b_parts = regex.exec(b);
-			if (a_parts == null)
-				return 1;
-			if (b_parts == null)
-				return -1;
-			var result = a_parts[1].localeCompare(b_parts[1]);
-			if (result != 0) {
-				return result;
-			} else {
-				result = a_parts[2] - b_parts[2];
-				if (result != 0) {
-					return result;
-				} else {
-					return a_parts[3].localeCompare(b_parts[3]);
-				}
-			}
-		});
-	default:
-		return groups.sort();
-	}
+    switch(key) {
+    case KeysEnum.SEVERITY:
+        return ["high", "medium", "low"];
+    case KeysEnum.DISA:
+        return groups.sort(function(a, b){
+            return parseInt(a) - parseInt(b);
+        });
+    case KeysEnum.NIST:
+        return groups.sort(function(a, b){
+            var regex = /(\w\w)-(\d+)(.*)/;
+            var a_parts = regex.exec(a);
+            var b_parts = regex.exec(b);
+            if (a_parts == null)
+                return 1;
+            if (b_parts == null)
+                return -1;
+            var result = a_parts[1].localeCompare(b_parts[1]);
+            if (result != 0) {
+                return result;
+            } else {
+                result = a_parts[2] - b_parts[2];
+                if (result != 0) {
+                    return result;
+                } else {
+                    return a_parts[3].localeCompare(b_parts[3]);
+                }
+            }
+        });
+    default:
+        return groups.sort();
+    }
 }
 
 function groupRulesBy(key) {
-	/* We must reset the treetable and process grouping upon the original
-	 * table to avoid unwanted duplicties in new table. */
-	resetTreetable();
-	if (key == KeysEnum.DEFAULT)
-		return;
+    /* We must reset the treetable and process grouping upon the original
+     * table to avoid unwanted duplicties in new table. */
+    resetTreetable();
+    if (key == KeysEnum.DEFAULT)
+        return;
 
-	/* Browse the rules and sort them into groups */
-	var lines = {};
-	$(".rule-overview-leaf").each(function() {
-		$(this).children("td:first").css("padding-left","0px");
-		var id = $(this).attr("data-tt-id");
-		var target_groups = getTargetGroupsList($(this), key);
-		for (i = 0; i < target_groups.length; i++) {
-			var target_group = target_groups[i];
-			if (!lines.hasOwnProperty(target_group)) {
-				/* Create a new group */
-				lines[target_group] = [newGroupLine(target_group)];
-			}
-			var clone = $(this).clone();
-			clone.attr("data-tt-id", id + "copy" + i);
-			clone.attr("data-tt-parent-id", target_group);
-			var new_line = clone.wrap("<div>").parent().html();
-			lines[target_group].push(new_line);
-		}
-	});
+    /* Browse the rules and sort them into groups */
+    var lines = {};
+    $(".rule-overview-leaf").each(function() {
+        $(this).children("td:first").css("padding-left","0px");
+        var id = $(this).attr("data-tt-id");
+        var target_groups = getTargetGroupsList($(this), key);
+        for (i = 0; i < target_groups.length; i++) {
+            var target_group = target_groups[i];
+            if (!lines.hasOwnProperty(target_group)) {
+                /* Create a new group */
+                lines[target_group] = [newGroupLine(target_group)];
+            }
+            var clone = $(this).clone();
+            clone.attr("data-tt-id", id + "copy" + i);
+            clone.attr("data-tt-parent-id", target_group);
+            var new_line = clone.wrap("<div>").parent().html();
+            lines[target_group].push(new_line);
+        }
+    });
 
-	/* Remove old treetable and replace it with a new one */
-	$(".treetable").remove();
-	var groups = sortGroups(Object.keys(lines), key);
-	var html_text = "";
-	for (i = 0; i < groups.length; i++) {
-		html_text += lines[groups[i]].join("\n");
-	}
-	new_table ="<table class=\"treetable table table-bordered\"><thead><tr><th>Group</th> <th style=\"width: 120px; text-align: center\">Severity</th><th style=\"width: 120px; text-align: center\">Result</th></tr></thead><tbody>" + html_text + "</tbody></table>";
-	$("#rule-overview").append(new_table);
-	is_original = false;
-	$(".treetable").treetable({
-		column: 0,
-		expandable: true,
-		clickableNodeNames: true,
-		initialState: "expanded",
-		indent: 0 });
-	stripeTreeTable();
+    /* Remove old treetable and replace it with a new one */
+    $(".treetable").remove();
+    var groups = sortGroups(Object.keys(lines), key);
+    var html_text = "";
+    for (i = 0; i < groups.length; i++) {
+        html_text += lines[groups[i]].join("\n");
+    }
+    new_table ="<table class=\"treetable table table-bordered\"><thead><tr><th>Group</th> <th style=\"width: 120px; text-align: center\">Severity</th><th style=\"width: 120px; text-align: center\">Result</th></tr></thead><tbody>" + html_text + "</tbody></table>";
+    $("#rule-overview").append(new_table);
+    is_original = false;
+    $(".treetable").treetable({
+        column: 0,
+        expandable: true,
+        clickableNodeNames: true,
+        initialState: "expanded",
+        indent: 0 });
+    stripeTreeTable();
 }
 
 function stripeTreeTable()
 {
-	var rows = $(".rule-overview-leaf:not(.rule-result-filtered)");
-	var even = false;
-	$(rows).each(function(){
-		$(this).css("background-color", even ? "#F9F9F9" : "inherit");
-		even = !even;
-	});
+    var rows = $(".rule-overview-leaf:not(.rule-result-filtered)");
+    var even = false;
+    $(rows).each(function(){
+        $(this).css("background-color", even ? "#F9F9F9" : "inherit");
+        even = !even;
+    });
 }
