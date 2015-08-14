139308803b9cdfa3752d283f0ed2fff38443d734
Zbynek Moravec <zmoravec@redhat.com>
2015-08-14 13:37:07 +0200

    test: add rbt_gen to ignore_list of checking #include config.h

--- tests/codestyle/all.sh
+++ tests/codestyle/all.sh
@@ -36,9 +36,10 @@ function shell_script_syntax(){
 
 function test_config_h(){
 	text='(#\s*include\s+<config.h>)|(#\s*include\s+"config.h")'
-	
-	ignore_list='MurmurHash3.c$'
+
+	ignore_list='(MurmurHash3.c|rbt_gen.c)$'
 	echo "Files from this mask will not be checked: $ignore_list"
+
 	codebase=$(find $top_srcdir/src/ -name "*.c" | sort)
 	echo "$codebase" | grep -vE "$ignore_list" | while read filename;
 	do
