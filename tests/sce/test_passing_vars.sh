31f8429305de3a60fc9ac6952ab5321a02704f85
Michal Šrubař <msrubar@redhat.com>
2015-08-26 11:05:10 +0200

    Test that values specified in a profile are passed to a SCE script correctly

---
+++ tests/sce/test_passing_vars.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+# Author:
+#	Michal Srubar <msrubar@redhat.com>
+
+# Test whether the value of set with <refine-value> or <set-value> is passed to
+# the SCE script correctly or not.
+
+set -e -o pipefail
+
+. ${srcdir}/../test_common.sh
+
+# Test Cases.
+function test_passing_values {
+
+  local DEFFILE=${srcdir}/$1
+  local RESFILE=$1.results
+
+  [ -f $RESFILE ] && rm -f $RESFILE
+
+  $OSCAP xccdf eval --results "$RESFILE" --profile "default" "$DEFFILE"
+
+  # ensure that value set with <refine-value> was passed correctly
+  grep "VALUE_1=fail" "$RESFILE"
+  # ensure that value set with <set-value> was passed correctly
+  grep "VALUE_2=my_pass_val" "$RESFILE"
+}
+
+# Testing.
+test_init "test_passing_values.log"
+
+test_run "sce" test_passing_values test_passing_vars_xccdf.xml
+
+test_exit
+
