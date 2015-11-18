c4f1f9bcc3e2fd56b1fd3d249445d30b3676e7de
Jan Černý <jcerny@redhat.com>
2015-11-18 09:08:43 +0100

    Add new messages to OVAL session
    With those few messages we would slowly start to see
    the "story of OVAL evaluation" in the log file.

--- src/OVAL/oval_session.c
+++ src/OVAL/oval_session.c
@@ -29,6 +29,7 @@
 #include <string.h>
 
 #include "common/alloc.h"
+#include "common/debug_priv.h"
 #include "common/util.h"
 #include "common/_error.h"
 #include "common/oscapxml.h"
@@ -95,6 +96,7 @@ struct oval_session *oval_session_new(const char *filename)
 		return NULL;
 	}
 
+	dI("Created a new OVAL session from input file '%s'.\n", filename);
 	return session;
 }
 
@@ -268,6 +270,9 @@ static int oval_session_load_variables(struct oval_session *session)
 				return 1;
 			}
 		}
+		dI("Loaded OVAL variables.\n");
+	} else {
+		dI("No external OVAL variables provided.\n");
 	}
 
 	return 0;
@@ -357,6 +362,7 @@ int oval_session_evaluate(struct oval_session *session, char *probe_root, agent_
 			oscap_seterr(OSCAP_EFAMILY_OSCAP, "Failed to set the OSCAP_PROBE_ROOT environment variable.");
 			return 1;
 		}
+		dI("OSCAP_PROBE_ROOT environment variable set to '%s'.\n", probe_root);
 	}
 
 	if (oval_session_setup_agent(session) != 0) {
@@ -370,6 +376,7 @@ int oval_session_evaluate(struct oval_session *session, char *probe_root, agent_
 
 	session->res_model = oval_agent_get_results_model(session->sess);
 
+	dI("OVAL evaluation successfully finished.\n");
 	return 0;
 }
 
