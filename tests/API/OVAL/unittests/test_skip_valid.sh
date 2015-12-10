73675a66cea057bd2b743ce1a830fbf0eddfe2bc
Jan Černý <jcerny@redhat.com>
2015-12-10 15:03:51 +0100

    Add more assertions to a test
    This commit adds an assert to invalid state in result file
    and that a error message happens to stderr.

--- tests/API/OVAL/unittests/test_skip_valid.sh
+++ tests/API/OVAL/unittests/test_skip_valid.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
 result=`mktemp`
+stderr=`mktemp`
 
 set -e
 set -o pipefail
 
-$OSCAP oval eval --results $result --skip-valid $srcdir/test_skip_valid.oval.xml
+$OSCAP oval eval --results $result --skip-valid $srcdir/test_skip_valid.oval.xml 2> $stderr
+
+grep -q "\*\*INVALID\*\*_state" $result
+grep -q "OpenSCAP Error: Invalid OVAL family" $stderr
 
 rm $result
+rm $stderr
 
