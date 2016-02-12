a42b51c5bc06020a0ac048cec5b8cae8de19cc51
Zbynek Moravec <zmoravec@redhat.com>
2016-02-12 12:53:42 +0100

    probe/rpmverifyfile: enable offline scan

--- src/OVAL/probes/unix/linux/rpmverifyfile.c
+++ src/OVAL/probes/unix/linux/rpmverifyfile.c
@@ -73,6 +73,9 @@
 #include "debug_priv.h"
 #include "probe/entcmp.h"
 
+#include <probe/probe.h>
+#include <probe/option.h>
+
 struct rpmverify_res {
 	char *name;  /**< package name */
 	const char *epoch;
@@ -334,7 +337,7 @@ void *probe_init (void)
 	g_rpm.rpmts = rpmtsCreate();
 
 	pthread_mutex_init(&(g_rpm.mutex), NULL);
-
+	probe_setoption(PROBEOPT_OFFLINE_MODE_SUPPORTED, PROBE_OFFLINE_CHROOT);
 	return ((void *)&g_rpm);
 }
 
