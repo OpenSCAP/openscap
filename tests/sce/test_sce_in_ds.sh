88a05328f33259e02eea6af61ca0bb8e10fb8a6d
Zbynek Moravec <zmoravec@redhat.com>
2016-02-24 11:57:35 +0100

    tests/sce: Add test for SCE in DS
    https://github.com/OpenSCAP/openscap/issues/295

---
+++ tests/sce/test_sce_in_ds.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/bash
+
+# Test to check usage of SCE in DS
+#
+# Author:
+#	Zbynek Moravec <zmoravec@redhat.com>
+
+. ../test_common.sh
+
+set -e -o pipefail
+
+function test_sce_in_ds {
+
+	xccdf_file=${srcdir}/$1
+	stderr=$(mktemp)
+	result=$(mktemp)
+
+	$OSCAP xccdf eval --results "$result" "$xccdf_file" 2> $stderr
+
+	assert_exists 1 "//result[text()='pass']"
+
+	[ -f $stderr ]; [ ! -s $stderr ]; rm $stderr
+	[ -f $result ]; [ -s $result ]; rm $result
+}
+
+# Testing.
+test_init "test_sce_in_ds.log"
+
+test_run "SCE in DS" test_sce_in_ds test_sce_in_ds.xml
+
+test_exit
