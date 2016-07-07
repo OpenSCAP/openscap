f7c12a5bab1b4240453037b0b89302462613cea8
Zbynek Moravec <zmoravec@redhat.com>
2016-07-07 15:59:45 +0200

    test/remediation results: Delete test_file before test
    If test failed, next time 'test_file' was already created with
    required right and fix wasn't applied

--- tests/API/XCCDF/unittests/test_remediation_invalid_characters.sh
+++ tests/API/XCCDF/unittests/test_remediation_invalid_characters.sh
@@ -6,6 +6,8 @@ name=$(basename $0 .sh)
 result=$(mktemp -t ${name}.out.XXXXXX)
 stderr=$(mktemp -t ${name}.err.XXXXXX)
 
+rm -f test_file # ensure not existence of test_file
+
 $OSCAP xccdf eval --remediate --results $result $srcdir/${name}.xccdf.xml 2> $stderr
 
 echo "Stderr file = $stderr"
