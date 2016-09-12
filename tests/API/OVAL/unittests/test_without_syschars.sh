30a07cd80320303042b91e99fcf74966808301bd
Zbynek Moravec <zmoravec@redhat.com>
2016-09-12 15:48:50 +0200

    test_without_syschars.sh: Update --without-syschars -> --without-syschar

--- tests/API/OVAL/unittests/test_without_syschars.sh
+++ tests/API/OVAL/unittests/test_without_syschars.sh
@@ -6,13 +6,13 @@ set -e
 set -o pipefail
 
 # Sanity check - system characteristics are provided by default
-$OSCAP oval eval                    --results $result $srcdir/test_without_syschars.xml
+$OSCAP oval eval                   --results $result $srcdir/test_without_syschars.xml
 
 assert_exists 1 '//system_data'
 assert_exists 1 '//collected_objects'
 
-# Check --without-syschars - no system characteristics expected
-$OSCAP oval eval --without-syschars --results $result $srcdir/test_without_syschars.xml
+# Check --without-syschar - no system characteristics expected
+$OSCAP oval eval --without-syschar --results $result $srcdir/test_without_syschars.xml
 
 assert_exists 0 '//system_data'
 assert_exists 0 '//collected_objects'
