1718a2aee0a9028130a3d9291a02e232c3a3466b
Martin Preisler <mpreisle@redhat.com>
2016-12-06 15:52:06 -0500

    Double quote variables to prevent word splitting in nist_test.sh

--- tests/nist/nist_test.sh
+++ tests/nist/nist_test.sh
@@ -11,8 +11,8 @@ set -e -o pipefail
 
 function test_nist {
 	test_dir="$1"
-	run_script="$(cd ${builddir}; pwd)/run"
-	${srcdir}/test_worker.py --scanner "${run_script} $OSCAP" ${srcdir}/$test_dir/
+	run_script="$(cd "${builddir}"; pwd)/run"
+	"${srcdir}/test_worker.py" --scanner "${run_script} $OSCAP" "${srcdir}/$test_dir/"
 	ret_val=$?
 	if [ $ret_val -eq 1 ]; then
 		return 1
