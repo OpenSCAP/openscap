b04ee4e6a218876bb5c717a800bf377a81e77282
Jan Černý <jcerny@redhat.com>
2017-02-01 11:47:45 +0100

    Improve the test for "oscap xccdf generate fix"
    The test will now check for negative cases and also it will
    check that ansible remediations are different than bash remediations.

--- tests/API/XCCDF/unittests/test_fix_arf.sh
+++ tests/API/XCCDF/unittests/test_fix_arf.sh
@@ -8,8 +8,10 @@ result_id="xccdf_org.open-scap_testresult_xccdf_moc.elpmaxe.www_profile_standard
 bash_line1="echo this_is_ok"
 bash_line2="echo fix_me_please"
 ansible_template="urn:xccdf:fix:script:ansible"
-ansible_line1="this_is_ok"
-ansible_line2="fix_me_please"
+ansible_task1a="\- name: ensure everything passes"
+ansible_task1b="shell: /bin/true"
+ansible_task2a="\- name: correct the failing case"
+ansible_task2b="shell: /bin/false"
 
 name=$(basename $0 .sh)
 results_arf=$(mktemp -t ${name}.arf.XXXXXX)
@@ -34,20 +36,26 @@ grep -q "$bash_line2" $script
 $OSCAP xccdf generate fix --profile $profile --template $ansible_template --output $playbook $results_arf >$stdout 2>$stderr
 [ -f $stdout ]; [ ! -s $stdout ]; rm $stdout
 [ -f $stderr ]; [ ! -s $stderr ]; rm $stderr
-grep -q "$ansible_line1" $playbook
-grep -q "$ansible_line2" $playbook
+grep -q "$ansible_task1a" $playbook
+grep -q "$ansible_task1b" $playbook
+grep -q "$ansible_task2a" $playbook
+grep -q "$ansible_task2b" $playbook
 
 # Generate a bash script based on scan results stored in ARF file
 $OSCAP xccdf generate fix --result-id $result_id --output $script $results_arf >$stdout 2>$stderr
 [ -f $stdout ]; [ ! -s $stdout ]; rm $stdout
 [ -f $stderr ]; [ ! -s $stderr ]; rm $stderr
+grep -q -v "$bash_line1" $script
 grep -q "$bash_line2" $script
 
 # Generate  an Ansible playbook based on scan results stored in ARF file
 $OSCAP xccdf generate fix --result-id $result_id --template $ansible_template --output $playbook $results_arf >$stdout 2>$stderr
 [ -f $stdout ]; [ ! -s $stdout ]; rm $stdout
 [ -f $stderr ]; [ ! -s $stderr ]; rm $stderr
-grep -q "$ansible_line2" $playbook
+grep -q -v "$ansible_task1a" $playbook
+grep -q -v "$ansible_task1b" $playbook
+grep -q "$ansible_task2a" $playbook
+grep -q "$ansible_task2b" $playbook
 
 rm $results_arf
 rm $script
