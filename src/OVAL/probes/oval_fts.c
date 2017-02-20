8d99fca89665e75cd48354cd3dc97358d75013ca
Jan Černý <jcerny@redhat.com>
2017-02-20 15:13:20 +0100

    Change category of a message and improve it
    This message has more an informational character, OpenSCAP adds
    the missing carret automatically, so it doesn't lead to any
    dangerous situation. This commit changes the category to INFO.
    Also it adds the string in question into the message
    to make the message more useful for OVAL content development.

--- src/OVAL/probes/oval_fts.c
+++ src/OVAL/probes/oval_fts.c
@@ -518,9 +518,9 @@ static int process_pattern_match(const char *path, pcre **regex_out)
 		pattern = malloc(plen + 1);
 		pattern[0] = '^';
 		memcpy(pattern + 1, path, plen);
-		dW("The pattern doesn't contain a leading caret - added. "
+		dI("The pattern '%s' doesn't contain a leading caret - added. "
 		   "All paths with the 'pattern match' operation must begin "
-		   "with a caret.");
+		   "with a caret.", path);
 	} else {
 		pattern = strdup(path);
 	}
