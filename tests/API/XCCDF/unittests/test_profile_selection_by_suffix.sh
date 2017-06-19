8875ac97544dada32635b6d2b74be4ef2b9da803
Katarina Jankov <kjankov@redhat.com>
2017-06-19 13:59:47 -0400

    Improved suffix selection test to search result file and stderror file for expected outputs.

--- tests/API/XCCDF/unittests/test_profile_selection_by_suffix.sh
+++ tests/API/XCCDF/unittests/test_profile_selection_by_suffix.sh
@@ -5,16 +5,27 @@ set -o pipefail
 
 name=$(basename $0 .sh)
 stderr=$(mktemp -t ${name}.out.XXXXXX)
+result=$(mktemp -t ${name}.out.XXXXXX)
+echo "Stderr file = $stderr"
+echo "Result file = $result"
 ret=0
 
 # Multiple matches should result in failure
 $OSCAP xccdf eval --profile common $srcdir/${name}.xccdf.xml 2> $stderr || ret=$?
 [ $ret -eq 1 ]
+# Checks that first test does result in multiple match error
+grep -Fq "Multiple matches found" $stderr
 
 # Should pass, because underscore creates unique suffix
-$OSCAP xccdf eval --profile _common $srcdir/${name}.xccdf.xml 2> $stderr
+$OSCAP xccdf eval --profile _common --results $result $srcdir/${name}.xccdf.xml 2> $stderr
+# Checks that full profile name is used in rule evaluation
+grep -Fq xccdf_org.open-scap_testresult_xccdf_moc.elpmaxe.www_profile_common $result
 
 # Should fail, because a profile with this suffix does not exist
 $OSCAP xccdf eval --profile xcommon $srcdir/${name}.xccdf.xml 2> $stderr || ret=$?
 [ $ret -eq 1 ]
- 
+# Checks that last test does result in no match error
+grep -Fq "No profile matching suffix \"xcommon\" was found" $stderr
+
+[ -f $stderr ]; rm $stderr
+rm $result
