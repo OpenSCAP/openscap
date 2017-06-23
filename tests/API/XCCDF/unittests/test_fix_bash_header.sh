ed0465c510e8991d6a2b3f61c2403f126388ee0d
Katarina Jankov <kjankov@redhat.com>
2017-06-23 18:16:33 -0400

    Improvement to test and addition of testing of generated headers for Ansible scripts

--- tests/API/XCCDF/unittests/test_fix_bash_header.sh
+++ tests/API/XCCDF/unittests/test_fix_bash_header.sh
@@ -4,6 +4,7 @@ set -x
 set -e
 set -o pipefail
 
+ansible_template="urn:xccdf:fix:script:ansible"
 profile="xccdf_moc.elpmaxe.www_profile_standard"
 result_id="xccdf_org.open-scap_testresult_xccdf_moc.elpmaxe.www_profile_standard"
 title="Standard System Security Profile"
@@ -17,36 +18,57 @@ results_arf=$(mktemp -t ${name}.out.XXXXXX)
 stdout=$(mktemp -t ${name}.out.XXXXXX)
 stderr=$(mktemp -t ${name}.out.XXXXXX)
 script=$(mktemp -t ${name}.sh.XXXXXX)
+playbook=$(mktemp -t ${name}.yml.XXXXXX)
 
-bash_header1="# Bash remediation role for $profile"
-bash_header2="# Profile Title:  $title"
-bash_header3="# Profile Description:  $description"
-bash_header4="# Benchmark ID:  $benchmark"
-bash_header5="# Benchmark Version:  $version"
-bash_header6="# XCCDF Version:  $xccdf_version"
+profile_header1="# Bash remediation role for $profile"
+profile_header2="# Profile Title:  $title"
+profile_header3="# Profile Description:  $description"
+profile_header4="# Benchmark ID:  $benchmark"
+profile_header5="# Benchmark Version:  $version"
+profile_header6="# XCCDF Version:  $xccdf_version"
+
+result_header1="# Bash remediation role for the results of evaluation of $profile"
 
 # Create an ARF
 $OSCAP xccdf eval --profile $profile --results-arf $results_arf $srcdir/$name.xccdf.xml >$stdout 2>$stderr || [ $? == 2 ]
 [ -f $stdout ]; [ -s $stdout ]; rm $stdout
 [ -f $stderr ]; [ ! -s $stderr ]; rm $stderr
 
-# Generate a bash script from a profile in ARF file
-$OSCAP xccdf generate fix --profile $profile --output $script $results_arf >$stdout 2>$stderr
+# Generate a bash script from an OpenSCAP profile 
+$OSCAP xccdf generate fix --profile $profile --output $script $srcdir/$name.xccdf.xml >$stdout 2>$stderr
 [ -f $stdout ]; [ ! -s $stdout ]; rm $stdout
 [ -f $stderr ]; [ ! -s $stderr ]; rm $stderr
-grep  "$bash_header1" $script
-grep  "$bash_header2" $script
-grep  "$bash_header3" $script
-grep  "$bash_header4" $script
-grep  "$bash_header5" $script
-grep  "$bash_header6" $script
+grep "$profile_header1" $script
+grep "$profile_header2" $script
+grep "$profile_header3" $script
+grep "$profile_header4" $script
+grep "$profile_header5" $script
+grep "$profile_header6" $script
 
-# Generate a bash script based on scan resu
-rm -f test_filelts stored in ARF file
+# Generate a bash script based on scan results
 $OSCAP xccdf generate fix --result-id $result_id --output $script $results_arf >$stdout 2>$stderr
 [ -f $stdout ]; [ ! -s $stdout ]; rm $stdout
 [ -f $stderr ]; [ ! -s $stderr ]; rm $stderr
+grep "$result_header1" $script
+
+
+# Generate an Ansible playbook from an OpenSCAP profile
+$OSCAP xccdf generate fix --profile $profile --template $ansible_template --output $playbook $srcdir/$name.xccdf.xml >$stdout 2>$stderr
+[ -f $stdout ]; [ ! -s $stdout ]; rm $stdout
+[ -f $stderr ]; [ ! -s $stderr ]; rm $stderr
+grep "$profile_header1" $playbook
+grep "$profile_header2" $playbook
+grep "$profile_header3" $playbook
+grep "$profile_header4" $playbook
+grep "$profile_header5" $playbook
+grep "$profile_header6" $playbook
+
+# Generate  an Ansible playbook based on scan results stored in ARF file
+$OSCAP xccdf generate fix --result-id $result_id --template $ansible_template --output $playbook $results_arf >$stdout 2>$stderr
+[ -f $stdout ]; [ ! -s $stdout ]; rm $stdout
+[ -f $stderr ]; [ ! -s $stderr ]; rm $stderr
+grep "$result_header1" $playbook
 
 rm $results_arf
 rm $script
-
+rm $playbook
