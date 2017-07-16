f9392988e4b70b3826316f093baef0769585d49c
Katarina Jankov <kjankov@redhat.com>
2017-07-16 17:32:05 -0400

    Tests for parsing  and export of CVRF files

--- src/CVRF/public/cvrf.h
+++ src/CVRF/public/cvrf.h
@@ -3,7 +3,7 @@
  * @{
  *
  * @file cvrf.h
- * @brief Interface to Common Vulnerability and Exposure dictionary
+ * @brief Interface to Common Vulnerability Reporting Framework
  */
 
 #ifndef _CVRF_H_
@@ -52,6 +52,114 @@ struct cvrf_vulnerability;
  * Use remove function otherwise.
  * @{
  * */
+
+/************************************************************/
+/** @} End of Getters group */
+
+
+/**
+ * Get supported version of CVRF XML
+ * @return version of XML file format
+ * @memberof cvrf_model
+ */
+const char * cvrf_model_supported(void);
+
+/**
+ * New CVRF model
+ * @memberof cvrf_model
+ * @return New CVRF model
+ */
+struct cvrf_model *cvrf_model_new(void);
+
+/**
+ * New ProductTree structure
+ * @memberof product_tree
+ * @return New CVRF ProductTree
+ */
+struct product_tree *product_tree_new(void);
+
+/**
+ * New CVRF branch of ProductTree or sub-branch
+ * @memberof cvrf_branch
+ * @return New CVRF branch
+ */
+struct cvrf_branch *cvrf_branch_new(void);
+
+/**
+ * New FullProductName of Branch or ProductTree
+ * @memberof product_name
+ * @return New FullProductName
+ */
+struct product_name *product_name_new(void);
+
+/**
+ * New CVRF Vulnerability
+ * @memberof cvrf_vulnerability
+ * @return New CVRF Vulnerability
+ */
+struct cvrf_vulnerability *cvrf_vulnerability_new(void);
+
+/**
+ * New ProductStatus member of a CVRF Vulnerability
+ * @memberof product_status
+ * @return New CVRF ProductStatus
+ */
+struct product_status *product_status_new(void);
+
+
+/**
+ *
+ *
+ */
+void cvrf_model_free(struct cvrf_model *cvrf) ;
+
+/**
+ *
+ *
+ */
+void product_tree_free(struct product_tree *tree);
+
+/**
+ *
+ *
+ */
+void cvrf_branch_free(struct cvrf_branch *branch);
+
+/**
+ *
+ *
+ */
+void product_name_free(struct product_name *full_name);
+
+/**
+ *
+ *
+ */
+void cvrf_vulnerability_free(struct cvrf_vulnerability *vulnerability);
+
+/**
+ *
+ *
+ */
+void product_status_free(struct product_status *status);
+
+
+
+/**
+ * Export CVRF model to XML file
+ * @memberof cvrf_model
+ * @param cvrf CVRF model
+ * @param file OSCAP export target
+ */
+void cvrf_model_export(struct cvrf_model *cvrf, const char *file);
+
+/**
+ * Parses the specified XML file and creates a list of CVRF data structures.
+ * @memberof cvrf_model
+ * @param file filename
+ * @return New CVRF model structure
+ */
+struct cvrf_model *cvrf_model_import(const char *file);
 
 
 /**@}*/
