c946c0741a4f078b8fe8083e3a0abc49a504f29e
Katarina Jankov <kjankov@redhat.com>
2017-08-04 11:59:50 -0400

    Use tempfiles instead of stored files for CVRF export and eval tests

--- tests/API/CVRF/test_api_cvrf.sh
+++ tests/API/CVRF/test_api_cvrf.sh
@@ -7,16 +7,24 @@ set -x
 
 . ../../test_common.sh
 
+name=$(basename $0 .sh)
+export=$(mktemp -t ${name}.export.xml.XXXXXX)
+results=$(mktemp -t ${name}.results.xml.XXXXXX)
+#stdout=$(mktemp -t ${name}.out.XXXXXX)
+#stderr=$(mktemp -t ${name}.err.XXXXXX)
+
 function test_api_cvrf_eval {
-	./test_api_cvrf --eval $srcdir/cvrf_1.1_template.xml cvrf_1.1_results.xml
+	./test_api_cvrf --eval $srcdir/$name.xml $results
+	
+	rm $results
 }
 
 function test_api_cvrf_export {
 	local ret_val=0
 
-	./test_api_cvrf --export-all $srcdir/cvrf_1.1_template.xml cvrf_1.1_template.out.xml
-	if [ $? -eq 0 ] && [ -f cvrf_1.1_template.out.xml ]; then
-	if ! $XMLDIFF $srcdir/cvrf_1.1_template.xml cvrf_1.1_template.out.xml; then
+	./test_api_cvrf --export-all $srcdir/$name.xml $export
+	if [ $? -eq 0 ] && [ -f $export ]; then
+	if ! $XMLDIFF $srcdir/$name.xml $export; then
 		echo "Exported file differs from what is expected!"
 		ret_val=1
 	fi
@@ -24,13 +32,14 @@ function test_api_cvrf_export {
 	echo "Cannot export!"
 	ret_val=1
 	fi
+	rm $export
 	return $ret_val
 }
 
 function test_api_cvrf_validate {
 	local ret_val=0
 
-	./test_api_cvrf --validate $srcdir/cvrf_1.1_template.xml
+	./test_api_cvrf --validate $srcdir/$name.xml
 	if [ $? -eq 0 ]; then
 	echo "Provided CVRF file is valid"
 	ret_val=0
