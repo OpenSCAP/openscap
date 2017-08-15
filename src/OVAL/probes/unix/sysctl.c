6225c59bb3800c8ef00b3b2cd722e5f0afcd42fa
Jan Černý <jcerny@redhat.com>
2017-08-15 08:44:55 +0200

    Remove resolved comment
    This behavior is not a bug and respective BZ was closed as NOTABUG.
    See https://bugzilla.redhat.com/show_bug.cgi?id=1473207

--- src/OVAL/probes/unix/sysctl.c
+++ src/OVAL/probes/unix/sysctl.c
@@ -187,8 +187,6 @@ int probe_main(probe_ctx *ctx, void *probe_arg)
                         fclose(fp);
 
 			/* Skip empty values as sysctl tool does.
-			 * If this behavior of sysctl will be indentified as a bug,
-			 * we will have to remove this block.
 			 * See https://bugzilla.redhat.com/show_bug.cgi?id=1473207
 			 */
 			if (l == 0) {
