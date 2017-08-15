17941107d78d2a0cf39ceda648a4463232bab8d4
Katarina Jankov <kjankov@redhat.com>
2017-08-15 13:53:10 -0400

    Add authorship to headers of CVRF files

--- src/CVRF/cvrf.c
+++ src/CVRF/cvrf.c
@@ -16,6 +16,9 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
+ * Authors:
+ * 		Katarina Jankov <kj226@cornell.edu>
+ *
  */
 #ifdef HAVE_CONFIG_H
 #include <config.h>
@@ -56,7 +59,6 @@ struct cvrf_index *cvrf_index_import(struct oscap_source *index_source) {
 struct cvrf_model *cvrf_model_import(struct oscap_source *source)
 {
 	__attribute__nonnull__(source);
-
 	if (source == NULL)
 		return NULL;
 
--- src/CVRF/cvrf_priv.c
+++ src/CVRF/cvrf_priv.c
@@ -16,6 +16,9 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
+ * * Authors:
+ * 		Katarina Jankov <kj226@cornell.edu>
+ *
  */
 #ifdef HAVE_CONFIG_H
 #include <config.h>
