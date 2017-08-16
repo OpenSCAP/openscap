a48ff8ad8031c4eacdefba2137bb1d872d5da7ba
Katarina Jankov <kjankov@redhat.com>
2017-08-16 17:27:09 -0400

    Rework CVRF command line interface for better opt parsing and workflow

--- utils/oscap-cvrf.c
+++ utils/oscap-cvrf.c
@@ -54,31 +54,31 @@ static struct oscap_module CVRF_EVALUATE_MODULE = {
 	.name = "eval",
 	.parent = &OSCAP_CVRF_MODULE,
 	.summary = "Evaluate system for vulnerabilities",
-	.usage = "[options] <cvrf file> <results file>",
+	.usage = "[options] <cvrf file.xml>",
 	.opt_parser = getopt_cvrf,
 	.func = app_cvrf_evaluate,
 	.help = "Options:\n"
-		"   --index\r\t\t\t\t - Use index file to evaluate a directory of CVRF files \n"
-	,
+		"   --index\r\t\t\t\t - Use index file to evaluate a directory of CVRF files.\n"
+		"   --results\r\t\t\t\t - Filename to which evaluation results will be saved.\n",
 };
 
 static struct oscap_module CVRF_EXPORT_MODULE = {
 	.name = "export",
 	.parent = &OSCAP_CVRF_MODULE,
 	.summary = "Download and export CVRF file to system",
-	.usage = "[options] <cvrf file> <export file>",
+	.usage = "[options] <cvrf file.xml>",
 	.opt_parser = getopt_cvrf,
 	.func = app_cvrf_export,
 	.help = "Options:\n"
 		"   --index\r\t\t\t\t - Use index file to export a directory of CVRF files \n"
-	,
+		"   --output\r\t\t\t\t - Filename to which exported CVRF document will be saved.\n",
 };
 
 static struct oscap_module CVRF_VALIDATE_MODULE = {
 	.name = "validate",
 	.parent = &OSCAP_CVRF_MODULE,
 	.summary = "Ensure that provided file input follows CVRF format correctly",
-	.usage = "[options] <cvrf file>",
+	.usage = "[options] <cvrf file.xml>",
 	.opt_parser = getopt_cvrf,
 	.func = app_cvrf_validate,
 	.help = "Options:\n"
@@ -97,9 +97,9 @@ static struct oscap_module* CVRF_SUBMODULES[] = {
 static int app_cvrf_evaluate(const struct oscap_action *action) {
 	int result = OSCAP_OK;
 	const char *os_name = "Red Hat Enterprise Linux Desktop Supplementary (v. 6)";
-	struct oscap_source *import_source = oscap_source_new_from_file(action->cvrf_action->cvrf_file);
+	struct oscap_source *import_source = oscap_source_new_from_file(action->cvrf_action->f_cvrf);
 
-	if (cvrf_export_results(import_source, action->cvrf_action->export_file, os_name) == -1) {
+	if (cvrf_export_results(import_source, action->cvrf_action->f_results, os_name) == -1) {
 		result = OSCAP_ERROR;
 		goto cleanup;
 	}
@@ -113,24 +113,39 @@ static int app_cvrf_evaluate(const struct oscap_action *action) {
 }
 
 static int app_cvrf_export(const struct oscap_action *action) {
+	struct oscap_source *import_source = oscap_source_new_from_file(action->cvrf_action->f_cvrf);
+	if (import_source == NULL)
+		return OSCAP_ERROR;
+
 	int result = OSCAP_OK;
-	struct oscap_source *import_source = oscap_source_new_from_file(action->cvrf_action->cvrf_file);
-	struct cvrf_model *model = cvrf_model_import(import_source);
-	if(!model) {
-		result = OSCAP_ERROR;
-		goto cleanup;
+	if (action->cvrf_action->index == 1) {
+		struct cvrf_index *index = cvrf_index_import(import_source);
+		if (index == NULL) {
+			result = OSCAP_ERROR;
+			goto cleanup;
+		}
+		struct oscap_source *export_source = cvrf_index_get_export_source(index);
+		if (oscap_source_save_as(export_source, action->cvrf_action->f_output) != 0)
+			result = OSCAP_ERROR;
+		oscap_source_free(export_source);
+		cvrf_index_free(index);
+	} else {
+		struct cvrf_model *model = cvrf_model_import(import_source);
+		if(model == NULL) {
+			result = OSCAP_ERROR;
+			goto cleanup;
+		}
+		struct oscap_source *export_source = cvrf_model_get_export_source(model);
+		if (oscap_source_save_as(export_source, action->cvrf_action->f_output) != 0)
+			result = OSCAP_ERROR;
+		oscap_source_free(export_source);
+		cvrf_model_free(model);
 	}
-	struct oscap_source *export_source = cvrf_model_get_export_source(model);
-	if (oscap_source_save_as(export_source, action->cvrf_action->export_file) != 0)
-		result = OSCAP_ERROR;
-	oscap_source_free(export_source);
 
 	cleanup:
 		if (oscap_err())
 			fprintf(stderr, "%s %s\n", OSCAP_ERR_MSG, oscap_err_desc());
 
-	if (model)
-		cvrf_model_free(model);
 	oscap_source_free(import_source);
 	free(action->cvrf_action);
 	return result;
@@ -138,7 +153,7 @@ static int app_cvrf_export(const struct oscap_action *action) {
 
 static int app_cvrf_validate(const struct oscap_action *action) {
 	int result;
-	struct oscap_source *source = oscap_source_new_from_file(action->cvrf_action->cvrf_file);
+	struct oscap_source *source = oscap_source_new_from_file(action->cvrf_action->f_cvrf);
 	int ret = oscap_source_validate(source, reporter, (void *) action);
 
 	if (ret==-1) {
@@ -154,34 +169,45 @@ static int app_cvrf_validate(const struct oscap_action *action) {
 	return result;
 }
 
+// There will likely be more options needed in the future; this is
+// just a basic set up for the command line usage
+enum cvrf_opt {
+	CVRF_OPT_INDEX,
+	CVRF_OPT_RESULT_FILE,
+	CVRF_OPT_OUTPUT_FILE,
+};
+
 bool getopt_cvrf(int argc, char **argv, struct oscap_action *action) {
-	if(action->module == &CVRF_EVALUATE_MODULE) {
-		if( argc < 5 || argc > 6) {
-			oscap_module_usage(action->module, stderr, "Wrong number of parameters.\n");
-			return false;
+	action->doctype = OSCAP_DOCUMENT_CVRF_FEED;
+	action->cvrf_action = malloc(sizeof(struct cvrf_action));
+	struct cvrf_action *cvrf_action = action->cvrf_action;
+
+	static const struct option long_options[] = {
+		{"index", 0, NULL, CVRF_OPT_INDEX},
+		{"results", 1, NULL, CVRF_OPT_RESULT_FILE},
+		{"output", 1, NULL, CVRF_OPT_OUTPUT_FILE},
+		{0, 0, 0, 0}
+	};
+
+	int c;
+	while ((c = getopt_long(argc, argv, "+", long_options, NULL)) != -1) {
+		switch (c) {
+			case CVRF_OPT_INDEX:
+				cvrf_action->index = 1;
+				break;
+			case CVRF_OPT_RESULT_FILE:
+				cvrf_action->f_results = optarg;
+				break;
+			case CVRF_OPT_OUTPUT_FILE:
+				cvrf_action->f_output = optarg;
+				break;
+			default:
+				return oscap_module_usage(action->module, stderr, NULL);
 		}
-		action->doctype = OSCAP_DOCUMENT_CVRF_FEED;
-		action->cvrf_action = malloc(sizeof(struct cvrf_action));
-		action->cvrf_action->cvrf_file=argv[3];
-		action->cvrf_action->export_file=argv[4];
-	} else if (action->module == &CVRF_EXPORT_MODULE) {
-		if( argc < 5 || argc > 6) {
-			oscap_module_usage(action->module, stderr, "Wrong number of parameters.\n");
-			return false;
-		}
-		action->doctype = OSCAP_DOCUMENT_CVRF_FEED;
-		action->cvrf_action = malloc(sizeof(struct cvrf_action));
-		action->cvrf_action->cvrf_file=argv[3];
-		action->cvrf_action->export_file=argv[4];
-	} else if (action->module == &CVRF_VALIDATE_MODULE) {
-		if( argc < 4 || argc > 5) {
-			oscap_module_usage(action->module, stderr, "Wrong number of parameters.\n");
-			return false;
-		}
-		action->doctype = OSCAP_DOCUMENT_CVRF_FEED;
-		action->cvrf_action = malloc(sizeof(struct cvrf_action));
-		action->cvrf_action->cvrf_file=argv[3];
 	}
 
+	if (optind >= argc)
+		return oscap_module_usage(action->module, stderr, "CVRF file needs to be specified!\n");
+	cvrf_action->f_cvrf = argv[optind];
 	return true;
 }
