d7c424000bbd82c81c8a1986f379d277eeaca0ad
jlcharton <7339239+jlcharton@users.noreply.github.com>
2017-09-17 22:33:23 +0200

    Minor fix for AIX
    AIX requires <sys/socket.h> for AF_INET.

--- src/OVAL/probes/probe-api.c
+++ src/OVAL/probes/probe-api.c
@@ -38,6 +38,7 @@
 #include <errno.h>
 #include <arpa/inet.h> /* inet_pton() in probe_ent_from_cstr() */
 #include <netinet/in.h>
+#include <sys/socket.h>
 
 #include "debug_priv.h"
 #include "_probe-api.h"
