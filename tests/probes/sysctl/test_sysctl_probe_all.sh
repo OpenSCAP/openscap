87ac3e8ddb7db35a668548dd84b7e5515892e108
Jan Černý <jcerny@redhat.com>
2017-10-18 15:45:04 +0200

    Add tests from probes/sysctl directory
    Needed to amend the test case, because the probe binary is no longer
    called 'lt-probe_sysctl', but it's only 'probe_sysctl'.

--- tests/probes/sysctl/test_sysctl_probe_all.sh
+++ tests/probes/sysctl/test_sysctl_probe_all.sh
@@ -30,7 +30,7 @@ grep unix-sys:name "$result" | sed -E 's;.*>(.*)<.*;\1;g' | sort > "$ourNames"
 diff "$sysctlNames" "$ourNames"
 
 # remove oscap error message related to permissions from stderr
-sed -i -E "/^E: lt-probe_sysctl: Can't read sysctl value from /d" "$stderr"
+sed -i -E "/^E: probe_sysctl: Can't read sysctl value from /d" "$stderr"
 [ ! -s $stderr ]
 
 rm $stderr $result $ourNames $sysctlNames
