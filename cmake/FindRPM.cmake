5c61721a90337e45fd0fdc802f4366a9d1f25b3b
Jan Černý <jcerny@redhat.com>
2017-10-23 10:24:38 +0200

    Detect RPM version
    Some RPM probes are driven by variables like HAVE_RPM46 and HAVE_RPM47.
    This variables are set in root CMakeLists by checking of RPM46_FOUND
    and RPM47_FOUND and are exported to config.h during configuration.
    However the FindRPM.cmake module didn't define these variables.

--- cmake/FindRPM.cmake
+++ cmake/FindRPM.cmake
@@ -26,6 +26,10 @@ find_library(RPMIO_LIBRARY
 	PATHS ${RPM_PKGCONF_LIBRARY_DIRS}
 )
 
+set(RPM_VERSION ${RPM_PKGCONF_VERSION})
+string(COMPARE GREATER "4.6" ${RPM_VERSION} RPM46_FOUND)
+string(COMPARE GREATER "4.7" ${RPM_VERSION} RPM47_FOUND)
+
 # Set the include dir variables and the libraries and let libfind_process do the rest.
 # NOTE: Singular variables for this library, plural for libraries this this lib depends on.
 set(RPM_PROCESS_INCLUDES RPM_INCLUDE_DIR)
