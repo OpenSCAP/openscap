d8dd376d077d49fd2f8a5edf734cf5c8aba47ff1
Jan Černý <jcerny@redhat.com>
2017-12-12 13:20:01 +0100

    Add fallabck for getopt_long

--- compat/compat.h
+++ compat/compat.h
@@ -63,4 +63,30 @@ typedef SSIZE_T ssize_t;
 
 #endif
 
+#ifndef HAVE_GETOPT_H
+
+#define __getopt_argv_const const
+#define no_argument		0
+#define required_argument	1
+#define optional_argument	2
+
+char *optarg;
+int optind;
+int opterr;
+int optopt;
+
+struct option
+{
+	const char *name;
+	int has_arg;
+	int *flag;
+	int val;
+};
+
+int getopt_long(int ___argc, char *__getopt_argv_const *___argv,
+	const char *__shortopts,
+	const struct option *__longopts, int *__longind);
+
+#endif
+
 #endif
