aa6578355905b9ddffb4cb13d5fbc4d7a6121131
Martin Preisler <mpreisle@redhat.com>
2018-01-08 15:42:13 -0500

    Avoid repeating definition of assert_correct_xlinks in DS tests

--- tests/DS/test_ds_misc.sh
+++ tests/DS/test_ds_misc.sh
@@ -6,28 +6,9 @@
 set -e -o pipefail
 
 . $builddir/tests/test_common.sh
+. $srcdir/test_ds_common.sh
 
 # Test Cases.
-
-function assert_correct_xlinks()
-{
-	local DS=$1
-	local stderr=$(mktemp)
-	$OSCAP info $DS 2> $stderr
-	diff $stderr /dev/null
-	rm $stderr
-
-	# First of all make sure that there is at least one ds:component-ref.
-	[ "$($XPATH $DS 'count(//*[local-name()="component-ref"])')" != "0" ]
-	# We want to catch cases when this element has different namespace.
-	local ns=$($XPATH $DS 'name(//*[local-name()="component-ref"][1])' | sed 's/:.*$/:/')
-	[ "$ns" != "component-ref" ] || ns=""
-	# Ensure there is at least some xlink.
-	[ "`$XPATH $DS \"count(//${ns}component-ref/@xlink:href)\"`" != "0" ]
-	# This asserts that there is none component-ref/xlink:href broken.
-	# Previously, we have seen datastreams with broken xlinks (see trac#286).
-	[ "`$XPATH $DS  \"count(//${ns}component-ref[substring(@xlink:href, 2, 10000) != (//${ns}component/@${ns}id | //${ns}extended-component/@${ns}id)])\"`" == "0" ]
-}
 
 sds_add_multiple_twice(){
 	local DIR="${srcdir}/sds_multiple_oval"
