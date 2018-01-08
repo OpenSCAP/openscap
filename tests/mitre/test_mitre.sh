4901aad4d3057de9b42c545b33dfbd383223d013
Martin Preisler <mpreisle@redhat.com>
2018-01-08 16:43:22 -0500

    Split test_mitre_unix_probes from test_mitre.sh

--- tests/mitre/test_mitre.sh
+++ tests/mitre/test_mitre.sh
@@ -11,9 +11,6 @@
 # Test Cases.
 test_init "test_mitre.log"
 
-# does not work because of symplink `/etc/rc' -> `/etc/rc.d/rc' (oval:org.mitre.oval.test:tst:102)
-#test_run "unix-def_file_test.xml" test_mitre unix-def_file_test.xml "true"
-
 test_run "oval_binary_datatype.xml" test_mitre oval_binary_datatype.xml "true"
 test_run "oval_boolean_datatype.xml" test_mitre oval_boolean_datatype.xml "true"
 test_run "oval_check_enumeration_entity.xml" test_mitre oval_check_enumeration_entity.xml "true"
@@ -51,35 +48,5 @@ test_run "oval_float_datatype.xml" test_mitre oval_float_datatype.xml "true"
 test_run "oval_int_datatype.xml" test_mitre oval_int_datatype.xml "true"
 test_run "oval_string_datatype.xml" test_mitre oval_string_datatype.xml "true"
 test_run "oval_version_datatype.xml" test_mitre oval_version_datatype.xml "true"
-
-test_run "unix-def_password_test.xml" test_mitre unix-def_password_test.xml "true"
-
-# these are outdated
-#test_run "unix-def_process58_test.xml" test_mitre unix-def_process58_test.xml "true"
-#test_run "unix-def_process_test.xml" test_mitre unix-def_process_test.xml "true"
-
-# Fedora 16 and RHEL-7 - no runlevel
-if [[ ( ${DISTRO#Fedora} != "$DISTRO" && $DISTRO_RELEASE -lt 16 ) || \
-	( ${DISTRO#Red Hat} != "$DISTRO" && $DISTRO_RELEASE -lt 7 ) ]]; then
-	test_run "unix-def_runlevel_test.xml" test_mitre unix-def_runlevel_test.xml "true"
-fi
-
-test_run "unix-def_uname_test.xml" test_mitre unix-def_uname_test.xml "true"
-
-# needs only two interfaces - lo, eth0 - without ipv6 addresses
-#test_run "unix-def_interface_test.xml" test_mitre unix-def_interface_test.xml "true"
-
-# root needed
-if [[ $(id -u) == 0 ]]; then
-	test_run "unix-def_shadow_test.xml" test_mitre unix-def_shadow_test.xml "true"
-fi
-
-# install xinetd, telnet-server and tftp-server in order to test xinetd probe
-if [[ -f "/etc/xinetd.conf" && -f "/etc/xinetd.d/tftp" && -f "/etc/xinetd.d/telnet" ]]; then
-	test_run "unix-def_xinetd_test.xml" test_mitre unix-def_xinetd_test.xml "true"
-fi
-
-# Unsupported objects on Fedora
-#test_run "unix-def_inetd_test.xml" test_mitre unix-def_inetd_test.xml "unknown"
 
 test_exit cleanup_mitre
---
+++ tests/mitre/test_mitre_unix_probes.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+# Author:
+#      Peter Vrabec, <pvrabec@redhat.com>
+#	   Daniel Kopecek <dkopecek@redhat.com>
+
+
+. $builddir/tests/test_common.sh
+. $srcdir/test_mitre_common.sh
+
+# Test Cases.
+test_init "test_mitre_unix_probes.log"
+
+# does not work because of symplink `/etc/rc' -> `/etc/rc.d/rc' (oval:org.mitre.oval.test:tst:102)
+#test_run "unix-def_file_test.xml" test_mitre unix-def_file_test.xml "true"
+
+test_run "unix-def_password_test.xml" test_mitre unix-def_password_test.xml "true"
+
+# these are outdated
+#test_run "unix-def_process58_test.xml" test_mitre unix-def_process58_test.xml "true"
+#test_run "unix-def_process_test.xml" test_mitre unix-def_process_test.xml "true"
+
+# Fedora 16 and RHEL-7 - no runlevel
+if [[ ( ${DISTRO#Fedora} != "$DISTRO" && $DISTRO_RELEASE -lt 16 ) || \
+	( ${DISTRO#Red Hat} != "$DISTRO" && $DISTRO_RELEASE -lt 7 ) ]]; then
+	test_run "unix-def_runlevel_test.xml" test_mitre unix-def_runlevel_test.xml "true"
+fi
+
+test_run "unix-def_uname_test.xml" test_mitre unix-def_uname_test.xml "true"
+
+# needs only two interfaces - lo, eth0 - without ipv6 addresses
+#test_run "unix-def_interface_test.xml" test_mitre unix-def_interface_test.xml "true"
+
+# root needed
+if [[ $(id -u) == 0 ]]; then
+	test_run "unix-def_shadow_test.xml" test_mitre unix-def_shadow_test.xml "true"
+fi
+
+# install xinetd, telnet-server and tftp-server in order to test xinetd probe
+if [[ -f "/etc/xinetd.conf" && -f "/etc/xinetd.d/tftp" && -f "/etc/xinetd.d/telnet" ]]; then
+	test_run "unix-def_xinetd_test.xml" test_mitre unix-def_xinetd_test.xml "true"
+fi
+
+# Unsupported objects on Fedora
+#test_run "unix-def_inetd_test.xml" test_mitre unix-def_inetd_test.xml "unknown"
+
+test_exit cleanup_mitre
