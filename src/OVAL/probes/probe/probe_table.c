66ba26d12fe72d1a33a7d69ad9d7cf513122f346
Jan Černý <jcerny@redhat.com>
2018-02-22 12:30:13 +0100

    Add a probe table
    This table maps OVAL objects to functions that can evaluate them.

---
+++ src/OVAL/probes/probe/probe_table.c
@@ -0,0 +1,81 @@
+/*
+ * Copyright 2018 Red Hat Inc., Durham, North Carolina.
+ * All Rights Reserved.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Authors:
+ *      "Jan Černý" <jcerny@redhat.com>
+ */
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include "probe_table.h"
+#include "independent/system_info.h"
+#include "independent/family.h"
+#include "oval_types.h"
+#include "independent/textfilecontent54.h"
+
+typedef struct probe_table_entry {
+	oval_subtype_t type;
+	probe_init_function_t probe_init_function;
+	probe_main_function_t probe_main_function;
+	probe_fini_function_t probe_fini_function;
+	probe_offline_mode_function_t probe_offline_mode_function;
+} probe_table_entry_t;
+
+static const probe_table_entry_t probe_table[] = {
+	/* {type, init, main, fini, offline} */
+	{OVAL_INDEPENDENT_FAMILY, family_probe_init, family_probe_main, NULL, NULL},
+	{OVAL_INDEPENDENT_TEXT_FILE_CONTENT_54, textfilecontent54_probe_init, textfilecontent54_probe_main, NULL, NULL},
+	{OVAL_SUBTYPE_SYSINFO, system_info_probe_init, system_info_probe_main, NULL, NULL},
+	{OVAL_SUBTYPE_UNKNOWN, NULL, NULL, NULL, NULL}
+};
+
+static probe_table_entry_t *probe_table_get(oval_subtype_t type)
+{
+	probe_table_entry_t *entry = probe_table;
+	while (entry->probe_main_function != NULL && entry->type != type)
+	{
+		entry++;
+	}
+	return entry;
+}
+
+probe_init_function_t probe_table_get_init_function(oval_subtype_t type)
+{
+	probe_table_entry_t *entry = probe_table_get(type);
+	return entry->probe_init_function;
+}
+
+probe_main_function_t probe_table_get_main_function(oval_subtype_t type)
+{
+	probe_table_entry_t *entry = probe_table_get(type);
+	return entry->probe_main_function;
+}
+
+probe_fini_function_t probe_table_get_fini_function(oval_subtype_t type)
+{
+	probe_table_entry_t *entry = probe_table_get(type);
+	return entry->probe_fini_function;
+}
+
+probe_offline_mode_function_t probe_table_get_offline_mode_function(oval_subtype_t type)
+{
+	probe_table_entry_t *entry = probe_table_get(type);
+	return entry->probe_offline_mode_function;
+}
---
+++ src/OVAL/probes/probe/probe_table.h
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2018 Red Hat Inc., Durham, North Carolina.
+ * All Rights Reserved.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Authors:
+ *      "Jan Černý" <jcerny@redhat.com>
+ */
+
+
+#ifndef PROBE_TABLE_H
+#define PROBE_TABLE_H
+
+#include "probe-api.h"
+
+typedef void *(*probe_init_function_t)(void);
+typedef int (*probe_main_function_t)(probe_ctx *ctx, void *arg);
+typedef void (*probe_fini_function_t)(void *probe_arg);
+typedef void (*probe_offline_mode_function_t)(void);
+
+probe_init_function_t probe_table_get_init_function(oval_subtype_t type);
+probe_main_function_t probe_table_get_main_function(oval_subtype_t type);
+probe_fini_function_t probe_table_get_fini_function(oval_subtype_t type);
+probe_offline_mode_function_t probe_table_get_offline_mode_function(oval_subtype_t type);
+
+#endif //OPENSCAP_PROBE_TABLE_H
