04d213e1837985c16bb96f54816e4c62ca560490
Jan Černý <jcerny@redhat.com>
2018-05-04 13:41:35 +0200

    Use items() method instead of iteritems() to iterate dictionary
    iteritems() is not available in Python 3, items() is available
    in both Python 2 and 3

--- tests/sce/script_tester.py
+++ tests/sce/script_tester.py
@@ -52,12 +52,12 @@ xccdf_result_map["XCCDF_RESULT_INFORMATIONAL"] = 108
 xccdf_result_map["XCCDF_RESULT_FIXED"] = 109
 
 xccdf_reverse_result_map = dict()
-for key, value in xccdf_result_map.iteritems():
+for key, value in xccdf_result_map.items():
     xccdf_reverse_result_map[value] = key
 
 # set all XCCDF result types as environment variables
 # (this is especially useful for bash scripts)
-for key, value in xccdf_result_map.iteritems():
+for key, value in xccdf_result_map.items():
     os.environ[key] = str(value)
 
 import subprocess
