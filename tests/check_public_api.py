041d0a01baafc3323aa8d660f8c49ae3e13ab85e
Jan Černý <jcerny@redhat.com>
2018-08-14 17:32:26 +0200

    Improve the code style
    Adds docstrings, fixes some PEP8 problems, improves readability,
    uses sets by default without a need to typecast.

--- tests/check_public_api.py
+++ tests/check_public_api.py
@@ -17,27 +17,32 @@ def get_public_symbols_from_shared_object(prefix):
     nm_command = ["nm", "--demangle", "--dynamic", "--defined-only", so_path]
     nm_output_bytes = subprocess.check_output(nm_command)
     nm_output = nm_output_bytes.decode(encoding="utf-8")
-    symbols = [line.split()[2] for line in nm_output.splitlines()]
-    return set(symbols)
+    symbols = {line.split()[2] for line in nm_output.splitlines()}
+    return symbols
 
 
 def get_public_headers(prefix):
     """
     It is useful to check if all the public headers are actually installed under $PREFIX/include/openscap.
     :param prefix: CMAKE_INSTALL_PREFIX
-    :return: a set of public headers file paths
+    :return: a set of public headers filepaths
     """
-    public_headers = []
+    public_headers = set()
     include_path = os.path.join(prefix, "include", "openscap")
     for root, dirs, files in os.walk(include_path):
         for f in files:
             full_path = os.path.join(root, f)
-            public_headers.append(full_path)
-    return set(public_headers)
+            public_headers.add(full_path)
+    return public_headers
 
 
-def _get_public_symbols_from_header(header_path):
-    symbols = []
+def get_public_symbols_from_header(header_path):
+    """
+    Parses a header file to find symbols marked by OSCAP_API macro.
+    :param header_path: path to the header file
+    :return: a list of public symbols in the header file
+    """
+    symbols = set()
     prototype_regex = re.compile(r"^[ \t]*OSCAP_API[^;]*;", re.MULTILINE)
     with open(header_path, "r") as header_file:
         content = header_file.read()
@@ -47,7 +52,7 @@ def _get_public_symbols_from_header(header_path):
         p = p.replace("\n", " ")
         f_match = function_name_regex.search(p)
         f_name = f_match.group(1) if f_match else "unknown"
-        symbols.append(f_name)
+        symbols.add(f_name)
     return symbols
 
 
@@ -56,18 +61,25 @@ def get_public_symbols_from_headers(headers):
     Find public (exported) symbols in header files by finding symbols
     marked as OSCAP_API.
     :param headers: a list of public headers file paths
-    :return: a set of public symbols of public headers file paths
+    :return: a set of public symbols in public headers
     """
-    symbols = []
+    symbols = set()
     for h in headers:
-        symbols.extend(_get_public_symbols_from_header(h))
-    return set(symbols)
+        symbols.update(get_public_symbols_from_header(h))
+    return symbols
 
 
-def analyze(src_dir):
+def analyze_project_artifacts(src_dir):
+    """
+    Analyze the project and find all the symbols declared as public in
+    public header files and all the symbols actually exported in built
+    library. This is done on a real build and a real installation.
+    The artifacts are installed in a temporary directory which is
+    automatically removed when this function returns.
+    :param src_dir: Path to project sources
+    :return: tuple (header_symbols, so_symbols)
+    """
     cwd = os.getcwd()
-    #so_symbols = set()
-    #header_symbols = set()
     with tempfile.TemporaryDirectory() as prefix:
         build_dir = os.path.join(src_dir, "build")
         os.chdir(build_dir)
@@ -80,7 +92,8 @@ def analyze(src_dir):
         subprocess.run(cmake_command)
         make_command = ["make"]
         subprocess.run(make_command)
-        # TODO: remove make docs after https://github.com/OpenSCAP/openscap/issues/1161 is resolved
+        # TODO: remove make docs after
+        # https://github.com/OpenSCAP/openscap/issues/1161 is resolved
         make_docs_command = ["make", "docs"]
         subprocess.run(make_docs_command)
         make_install_command = ["make", "install"]
@@ -94,20 +107,25 @@ def analyze(src_dir):
 
 def main():
     src_dir = os.path.join(os.getcwd(), "..")
-    header_symbols, so_symbols = analyze(src_dir)
+    header_symbols, so_symbols = analyze_project_artifacts(src_dir)
     print("Shared object symbols: %d" % len(so_symbols))
-    print("Public header symbols: %d" % len(header_symbols))
+    print("Public header symbols: %d\n" % len(header_symbols))
     print()
+
     so_only = so_symbols.difference(header_symbols)
-    print("The following %d symbols are exported in binary, but are not present in public header files:" % len(so_only))
+    print("The following %d symbols are exported in binary, " \
+            "but are not present in public header files:" % len(so_only))
     for s in sorted(so_only):
         print(s)
-    header_only = header_symbols.difference(so_symbols)
     print()
-    print("The following %d symbols are present in public header files, but are not exported in binary:" % len(header_only))
+
+    header_only = header_symbols.difference(so_symbols)
+    print("The following %d symbols are present in public header files, " \
+            "but are not exported in binary:" % len(header_only))
     for s in sorted(header_only):
         print(s)
 
 
 if __name__ == "__main__":
     main()
+
