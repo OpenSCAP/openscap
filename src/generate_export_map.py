0ce12c76abaca37c29a852c6cc1474d29b8b1a3a
Jan Černý <jcerny@redhat.com>
2018-08-21 14:53:22 +0200

    Define symbols that are exported in DSO using an export map
    The export map will be generated automatically from the header
    files by a Python scripts. This way prevents us from having
    to maintain an explicit list. All the decalrations labeled by
    OSCAP_API macros in *public* header files will be exported.
    All the remaining symbols will be hidden.
    For more details, please see especially Section 2.2.5.
    https://www.akkadia.org/drepper/dsohowto.pdf

---
+++ src/generate_export_map.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python3
+
+import os
+import sys
+import re
+import argparse
+
+
+def find_public_headers(srcdir):
+    public_headers = []
+    for dirpath, dirnames, filenames in os.walk(srcdir):
+        if dirpath.endswith("/public"):
+            for name in filenames:
+                if name.endswith(".h"):
+                    full_path = os.path.abspath(os.path.join(dirpath, name))
+                    public_headers.append(full_path)
+    return public_headers
+
+
+def get_public_symbols_from_header(header_path):
+    symbols = set()
+    prototype_regex = re.compile(r"^[ \t]*OSCAP_API[^;]*;", re.MULTILINE)
+    with open(header_path, "r") as header_file:
+        content = header_file.read()
+    prototypes = prototype_regex.findall(content)
+    function_name_regex = re.compile(r"([a-zA-Z0-9_]+)\s*\(.*\)")
+    data_name_regex = re.compile(r"\s+([a-zA-Z0-9_]+)\s*;")
+    for p in prototypes:
+        p = p.replace("\n", " ")
+        match = function_name_regex.search(p)
+        if not match:
+            match = data_name_regex.search(p)
+        if not match:
+            print("Invalid prototype '%s'" % p, file=sys.stderr)
+            continue
+        symbol_name = match.group(1)
+        symbols.add(symbol_name)
+    return symbols
+
+
+def get_all_public_symbols(srcdir):
+    symbols = set()
+    public_headers = find_public_headers(srcdir)
+    for header in public_headers:
+        symbols.update(get_public_symbols_from_header(header))
+    return sorted(symbols)
+
+
+def generate_export_map(symbols, file):
+    prolog = "{\n" \
+             "    global:\n"
+    file.write(prolog)
+    for s in symbols:
+        file.write("        %s;\n" % s)
+    epilog = "    local: *;\n" \
+             "};\n"
+    file.write(epilog)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("srcdir",
+                        help="Source code directory")
+    parser.add_argument("--output",
+                        type=argparse.FileType('w'),
+                        default=sys.stdout,
+                        help="Specify file path to write the export map")
+    args = parser.parse_args()
+    symbols = get_all_public_symbols(args.srcdir)
+    generate_export_map(symbols, args.output)
