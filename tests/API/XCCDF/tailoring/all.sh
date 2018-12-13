e3bd2eddeb1e75adda6ced63833d64c5d49c40ad
Jan Černý <jcerny@redhat.com>
2018-12-13 14:58:19 +0100

    Test profile that customizes set of selected rules

--- tests/API/XCCDF/tailoring/all.sh
+++ tests/API/XCCDF/tailoring/all.sh
@@ -98,6 +98,24 @@ function test_api_xccdf_tailoring_simple_include_in_arf {
     rm -f $result
 }
 
+function test_api_xccdf_tailoring_profile_include_in_arf {
+    local INPUT=$srcdir/$1
+    local TAILORING=$srcdir/$2
+
+    result=`mktemp`
+    # "xccdf_com.example.www_profile_customized" customizes "xccdf_com.example.www_profile_baseline1"
+    $OSCAP xccdf eval --tailoring-file $TAILORING --profile "xccdf_com.example.www_profile_customized" --results-arf $result $INPUT || [ "$?" == "2" ]
+
+    component_xpath='/arf:asset-report-collection/arf:report-requests/arf:report-request/arf:content/ds:data-stream-collection/ds:component'
+    assert_exists 3 $component_xpath
+    assert_exists 3 $component_xpath'/@timestamp'
+    assert_exists 1 $component_xpath'/xccdf:Tailoring'
+    assert_exists 1 $component_xpath'/xccdf:Tailoring/xccdf:Profile'
+    assert_exists 1 $component_xpath'/xccdf:Tailoring/xccdf:Profile[@id="xccdf_com.example.www_profile_customized"]'
+    assert_exists 1 $component_xpath'/xccdf:Tailoring/xccdf:Profile[@extends="xccdf_com.example.www_profile_baseline1"]'
+    rm -f $result
+}
+
 # Testing.
 
 test_init "test_api_xccdf_tailoring.log"
@@ -116,5 +134,7 @@ test_run "test_api_xccdf_tailoring_oscap_info_11" test_api_xccdf_tailoring_oscap
 test_run "test_api_xccdf_tailoring_oscap_info_12" test_api_xccdf_tailoring_oscap_info simple-tailoring.xml 1
 test_run "test_api_xccdf_tailoring_autonegotiation" test_api_xccdf_tailoring_autonegotiation simple-tailoring-autonegotiation.xml xccdf_org.open-scap_profile_default 1
 test_run "test_api_xccdf_tailoring_simple_include_in_arf" test_api_xccdf_tailoring_simple_include_in_arf simple-xccdf.xml simple-tailoring.xml
+test_run "test_api_xccdf_tailoring_profile_include_in_arf" test_api_xccdf_tailoring_profile_include_in_arf baseline.xccdf.xml baseline.tailoring.xml
+
 
 test_exit
