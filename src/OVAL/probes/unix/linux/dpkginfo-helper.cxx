d328d01ba632a49a57c44fb8075a0753a07c5cac
Julian Andres Klode <julian.klode@canonical.com>
2019-06-18 11:57:58 +0200

    Include additional headers in dkpginfo-helper for apt 1.9
    apt 1.9 cleans up the header includes a bit, causing the build
    to fail, so add the additional headers we are missing.

--- src/OVAL/probes/unix/linux/dpkginfo-helper.cxx
+++ src/OVAL/probes/unix/linux/dpkginfo-helper.cxx
@@ -9,9 +9,12 @@
 
 #include <apt-pkg/init.h>
 #include <apt-pkg/error.h>
+#include <apt-pkg/configuration.h>
+#include <apt-pkg/fileutl.h>
 #include <apt-pkg/mmap.h>
 #include <apt-pkg/pkgcache.h>
 #include <apt-pkg/pkgrecords.h>
+#include <apt-pkg/pkgsystem.h>
 
 #include "dpkginfo-helper.h"
 
