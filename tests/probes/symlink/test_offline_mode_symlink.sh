e5a0cb22f4bf040f2a64fb7cb442f7b80e8d571c
Vojtech Polasek <vpolasek@redhat.com>
2019-12-03 09:14:37 +0100

    fix tests based on real runs
    used WIP patch of Openscap, so far tests can't be still ran

--- tests/probes/symlink/test_offline_mode_symlink.sh
+++ tests/probes/symlink/test_offline_mode_symlink.sh
@@ -34,7 +34,7 @@ function test_offline_mode_symlink {
     ln -s /etc/shadow $tmpdir/etc/passwd
     touch $tmpdir/file_in_root
     ln -s ../../../../../file_in_root $tmpdir/path_traversal_symlink
-    ln -s ./some_file $tmpdir/symlink_with_period_in_path
+    ln -s ./some_file $tmpdir/symlink_with_dot_in_path
     touch /tmp/symlinktarget
     ln -s /tmp/symlinktarget /tmp/symlinktest
 
@@ -42,7 +42,7 @@ function test_offline_mode_symlink {
 
     bash ${srcdir}/test_offline_mode_symlink.xml.sh "" > "$DF"
     export OSCAP_PROBE_ROOT="$tmpdir"
-    $OSCAP oval eval --verbose INFO --results $RF $DF
+    $OSCAP oval eval --results $RF $DF
 
     result=$RF
 
@@ -54,21 +54,18 @@ function test_offline_mode_symlink {
     p='oval_results/results/system/oval_system_characteristics/'
     assert_exists 12 $p'collected_objects/object'
     assert_exists 6 $p'collected_objects/object[@flag="complete"]'
-    assert_exists 9 $p'system_data/unix-sys:symlink_item'
+    assert_exists 6 $p'system_data/unix-sys:symlink_item'
     assert_exists 6 $p'system_data/unix-sys:symlink_item[@status="exists"]'
     assert_exists 1 $p'system_data/unix-sys:symlink_item/unix-sys:filepath[text()="'/normal_symlink'"]'
     assert_exists 1 $p'system_data/unix-sys:symlink_item/unix-sys:filepath[text()="'/normal_symlink_relative'"]'
-    assert_exists 2 $p'system_data/unix-sys:symlink_item/unix-sys:canonical_path[text()="/some_file"]'
+    assert_exists 3 $p'system_data/unix-sys:symlink_item/unix-sys:canonical_path[text()="/some_file"]'
     assert_exists 1 $p'system_data/unix-sys:symlink_item/unix-sys:filepath[text()="'/chained_symlink'"]'
     assert_exists 1 $p'system_data/unix-sys:symlink_item/unix-sys:canonical_path[text()="/other_file"]'
     assert_exists 1 $p'system_data/unix-sys:symlink_item/unix-sys:filepath[text()="'/etc/passwd'"]'
     assert_exists 1 $p'system_data/unix-sys:symlink_item/unix-sys:canonical_path[text()="/etc/shadow"]'
-    assert_exists 1 $p'system_data/unix-sys:symlink_item/unix-sys:filepath[text()="'/chained_symlink_relative'"]'
-    assert_exists 1 $p'system_data/unix-sys:symlink_item/unix-sys:canonical_path[text()="/other_file"]'
     assert_exists 1 $p'system_data/unix-sys:symlink_item/unix-sys:filepath[text()="'/path_traversal_symlink'"]'
     assert_exists 1 $p'system_data/unix-sys:symlink_item/unix-sys:canonical_path[text()="/file_in_root"]'
     assert_exists 1 $p'system_data/unix-sys:symlink_item/unix-sys:filepath[text()="'/symlink_with_dot_in_path'"]'
-    assert_exists 1 $p'system_data/unix-sys:symlink_item/unix-sys:canonical_path[text()="/some_file"]'
     assert_exists 3 $p'collected_objects/object[@flag="error"]'
     assert_exists 3 $p'collected_objects/object[@flag="does not exist"]'
     assert_exists 6 $p'collected_objects/object/message'
