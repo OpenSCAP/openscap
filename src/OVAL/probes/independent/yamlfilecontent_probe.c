6b20c53d694b5608bfb302e670df8b3a5222b93c
Jan Černý <jcerny@redhat.com>
2020-02-14 14:22:17 +0100

    Extract input data from probe context

--- src/OVAL/probes/independent/yamlfilecontent_probe.c
+++ src/OVAL/probes/independent/yamlfilecontent_probe.c
@@ -25,9 +25,28 @@
 #endif
 
 #include "yamlfilecontent_probe.h"
+#include "sexp-manip.h"
 
 
 int yamlfilecontent_probe_main(probe_ctx *ctx, void *arg)
 {
+	SEXP_t *probe_in = probe_ctx_getobject(ctx);
+	SEXP_t *behaviors_ent = probe_obj_getent(probe_in, "behaviors", 1);
+	SEXP_t *filepath_ent = probe_obj_getent(probe_in, "filepath", 1);
+	SEXP_t *path_ent = probe_obj_getent(probe_in, "path", 1);
+	SEXP_t *filename_ent = probe_obj_getent(probe_in, "filename", 1);
+	SEXP_t *yamlpath_ent = probe_obj_getent(probe_in, "yamlpath", 1);
+	SEXP_t *yamlpath_val = probe_ent_getval(yamlpath_ent);
+	char *yamlpath_str = SEXP_string_cstr(yamlpath_val);
+
+
+cleanup:
+	free(yamlpath_str);
+	SEXP_free(yamlpath_val);
+	SEXP_free(yamlpath_ent);
+	SEXP_free(filename_ent);
+	SEXP_free(path_ent);
+	SEXP_free(filepath_ent);
+	SEXP_free(behaviors_ent);
 	return 0;
 }
