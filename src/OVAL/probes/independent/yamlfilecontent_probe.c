723c46f545f8a09c9945249f3051a7f68620b28b
Jan Černý <jcerny@redhat.com>
2020-02-18 09:37:19 +0100

    Query YAML files using yaml-path
    At this moment the raw returned data is directly used to populate
    the 'value_of' element.

--- src/OVAL/probes/independent/yamlfilecontent_probe.c
+++ src/OVAL/probes/independent/yamlfilecontent_probe.c
@@ -24,16 +24,112 @@
 #include <config.h>
 #endif
 
+#include <errno.h>
+#include <yaml.h>
+#include <yaml-path.h>
+
 #include "yamlfilecontent_probe.h"
 #include "sexp-manip.h"
 #include "debug_priv.h"
 #include "oval_fts.h"
 
+
+static int yaml_path_query(const char *filepath, const char *yaml_path_cstr, unsigned char *output_buffer, size_t output_buffer_size, probe_ctx *ctx)
+{
+	int ret = 0;
+	FILE *yaml_file = fopen(filepath, "r");
+	if (yaml_file == NULL) {
+		SEXP_t *msg = probe_msg_creatf(OVAL_MESSAGE_LEVEL_ERROR,
+			"Unable to open file '%s': %s", filepath, strerror(errno));
+		probe_cobj_add_msg(probe_ctx_getresult(ctx), msg);
+		SEXP_free(msg);
+		probe_cobj_set_flag(probe_ctx_getresult(ctx), SYSCHAR_FLAG_ERROR);
+		return -1;
+	}
+
+	yaml_path_t *yaml_path = yaml_path_create();
+	if (yaml_path_parse(yaml_path, (char *) yaml_path_cstr)) {
+		SEXP_t *msg = probe_msg_creatf(OVAL_MESSAGE_LEVEL_ERROR,
+			"Invalid YAML path '%s' (%s)\n", yaml_path_cstr,
+			yaml_path_error_get(yaml_path)->message);
+		probe_cobj_add_msg(probe_ctx_getresult(ctx), msg);
+		SEXP_free(msg);
+		probe_cobj_set_flag(probe_ctx_getresult(ctx), SYSCHAR_FLAG_ERROR);
+		fclose(yaml_file);
+		return -1;
+	};
+
+	yaml_parser_t parser;
+	yaml_parser_initialize(&parser);
+	yaml_parser_set_input_file(&parser, yaml_file);
+
+	yaml_emitter_t emitter;
+	yaml_emitter_initialize(&emitter);
+	size_t size_written;
+	yaml_emitter_set_output_string(&emitter,
+		output_buffer, output_buffer_size, &size_written);
+	yaml_emitter_set_width(&emitter, -1);
+
+	yaml_event_t event;
+	bool done = false;
+
+	do {
+		if (yaml_parser_parse(&parser, &event)) {
+			done = (event.type == YAML_STREAM_END_EVENT);
+			if (yaml_path_filter_event(yaml_path, &parser, &event,
+					YAML_PATH_FILTER_RETURN_ALL)) {
+				if (!yaml_emitter_emit(&emitter, &event)) {
+					SEXP_t *msg = probe_msg_creatf(OVAL_MESSAGE_LEVEL_ERROR,
+						"YAML emitter error: yaml_emitter_emit returned 0: %s",
+						emitter.problem);
+					probe_cobj_add_msg(probe_ctx_getresult(ctx), msg);
+					SEXP_free(msg);
+					probe_cobj_set_flag(probe_ctx_getresult(ctx), SYSCHAR_FLAG_ERROR);
+					ret = -1;
+					goto cleanup;
+				}
+			} else {
+				yaml_event_delete(&event);
+			}
+		} else {
+			SEXP_t *msg = probe_msg_creatf(OVAL_MESSAGE_LEVEL_ERROR,
+				"YAML parser error: yaml_parse_parse returned 0: %s",
+				parser.problem);
+			probe_cobj_add_msg(probe_ctx_getresult(ctx), msg);
+			SEXP_free(msg);
+			probe_cobj_set_flag(probe_ctx_getresult(ctx), SYSCHAR_FLAG_ERROR);
+			ret = -1;
+			goto cleanup;
+		}
+	} while (!done);
+
+	/* string output_buffer contains '\n' at the end */
+	output_buffer[size_written - 1] = '\0';
+
+cleanup:
+	yaml_parser_delete(&parser);
+	yaml_emitter_delete(&emitter);
+
+	yaml_path_destroy(yaml_path);
+	fclose(yaml_file);
+
+	return ret;
+}
+
 static int process_yaml_file(const char *path, const char *filename, const char *yamlpath, probe_ctx *ctx)
 {
+	int ret = 0;
 	char *filepath = oscap_path_join(path, filename);
 
-	/* TODO: insert code using libyaml-yamlpath-filter here */
+	size_t output_buffer_size = 1024;
+	unsigned char *output_buffer = calloc(output_buffer_size, sizeof(unsigned char));
+
+	if (yaml_path_query(filepath, yamlpath, output_buffer, output_buffer_size, ctx)) {
+		ret = -1;
+		goto cleanup;
+	}
+
+	/* TODO: type conversion of output_buffer data */
 
 	SEXP_t *item = probe_item_create(
 		OVAL_INDEPENDENT_YAML_FILE_CONTENT,
@@ -42,15 +138,18 @@ static int process_yaml_file(const char *path, const char *filename, const char 
 		"path", OVAL_DATATYPE_STRING, path,
 		"filename", OVAL_DATATYPE_STRING, filename,
 		"yamlpath", OVAL_DATATYPE_STRING, yamlpath,
+		"value_of", OVAL_DATATYPE_STRING, output_buffer,
 		/*
-		"value_of",
 		"windows_view",
 		*/
 		NULL
 	);
 	probe_item_collect(ctx, item);
+
+cleanup:
+	free(output_buffer);
 	free(filepath);
-	return 0;
+	return ret;
 }
 
 int yamlfilecontent_probe_main(probe_ctx *ctx, void *arg)
@@ -81,7 +180,6 @@ int yamlfilecontent_probe_main(probe_ctx *ctx, void *arg)
 		oval_fts_close(ofts);
 	}
 
-cleanup:
 	free(yamlpath_str);
 	SEXP_free(yamlpath_val);
 	SEXP_free(yamlpath_ent);
