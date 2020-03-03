b2b00e2316439d6517c8b763f542db637ebcf0b0
Matus Marhefka <mmarhefk@redhat.com>
2020-03-03 14:12:39 +0100

    tests/probes/environmentvariable58: add test to cover 7c78b84dd

--- tests/probes/environmentvariable58/test_probes_environmentvariable58.sh
+++ tests/probes/environmentvariable58/test_probes_environmentvariable58.sh
@@ -24,13 +24,16 @@ function test_probes_environmentvariable58 {
     local ret_val=0;
     local DF="$1.xml"
     local RF="$1.results.xml"
+    echo "result file: $RF"
+    local stderr=$(mktemp $1.err.XXXXXX)
+    echo "stderr file: $stderr"
 
     [ -f $RF ] && rm -f $RF
 
     bash ${srcdir}/$1.xml.sh > $DF
     LINES=$?
 
-    $OSCAP oval eval --results $RF $DF
+    $OSCAP oval eval --results $RF $DF 2> $stderr
 
     if [ -f $RF ]; then
 	verify_results "def" $DF $RF 1 && verify_results "tst" $DF $RF $LINES
@@ -39,6 +42,11 @@ function test_probes_environmentvariable58 {
 	ret_val=1
     fi
 
+    # Test fails if there are any warnings or "Entity has no value" on stderr.
+    echo "Verify that there are no warnings on stderr"
+    grep -Ei "(W:|Entity has no value)" $stderr && ret_val=1
+
+    rm $stderr
     return $ret_val
 }
 
