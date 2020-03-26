e461362989da3bd28102f7a9ccce53c5c9f0b58d
Evgeny Kolesnikov <ekolesni@redhat.com>
2020-03-27 00:25:28 +0100

    Add tests for selinuxsecuritycontext probe

---
+++ tests/probes/selinuxsecuritycontext/test_probes_selinuxsecuritycontext.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+. $builddir/tests/test_common.sh
+
+function test_probes_selinuxsecuritycontext {
+    # if the user doesn't have SELinux enabled we will skip the test
+    selinuxenabled || return 255
+
+    probecheck "selinuxboolean" || return 255
+
+    local ret_val=0;
+    local DF="${srcdir}/test_probes_selinuxsecuritycontext.xml"
+    local RF="test_probes_selinuxsecuritycontext.results.xml"
+
+    [ -f $RF ] && rm -f $RF
+
+    $OSCAP oval eval --results $RF $DF
+
+    if [ -f $RF ]; then
+        verify_results "def" $DF $RF 1 && verify_results "tst" $DF $RF 1
+        ret_val=$?
+    else
+        ret_val=1
+    fi
+
+    return $ret_val
+}
+
+test_probes_selinuxsecuritycontext
---
+++ tests/probes/selinuxsecuritycontext/test_probes_selinuxsecuritycontext_offline_mode.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+. $builddir/tests/test_common.sh
+
+function test_probes_selinuxsecuritycontext_offline_mode {
+    # if the user doesn't have SELinux enabled we will skip the test
+    selinuxenabled || return 255
+
+    probecheck "selinuxboolean" || return 255
+
+    local ret_val=0;
+    local DF="${srcdir}/test_probes_selinuxsecuritycontext_offline_mode.xml"
+    local RF="test_probes_selinuxsecuritycontext_offline_mode.results.xml"
+
+    [ -f $RF ] && rm -f $RF
+
+    tmpdir=$(mktemp -t -d "test_offline_mode_selinuxsecuritycontext.XXXXXX")
+
+    ln -s /dev $tmpdir
+
+    set_chroot_offline_test_mode "$tmpdir"
+
+    $OSCAP oval eval --results $RF $DF
+
+    unset_chroot_offline_test_mode
+
+    if [ -f $RF ]; then
+        verify_results "def" $DF $RF 1 && verify_results "tst" $DF $RF 1
+        ret_val=$?
+    else
+        ret_val=1
+    fi
+
+    rm -rf ${tmpdir}
+
+    return $ret_val
+}
+
+test_probes_selinuxsecuritycontext_offline_mode
