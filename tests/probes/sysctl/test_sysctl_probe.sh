7ef76a87c3e718ddd1366b0f6efd57107e6204fa
Tom Seewald <tseewald@gmail.com>
2020-08-07 21:15:54 -0500

    Add test_sysctl_probe_freebsd.oval.xml
    FreeBSD's sysctl for displaying the hostname is "kern.hostname", not
    "kernel.hostname" as on Linux. Add this file so that the sysctl probe's
    functionality can be tested properly on FreeBSD.
    
    Also update test_sysctl_probe{_all}.sh scripts so that they choose the
    correct xml file to parse.

--- tests/probes/sysctl/test_sysctl_probe.sh
+++ tests/probes/sysctl/test_sysctl_probe.sh
@@ -5,19 +5,34 @@
 set -e -o pipefail
 
 function perform_test {
-probecheck "sysctl" || return 255
+	probecheck "sysctl" || return 255
 
-result=`mktemp`
-stderr=`mktemp`
-hostname=`hostname`
-$OSCAP oval eval --results $result $srcdir/test_sysctl_probe.oval.xml 2>$stderr
+	result=`mktemp`
+	stderr=`mktemp`
+	hostname=`hostname`
+	case $(uname) in
+		FreeBSD)
+			$OSCAP oval eval --results $result $srcdir/test_sysctl_probe_freebsd.oval.xml 2>$stderr
+			;;
+		*)
+			$OSCAP oval eval --results $result $srcdir/test_sysctl_probe.oval.xml 2>$stderr
+			;;
+	esac
 
-[ ! -s $stderr ]
-assert_exists 1 "/oval_results/results/system/oval_system_characteristics/system_data/unix-sys:sysctl_item/unix-sys:name[text()='kernel.hostname']"
-assert_exists 1 "/oval_results/results/system/oval_system_characteristics/system_data/unix-sys:sysctl_item/unix-sys:value[text()='$hostname']"
+	[ ! -s $stderr ]
+	case $(uname) in
+		FreeBSD)
+			assert_exists 1 "/oval_results/results/system/oval_system_characteristics/system_data/unix-sys:sysctl_item/unix-sys:name[text()='kern.hostname']"
+			assert_exists 1 "/oval_results/results/system/oval_system_characteristics/system_data/unix-sys:sysctl_item/unix-sys:value[text()='$hostname']"
+			;;
+		*)
+			assert_exists 1 "/oval_results/results/system/oval_system_characteristics/system_data/unix-sys:sysctl_item/unix-sys:name[text()='kernel.hostname']"
+			assert_exists 1 "/oval_results/results/system/oval_system_characteristics/system_data/unix-sys:sysctl_item/unix-sys:value[text()='$hostname']"
+			;;
+	esac
 
-rm $result
-rm $stderr
+	rm $result
+	rm $stderr
 }
 
 perform_test
