57c401455916a95e182471725c2a58543753e9f0
Tom Seewald <tseewald@gmail.com>
2020-08-08 21:15:33 -0500

    Check if process58 probe is available before trying to test it.
    This fixes an issue where if a system did not support the process58
    probe, the tests would fail instead of being skipped. An example of
    this is FreeBSD which does not have libcap, and therefore does not
    support the process58 probe.

--- tests/probes/process58/all.sh
+++ tests/probes/process58/all.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 . $builddir/tests/test_common.sh
+probecheck "process58" || exit 255
 
 test_init tests_probes_process88.log
 test_run "Ensure that selinux_domain_label is collected" $srcdir/selinux_domain_label.sh
