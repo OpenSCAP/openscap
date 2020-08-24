OpenSCAP/openscap

commit abf6adb460282cb4d1ca7426aa8f6aee5ec1d15c

Move values of __fsdev_init parameters inside __fsdev_init
Function __fsdev_init is called at only single place in the whole
codebase, therefore the parameters have always the same values.
Instead of passing these values as parameters we can define them
as variables inside __fsdev_init and remove those parameters from
the signature.

src/OVAL/probes/fsdev.c
@@ -185,7 +185,7 @@ is_local_fs(struct mntent *ment)
 
 #endif /* _AIX */
 
-static fsdev_t *__fsdev_init(fsdev_t * lfs, const char **fs, size_t fs_cnt)
+static fsdev_t *__fsdev_init(fsdev_t *lfs)
 {
 	int e;
 	FILE *fp;
@@ -193,6 +193,9 @@ static fsdev_t *__fsdev_init(fsdev_t * lfs, const char **fs, size_t fs_cnt)
 
 	struct mntent *ment;
 	struct stat st;
+
+	const char **fs = NULL;
+	size_t fs_cnt = 0;
 
 	fp = setmntent(_PATH_MOUNTED, "r");
 	if (fp == NULL) {
@@ -239,11 +242,14 @@ static fsdev_t *__fsdev_init(fsdev_t * lfs, const char **fs, size_t fs_cnt)
 	return (lfs);
 }
 #elif defined(__FreeBSD__) || defined(__FreeBSD_kernel__)
-static fsdev_t *__fsdev_init(fsdev_t * lfs, const char **fs, size_t fs_cnt)
+static fsdev_t *__fsdev_init(fsdev_t *lfs)
 {
 	struct statfs *mntbuf = NULL;
 	struct stat st;
 	int i;
+
+	const char **fs = NULL;
+	size_t fs_cnt = 0;
 
 	lfs->cnt = getmntinfo(&mntbuf, (fs == NULL ? MNT_LOCAL : 0) | MNT_NOWAIT);
 	lfs->ids = malloc(sizeof(dev_t) * lfs->cnt);
@@ -276,7 +282,7 @@ static fsdev_t *__fsdev_init(fsdev_t * lfs, const char **fs, size_t fs_cnt)
 #define DEVID_ARRAY_SIZE 16
 #define DEVID_ARRAY_ADD  8
 
-static fsdev_t *__fsdev_init(fsdev_t * lfs, const char **fs, size_t fs_cnt)
+static fsdev_t *__fsdev_init(fsdev_t *lfs)
 {
 	int e;
 	FILE *fp;
@@ -284,6 +290,9 @@ static fsdev_t *__fsdev_init(fsdev_t * lfs, const char **fs, size_t fs_cnt)
 
 	struct mnttab mentbuf;
 	struct stat st;
+
+	const char **fs = NULL;
+	size_t fs_cnt = 0;
 
 	fp = fopen(MNTTAB, "r");
 	if (fp == NULL) {
@@ -345,8 +354,6 @@ static fsdev_t *__fsdev_init(fsdev_t * lfs, const char **fs, size_t fs_cnt)
 
 fsdev_t *fsdev_init()
 {
-	const char **fs = NULL;
-	size_t fs_cnt = 0;
 	fsdev_t *lfs;
 
 	lfs = malloc(sizeof(fsdev_t));
@@ -354,7 +361,7 @@ fsdev_t *fsdev_init()
 	if (lfs == NULL)
 		return (NULL);
 
-	if (__fsdev_init(lfs, fs, fs_cnt) == NULL)
+	if (__fsdev_init(lfs) == NULL)
 		return (NULL);
 
         if (lfs->ids != NULL && lfs->cnt > 1)

commit edf97e03828f4012703eb3aabbbe98592895a49a

fsdev: Remove undeclared variable on FreeBSD and macOS.
The FreeBSD and macOS variant of __fsdev_init had an undeclared
"fs". Fix this by removing the variable.

This issue was introduced with commits abf6adb46 and 82fee23fa.

The first removed "fs" as a function parameter, but defined it within
the body of __fsdev_init(). The second commit removed the declaration
of fs but not its later usage.

src/OVAL/probes/fsdev.c
@@ -211,6 +211,7 @@ static fsdev_t *__fsdev_init(fsdev_t *lfs)
 
 	return (lfs);
 }
+
 #elif defined(OS_FREEBSD) || defined(OS_APPLE)
 static fsdev_t *__fsdev_init(fsdev_t *lfs)
 {
@@ -218,7 +219,7 @@ static fsdev_t *__fsdev_init(fsdev_t *lfs)
 	struct stat st;
 	int i;
 
-	lfs->cnt = getmntinfo(&mntbuf, (fs == NULL ? MNT_LOCAL : 0) | MNT_NOWAIT);
+	lfs->cnt = getmntinfo(&mntbuf, MNT_NOWAIT);
 	lfs->ids = malloc(sizeof(dev_t) * lfs->cnt);
 
 	for (i = 0; i < lfs->cnt; ++i) {