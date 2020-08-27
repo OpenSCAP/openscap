OpenSCAP/openscap

commit 31b5433474ec1f405081f857209d541e377942fb

Add sysctl_probe_main() for FreeBSD
Initial implementation that parses and reports the output of sysctl(8).
Also unconditionally include sysctl_probe.h. This fixes an issue where
non-Linux platforms would encounter a missing prototype error for
sysctl_probe_main().

src/OVAL/probes/unix/sysctl_probe.c
@@ -32,6 +32,19 @@
 
 #include <probe-api.h>
 #include "probe/entcmp.h"
+#include "sysctl_probe.h"
+
+#if defined(OS_FREEBSD)
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <string.h>
+
+#include "oval_fts.h"
+#include "common/debug_priv.h"
+
+#define SYSCTL_CMD "/sbin/sysctl -ae"
+#endif
 
 #if defined(OS_LINUX)
 
@@ -42,7 +55,6 @@
 #include <limits.h>
 #include "oval_fts.h"
 #include "common/debug_priv.h"
-#include "sysctl_probe.h"
 
 #define PROC_SYS_DIR "/proc/sys"
 #define PROC_SYS_MAXDEPTH 7
@@ -266,6 +278,85 @@ int sysctl_probe_main(probe_ctx *ctx, void *probe_arg)
 	SEXP_free(bh_entity);
 	SEXP_free(name_entity);
 
+        return (0);
+}
+
+#elif defined(OS_FREEBSD)
+int sysctl_probe_main(probe_ctx *ctx, void *probe_arg)
+{
+        FILE *fp;
+        char output[LINE_MAX];
+        const char* SEP = "=";
+        char* mib;
+        char* sysval;
+        SEXP_t *se_mib;
+        SEXP_t *name_entity, *probe_in;
+
+        probe_in    = probe_ctx_getobject(ctx);
+        name_entity = probe_obj_getent(probe_in, "name", 1);
+
+        if (name_entity == NULL) {
+                dE("Missing \"name\" entity in the input object");
+                return (PROBE_ENOENT);
+        }
+
+	/* FreeBSD's sysctl(8) uses undocumented, and potentially unstable,
+	 * kernel interfaces to obtain the list of system properties and values.
+	 * Hence we call the executable and parse its output rather than
+	 * implement the functionality ourselves which risks breakage if/when
+	 * the interfaces change.
+	 */
+        fp = popen(SYSCTL_CMD, "r");
+
+        if (!fp) {
+		dE("Failed to open output of %s", SYSCTL_CMD);
+		return (PROBE_EFATAL);
+        }
+
+        while (fgets(output, sizeof(output), fp)) {
+		char *strp;
+		mib = strtok_r(output, SEP, &strp);
+		sysval = strtok_r(NULL, SEP, &strp);
+
+		if (!mib)
+			continue;
+
+		if (!sysval)
+			continue;
+
+		se_mib = SEXP_string_new(mib, strlen(mib));
+
+		if (!se_mib) {
+			dE("Failed to allocate new SEXP_string for se_mib");
+			pclose(fp);
+			return (PROBE_ENOENT);
+		}
+
+		/* Remove newline */
+		sysval[strlen(sysval)-1] = '\0';
+
+		if (probe_entobj_cmp(name_entity, se_mib) == OVAL_RESULT_TRUE) {
+			SEXP_t *item;
+
+			item = probe_item_create(OVAL_UNIX_SYSCTL, NULL,
+						"name", OVAL_DATATYPE_SEXP, se_mib,
+						"value", OVAL_DATATYPE_STRING, sysval,
+						NULL);
+
+			if (!item) {
+				dE("probe_item_create() returned a null item");
+				pclose(fp);
+				SEXP_free(se_mib);
+				return (PROBE_ENOENT);
+			}
+
+			probe_item_collect(ctx, item);
+		}
+
+		SEXP_free(se_mib);
+        }
+
+        pclose(fp);
         return (0);
 }
 #else

commit dc6cf64526577e573c398c88c91d60788d25ae46

Fix whitespace issues in sysctl_probe.c
Fixes: 31b543347 ("Add sysctl_probe_main() for FreeBSD")

src/OVAL/probes/unix/sysctl_probe.c
@@ -284,21 +284,21 @@ int sysctl_probe_main(probe_ctx *ctx, void *probe_arg)
 #elif defined(OS_FREEBSD)
 int sysctl_probe_main(probe_ctx *ctx, void *probe_arg)
 {
-        FILE *fp;
-        char output[LINE_MAX];
-        const char* SEP = "=";
-        char* mib;
-        char* sysval;
-        SEXP_t *se_mib;
-        SEXP_t *name_entity, *probe_in;
+	FILE *fp;
+	char output[LINE_MAX];
+	const char* SEP = "=";
+	char* mib;
+	char* sysval;
+	SEXP_t *se_mib;
+	SEXP_t *name_entity, *probe_in;
 
-        probe_in    = probe_ctx_getobject(ctx);
-        name_entity = probe_obj_getent(probe_in, "name", 1);
+	probe_in    = probe_ctx_getobject(ctx);
+	name_entity = probe_obj_getent(probe_in, "name", 1);
 
-        if (name_entity == NULL) {
-                dE("Missing \"name\" entity in the input object");
-                return (PROBE_ENOENT);
-        }
+	if (name_entity == NULL) {
+		dE("Missing \"name\" entity in the input object");
+		return (PROBE_ENOENT);
+	}
 
 	/* FreeBSD's sysctl(8) uses undocumented, and potentially unstable,
 	 * kernel interfaces to obtain the list of system properties and values.
@@ -306,14 +306,14 @@ int sysctl_probe_main(probe_ctx *ctx, void *probe_arg)
 	 * implement the functionality ourselves which risks breakage if/when
 	 * the interfaces change.
 	 */
-        fp = popen(SYSCTL_CMD, "r");
+	fp = popen(SYSCTL_CMD, "r");
 
-        if (!fp) {
+	if (!fp) {
 		dE("Failed to open output of %s", SYSCTL_CMD);
 		return (PROBE_EFATAL);
-        }
+	}
 
-        while (fgets(output, sizeof(output), fp)) {
+	while (fgets(output, sizeof(output), fp)) {
 		char *strp;
 		mib = strtok_r(output, SEP, &strp);
 		sysval = strtok_r(NULL, SEP, &strp);
@@ -356,8 +356,8 @@ int sysctl_probe_main(probe_ctx *ctx, void *probe_arg)
 		SEXP_free(se_mib);
         }
 
-        pclose(fp);
-        return (0);
+	pclose(fp);
+	return (0);
 }
 #else
 int sysctl_probe_main(probe_ctx *ctx, void *probe_arg)