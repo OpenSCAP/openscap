5ed6ac781d96070e038d7b8c0797d18958a751e4
Jan Černý <jcerny@redhat.com>
2020-09-22 12:23:23 +0200

    Split tests for sysctl probe

--- tests/probes/sysctl/all.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-. $builddir/tests/test_common.sh
-
-test_init test_probes_sysctl.log
-test_run "test sysctl probe" $srcdir/test_sysctl_probe.sh
-test_run "test sysctl probe that collects everything" $srcdir/test_sysctl_probe_all.sh
-test_exit
