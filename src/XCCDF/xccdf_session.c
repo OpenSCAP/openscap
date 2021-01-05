OpenSCAP/openscap

commit 9b40767967e533bdb340ca4c91f2fd1192694820

Fix TestResult/benchmark/@href attribute
Make the href attribute an URI according to SCAP specification Section
4.5. This should fix SCAPVAL error RES-253-2 which occured when
validating ARFs by SCAPVAL. See #1629 for the problem description.

Also, it fixes a problem that xccdf_benchmark_item_clone didn't create
the clusters_dict hash table.

The existing tests are extended to test this property, the
function test_sds_external_xccdf has been splitted into 2 functions.

Fixes: #1629

src/XCCDF/xccdf_session.c
@@ -1374,21 +1374,28 @@ static int _build_xccdf_result_source(struct xccdf_session *session)
 			oscap_seterr(OSCAP_EFAMILY_OSCAP, "No XCCDF results to export.");
 			return 1;
 		}
-		struct xccdf_result* cloned_result = xccdf_result_clone(session->xccdf.result);
-		xccdf_benchmark_add_result(benchmark, cloned_result);
-		session->xccdf.result_source = xccdf_benchmark_export_source(benchmark, session->export.xccdf_file);
 
 		if (session->export.xccdf_file != NULL) {
+			struct xccdf_benchmark *cloned_benchmark = xccdf_benchmark_clone(benchmark);
+			struct xccdf_result *cloned_result = xccdf_result_clone(session->xccdf.result);
+			xccdf_benchmark_add_result(cloned_benchmark, cloned_result);
+			struct oscap_source *xccdf_result_source = xccdf_benchmark_export_source(cloned_benchmark, session->export.xccdf_file);
+			// cloned_result is freed during xccdf_benchmark_free
+			xccdf_benchmark_free(cloned_benchmark);
 			// Export XCCDF result file only when explicitly requested
-			if (oscap_source_save_as(session->xccdf.result_source, NULL) != 0) {
+			if (oscap_source_save_as(xccdf_result_source, NULL) != 0) {
 				oscap_seterr(OSCAP_EFAMILY_OSCAP, "Could not save file: %s",
-						oscap_source_readable_origin(session->xccdf.result_source));
+						oscap_source_readable_origin(xccdf_result_source));
+				oscap_source_free(xccdf_result_source);
 				return -1;
 			}
+			oscap_source_free(xccdf_result_source);
 		}
 
 		if (session->export.xccdf_stig_viewer_file != NULL) {
+			struct xccdf_result *cloned_result = xccdf_result_clone(session->xccdf.result);
 			struct oscap_source * stig_result = xccdf_result_stig_viewer_export_source(cloned_result, session->export.xccdf_stig_viewer_file);
+			xccdf_result_free(cloned_result);
 			if (oscap_source_save_as(stig_result, NULL) != 0) {
 				oscap_seterr(OSCAP_EFAMILY_OSCAP, "Could not save file: %s",
 						oscap_source_readable_origin(stig_result));
@@ -1398,6 +1405,14 @@ static int _build_xccdf_result_source(struct xccdf_session *session)
 			oscap_source_free(stig_result);
 		}
 
+		struct xccdf_result *cloned_result = xccdf_result_clone(session->xccdf.result);
+		if (xccdf_session_is_sds(session)) {
+			struct ds_sds_session *sds_session = xccdf_session_get_ds_sds_session(session);
+			const char *benchmark_uri = ds_sds_session_get_checklist_uri(sds_session);
+			xccdf_result_set_benchmark_uri(cloned_result, benchmark_uri);
+		}
+		xccdf_benchmark_add_result(benchmark, cloned_result);
+		session->xccdf.result_source = xccdf_benchmark_export_source(benchmark, session->export.xccdf_file);
 		/* validate XCCDF Results */
 		if (session->validate && session->full_validation) {
 			if (oscap_source_validate(session->xccdf.result_source, _reporter, NULL)) {

src/DS/ds_sds_session.c
@@ -50,7 +50,9 @@ struct ds_sds_session {
 	const char *target_dir;                 ///< Target directory for current split
 	const char *datastream_id;              ///< ID of selected datastream
 	const char *checklist_id;               ///< ID of selected checklist
+	const char *checklist_uri;              ///< URI of selected checklist
 	struct oscap_htable *component_sources;	///< oscap_source for parsed components
+	struct oscap_htable *component_uris;    ///< maps component refs to component URIs
 	bool fetch_remote_resources;            ///< Allows loading of external components;
 	download_progress_calllback_t progress;	///< Callback to report progress of download.
 };
@@ -72,6 +74,7 @@ struct ds_sds_session *ds_sds_session_new_from_source(struct oscap_source *sourc
 	struct ds_sds_session *sds_session = (struct ds_sds_session *) calloc(1, sizeof(struct ds_sds_session));
 	sds_session->source = source;
 	sds_session->component_sources = oscap_htable_new();
+	sds_session->component_uris = oscap_htable_new();
 	sds_session->progress = download_progress_empty_calllback;
 	return sds_session;
 }
@@ -84,6 +87,7 @@ void ds_sds_session_free(struct ds_sds_session *sds_session)
 			oscap_acquire_cleanup_dir(&(sds_session->temp_dir));
 		}
 		oscap_htable_free(sds_session->component_sources, (oscap_destruct_func) oscap_source_free);
+		oscap_htable_free(sds_session->component_uris, (oscap_destruct_func) free);
 		free(sds_session);
 	}
 }
@@ -91,10 +95,13 @@ void ds_sds_session_free(struct ds_sds_session *sds_session)
 void ds_sds_session_reset(struct ds_sds_session *session)
 {
 	session->checklist_id = NULL;
+	session->checklist_uri = NULL;
 	session->datastream_id = NULL;
 	session->target_dir = NULL;
 	oscap_htable_free(session->component_sources, (oscap_destruct_func) oscap_source_free);
 	session->component_sources = oscap_htable_new();
+	oscap_htable_free(session->component_uris, (oscap_destruct_func) free);
+	session->component_uris = oscap_htable_new();
 }
 
 struct ds_sds_index *ds_sds_session_get_sds_idx(struct ds_sds_session *session)
@@ -163,9 +170,19 @@ const char *ds_sds_session_get_checklist_id(const struct ds_sds_session *session
 	return session->checklist_id;
 }
 
+const char *ds_sds_session_get_checklist_uri(const struct ds_sds_session *session)
+{
+	return session->checklist_uri;
+}
+
 struct oscap_htable *ds_sds_session_get_component_sources(struct ds_sds_session *session)
 {
 	return session->component_sources;
+}
+
+struct oscap_htable *ds_sds_session_get_component_uris(struct ds_sds_session *session)
+{
+	return session->component_uris;
 }
 
 const char *ds_sds_session_get_readable_origin(const struct ds_sds_session *session)
@@ -210,6 +227,7 @@ struct oscap_source *ds_sds_session_select_checklist(struct ds_sds_session *sess
 		oscap_seterr(OSCAP_EFAMILY_OSCAP, "Could not extract %s with all dependencies from datastream.", session->checklist_id);
 		return NULL;
 	}
+	session->checklist_uri = oscap_htable_get(session->component_uris, session->checklist_id);
 	struct oscap_source *xccdf = oscap_htable_get(session->component_sources, session->checklist_id);
 	if (xccdf == NULL) {
 		oscap_seterr(OSCAP_EFAMILY_OSCAP, "Internal error: Could not acquire handle to '%s' source.", session->checklist_id);

src/DS/ds_sds_session_priv.h
@@ -37,6 +37,7 @@ xmlDoc *ds_sds_session_get_xmlDoc(struct ds_sds_session *session);
 int ds_sds_session_register_component_source(struct ds_sds_session *session, const char *relative_filepath, struct oscap_source *component);
 const char *ds_sds_session_get_target_dir(struct ds_sds_session *session);
 struct oscap_htable *ds_sds_session_get_component_sources(struct ds_sds_session *session);
+struct oscap_htable *ds_sds_session_get_component_uris(struct ds_sds_session *session);
 const char *ds_sds_session_get_readable_origin(const struct ds_sds_session *session);
 bool ds_sds_session_fetch_remote_resources(struct ds_sds_session *session);
 download_progress_calllback_t ds_sds_session_remote_resources_progress(struct ds_sds_session *session);

src/DS/public/ds_sds_session.h
@@ -120,6 +120,14 @@ OSCAP_API const char *ds_sds_session_get_datastream_id(const struct ds_sds_sessi
 OSCAP_API const char *ds_sds_session_get_checklist_id(const struct ds_sds_session *session);
 
 /**
+ * Return URI of currently selected component representing XCCDF within the DataStream
+ * @memberof ds_sds_session
+ * @param session The Source DataStream session
+ * @returns URI of selected component or NULL
+ */
+OSCAP_API const char *ds_sds_session_get_checklist_uri(const struct ds_sds_session *session);
+
+/**
  * Get component from Source DataStream by its href. This assumes that the component
  * has been already cached by the session. You can cache component or its dependencies
  * by calling ds_sds_session_select_checklist or ds_sds_session_register_component_with_dependencies.

src/DS/sds.c
@@ -450,7 +450,10 @@ int ds_sds_dump_component_ref_as(const xmlNodePtr component_ref, struct ds_sds_s
 
 	char* component_id = NULL;
 
+	// make a copy of xlink_href because ds_sds_dump_component_by_href modifies its second argument
+	char *xlink_href_copy = oscap_strdup(xlink_href);
 	int ret = ds_sds_dump_component_by_href(session, xlink_href, target_filename_dirname, relative_filepath, cref_id, &component_id);
+	oscap_htable_add(ds_sds_session_get_component_uris(session), cref_id, xlink_href_copy);
 
 	xmlFree(xlink_href);
 	xmlFree(cref_id);

src/XCCDF/item.c
@@ -1155,6 +1155,7 @@ struct xccdf_benchmark_item * xccdf_benchmark_item_clone(struct xccdf_item *pare
 	clone->items_dict = oscap_htable_new();
 	clone->profiles_dict = oscap_htable_new();
 	clone->results_dict = oscap_htable_new();
+	clone->clusters_dict = oscap_htable_new();
 	clone->notices = oscap_list_clone(item->notices, (oscap_clone_func) xccdf_notice_clone);
 	clone->plain_texts = oscap_list_clone(item->plain_texts, (oscap_clone_func) xccdf_plain_text_clone);
 	

tests/DS/test_ds_misc.sh
@@ -162,6 +162,7 @@ function test_eval_complex()
 	assert_exists 1 '//rule-result[@idref="xccdf_moc.elpmaxe.www_rule_second"]'
 	assert_exists 1 '//rule-result/result'
 	assert_exists 1 '//rule-result/result[text()="pass"]'
+	assert_exists 1 '//TestResult/benchmark[@href="#scap_org.open-scap_comp_second-xccdf.xml2"]'
 	rm $arf
 }
 
@@ -183,15 +184,30 @@ function test_oval_eval_id {
     echo "$OUT" | grep $4 > /dev/null
 }
 
-function test_sds_external_xccdf {
-    local SDS_FILE="${srcdir}/$2"
-    local XCCDF="$3"
-    local PROFILE="$4"
-    local result="${1}-${PROFILE}.xml"
+function test_sds_external_xccdf_in_ds {
+    local SDS_FILE="${srcdir}/sds_external_xccdf/sds.ds.xml"
+    local XCCDF="scap_org.open-scap_cref_xccdf.xml"
+    local PROFILE="xccdf_external_profile_datastream_1"
+    local result="$(mktemp)"
 
-    $OSCAP xccdf eval --xccdf-id "$XCCDF" --profile "$PROFILE" --results "$result" "$SDS_FILE"
+    $OSCAP xccdf eval --xccdf-id "$XCCDF" --profile "$PROFILE" --results-arf "$result" "$SDS_FILE"
 
     assert_exists 1 '//rule-result/result[text()="pass"]'
+    assert_exists 1 '//TestResult/benchmark[@href="file:xccdf.sds.xml#scap_1_comp_xccdf.xml"]'
+
+    rm -f "$result"
+}
+
+function test_sds_external_xccdf {
+    local SDS_FILE="${srcdir}/sds_external_xccdf/sds.ds.xml"
+    local XCCDF="scap_org.open-scap_cref_xccdf-file.xml"
+    local PROFILE="xccdf_external_profile_file_1"
+    local result="$(mktemp)"
+
+    $OSCAP xccdf eval --xccdf-id "$XCCDF" --profile "$PROFILE" --results-arf "$result" "$SDS_FILE"
+
+    assert_exists 1 '//rule-result/result[text()="pass"]'
+    assert_exists 1 '//TestResult/benchmark[@href="file:xccdf.xml"]'
 
     rm -f "$result"
 }
@@ -238,8 +254,8 @@ function test_ds_continue_without_remote_resources() {
 # Testing.
 test_init
 
-test_run "sds_external_xccdf" test_sds_external_xccdf sds_external_xccdf sds_external_xccdf/sds.ds.xml scap_org.open-scap_cref_xccdf.xml xccdf_external_profile_datastream_1
-test_run "sds_external_xccdf" test_sds_external_xccdf sds_external_xccdf sds_external_xccdf/sds.ds.xml scap_org.open-scap_cref_xccdf-file.xml xccdf_external_profile_file_1
+test_run "sds_external_xccdf_in_ds" test_sds_external_xccdf_in_ds
+test_run "sds_external_xccdf" test_sds_external_xccdf
 test_run "sds_tailoring" test_sds_tailoring sds_tailoring sds_tailoring/sds.ds.xml scap_com.example_datastream_with_tailoring xccdf_com.example_cref_tailoring_01 xccdf_com.example_profile_tailoring
 
 test_run "eval_simple" test_eval eval_simple/sds.xml

commit b8b90b4c04a130d9174148486b40bfc8454a290b

Fix a segmentation fault
When generating results with --stig-viewer a segmentation fault
occured. This segfault has been caused by
9b40767967e533bdb340ca4c91f2fd1192694820.

The patch makes the usage of benchmark and cloned benchmark in this
`if` block consistent with the previous `if` block above.

src/XCCDF/xccdf_session.c
@@ -1393,9 +1393,12 @@ static int _build_xccdf_result_source(struct xccdf_session *session)
 		}
 
 		if (session->export.xccdf_stig_viewer_file != NULL) {
+			struct xccdf_benchmark *cloned_benchmark = xccdf_benchmark_clone(benchmark);
 			struct xccdf_result *cloned_result = xccdf_result_clone(session->xccdf.result);
+			xccdf_benchmark_add_result(cloned_benchmark, cloned_result);
 			struct oscap_source * stig_result = xccdf_result_stig_viewer_export_source(cloned_result, session->export.xccdf_stig_viewer_file);
-			xccdf_result_free(cloned_result);
+			// cloned_result is freed during xccdf_benchmark_free
+			xccdf_benchmark_free(cloned_benchmark);
 			if (oscap_source_save_as(stig_result, NULL) != 0) {
 				oscap_seterr(OSCAP_EFAMILY_OSCAP, "Could not save file: %s",
 						oscap_source_readable_origin(stig_result));