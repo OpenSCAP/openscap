5decb41b65abe3d42edadd0e0f30b5a6d6f56901
Jan Černý <jcerny@redhat.com>
2021-03-17 15:07:52 +0100

    Fix buffer over run
    Addressing:
    Error: COMPILER_WARNING (CWE-758): [#def3] [important]
    openscap-1.3.4/compat/dev_to_tty.c: scope_hint: In function 'dev_to_tty.constprop.isra'
    openscap-1.3.4/compat/dev_to_tty.c:306:15: warning[-Wstringop-overflow=]: writing 1 byte into a region of size 0
      306 |     tmp[chop] = '\0';
          |               ^
    openscap-1.3.4/compat/dev_to_tty.c:286:15: note: at offset 128 into destination object 'buf' of size 128
      286 |   static char buf[TTY_NAME_SIZE];
          |               ^
      304|     /* gotta check before we chop or we may chop someone else's memory */
      305|     if(chop + (unsigned long)(tmp-buf) <= sizeof buf)
      306|->     tmp[chop] = '\0';
      307|     /* replace non-ASCII characters with '?' and return the number of chars */

--- compat/dev_to_tty.c
+++ compat/dev_to_tty.c
@@ -302,8 +302,7 @@ abbrev:
   if((flags&ABBREV_TTY) && !strncmp(tmp,"tty",  3) && tmp[3]) tmp += 3;
   if((flags&ABBREV_PTS) && !strncmp(tmp,"pts/", 4) && tmp[4]) tmp += 4;
   /* gotta check before we chop or we may chop someone else's memory */
-  if(chop + (unsigned long)(tmp-buf) <= sizeof buf)
-    // coverity[-Wstringop-overflow= : FALSE]
+  if(chop + (unsigned long)(tmp-buf) < sizeof buf)
     tmp[chop] = '\0';
   /* replace non-ASCII characters with '?' and return the number of chars */
   for(;;){
