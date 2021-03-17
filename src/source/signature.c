OpenSCAP/openscap

commit fbb762b358861e52e850404cefa1de03c492a8f9

Initial implementation of signature validation
Implement signature validation for SCAP 1.3 source data stream

src/source/signature.c
@@ -1,0 +1,290 @@
+/*
+ * Copyright 2021 Red Hat Inc., Durham, North Carolina.
+ * All Rights Reserved.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ *
+ * Author:
+ *     Jan Černý <jcerny@redhat.com>
+ */
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+
+#include <libxml/tree.h>
+#include <libxml/xmlmemory.h>
+#include <libxml/parser.h>
+
+#include <libxslt/xslt.h>
+#include <libxslt/security.h>
+
+#include <xmlsec/xmlsec.h>
+#include <xmlsec/xmltree.h>
+#include <xmlsec/xmldsig.h>
+#include <xmlsec/crypto.h>
+
+#include "common/_error.h"
+#include "common/debug_priv.h"
+#include "oscap_source.h"
+#include "oscap_source_priv.h"
+#include "signature_priv.h"
+
+struct oscap_signature_ctx {
+	const char *pubkey_pem; // path to the public key file in PEM format
+	const char *pubkey_cert_pem; // path to the public key certificate file in PEM format
+};
+
+struct oscap_signature_ctx *oscap_signature_ctx_new()
+{
+	struct oscap_signature_ctx *ctx = malloc(sizeof(struct oscap_signature_ctx));
+	ctx->pubkey_pem = NULL;
+	ctx->pubkey_cert_pem = NULL;
+	return ctx;
+}
+
+void oscap_signature_ctx_set_pubkey_pem(struct oscap_signature_ctx *ctx, const char *path)
+{
+	ctx->pubkey_pem = path;
+}
+
+void oscap_signature_ctx_set_pubkey_cert_pem(struct oscap_signature_ctx *ctx, const char *path)
+{
+	ctx->pubkey_cert_pem = path;
+}
+
+void oscap_signature_ctx_free(struct oscap_signature_ctx *ctx)
+{
+	free(ctx);
+}
+
+static int _mark_sds_id_elements(xmlDocPtr doc)
+{
+	/* Adds id attibutes of ds:component and ds:data-stream to the list
+	 * of globally known id attributes.
+	 */
+	xmlNodePtr root = xmlDocGetRootElement(doc);
+	xmlNodePtr node = root->children;
+	for (; node != NULL; node = node->next) {
+		if (node->type != XML_ELEMENT_NODE)
+			continue;
+		const char *name = (const char *)node->name;
+		if (strcmp(name, "component") && strcmp(name, "data-stream"))
+			continue;
+		xmlAttrPtr id_attr = xmlHasProp(node, BAD_CAST "id");
+		if (id_attr == NULL || id_attr->children == NULL) {
+			oscap_seterr(OSCAP_EFAMILY_XML, "Element \"%s\" doesn't have an id attribute\n", name);
+			return -1;
+		}
+		xmlChar *id_attr_value = xmlNodeListGetString(node->doc, id_attr->children, 1);
+		if (id_attr_value == NULL) {
+			oscap_seterr(OSCAP_EFAMILY_XML, "Attribute \"id\" in element \"%s\" has no value\n", name);
+			return -1;
+		}
+		/* check that we don't have same ID already */
+		xmlAttrPtr tmp_attr = xmlGetID(node->doc, id_attr_value);
+		if (tmp_attr == NULL) {
+			xmlAddID(NULL, node->doc, id_attr_value, id_attr);
+		} else if (tmp_attr != id_attr) {
+			oscap_seterr(OSCAP_EFAMILY_XML, "Duplicate ID attribute \"%s\"\n", id_attr_value);
+			xmlFree(id_attr_value);
+			return -1;
+		}
+		xmlFree(id_attr_value);
+	}
+	return 0;
+}
+
+static int _oscap_signature_validate_doc(xmlDocPtr doc, oscap_document_type_t scap_type, struct oscap_signature_ctx *ctx)
+{
+	int res = -1;
+	xmlNodePtr node = NULL;
+	xmlSecDSigCtxPtr dsigCtx = NULL;
+	xmlSecKeysMngrPtr mngr = NULL;
+	xsltSecurityPrefsPtr xsltSecPrefs = NULL;
+
+	dI("Validating XML signature.");
+
+	/* Init libxslt */
+	/* disable everything */
+	xsltSecPrefs = xsltNewSecurityPrefs();
+	xsltSetSecurityPrefs(xsltSecPrefs, XSLT_SECPREF_READ_FILE, xsltSecurityForbid);
+	xsltSetSecurityPrefs(xsltSecPrefs, XSLT_SECPREF_WRITE_FILE, xsltSecurityForbid);
+	xsltSetSecurityPrefs(xsltSecPrefs, XSLT_SECPREF_CREATE_DIRECTORY, xsltSecurityForbid);
+	xsltSetSecurityPrefs(xsltSecPrefs, XSLT_SECPREF_READ_NETWORK, xsltSecurityForbid);
+	xsltSetSecurityPrefs(xsltSecPrefs, XSLT_SECPREF_WRITE_NETWORK, xsltSecurityForbid);
+	xsltSetDefaultSecurityPrefs(xsltSecPrefs);
+
+	/* Init xmlsec library */
+	if (xmlSecInit() < 0) {
+		oscap_seterr(OSCAP_EFAMILY_XML, "Xmlsec initialization failed.");
+		return(-1);
+	}
+
+	/* Check loaded library version */
+	if (xmlSecCheckVersion() != 1) {
+		oscap_seterr(OSCAP_EFAMILY_XML, "Loaded xmlsec library version is not compatible.");
+		return(-1);
+	}
+
+	/* Init crypto library */
+	if (xmlSecCryptoAppInit(NULL) < 0) {
+		oscap_seterr(OSCAP_EFAMILY_XML, "Crypto initialization failed.");
+		return(-1);
+	}
+
+	/* Init xmlsec-crypto library */
+	if (xmlSecCryptoInit() < 0) {
+		oscap_seterr(OSCAP_EFAMILY_XML, "Xmlsec-crypto initialization failed.");
+		return(-1);
+	}
+
+	/* find Signature node */
+	xmlNodePtr root = xmlDocGetRootElement(doc);
+	node = xmlSecFindNode(root, xmlSecNodeSignature, xmlSecDSigNs);
+	if (node == NULL) {
+		/* Non-fatal error, datastreams without signatures are accepted */
+		dI("Signature node not found");
+		res = 0;
+		goto cleanup;
+	}
+
+	/* Adds id attibutes of ds:component and ds:data-stream as general id
+	 * attributes
+	 */
+	if (scap_type == OSCAP_DOCUMENT_SDS && _mark_sds_id_elements(doc) < 0) {
+		goto cleanup;
+	}
+
+	/* create and initialize keys manager */
+	mngr = xmlSecKeysMngrCreate();
+	if (mngr == NULL) {
+		oscap_seterr(OSCAP_EFAMILY_XML, "failed to create keys manager");
+		goto cleanup;
+	}
+	if (xmlSecCryptoAppDefaultKeysMngrInit(mngr) < 0) {
+		oscap_seterr(OSCAP_EFAMILY_XML, "failed to initialize keys manager");
+		goto cleanup;
+	}
+
+	/* create signature context */
+	dsigCtx = xmlSecDSigCtxCreate(mngr);
+	if (dsigCtx == NULL) {
+		oscap_seterr(OSCAP_EFAMILY_XML, "failed to create signature context");
+		goto cleanup;
+	}
+
+	/* Verify signature */
+	if (xmlSecDSigCtxVerify(dsigCtx, node) < 0) {
+		oscap_seterr(OSCAP_EFAMILY_XML, "Signature verification failed");
+		goto cleanup;
+	}
+
+	if (dsigCtx->status == xmlSecDSigStatusSucceeded) {
+		dI("Signature is OK");
+		res = 0;
+	} else {
+		dI("Signature is invalid.");
+		res = 1;
+	}
+
+	/* compare good/bad references */
+	int good = 0;
+	xmlSecDSigReferenceCtxPtr dsigRefCtx;
+	int size = xmlSecPtrListGetSize(&(dsigCtx->signedInfoReferences));
+	for (int i = good = 0; i < size; ++i) {
+		dsigRefCtx = (xmlSecDSigReferenceCtxPtr)xmlSecPtrListGetItem(&(dsigCtx->signedInfoReferences), i);
+		if(dsigRefCtx == NULL) {
+			oscap_seterr(OSCAP_EFAMILY_XML, "Reference ctx is null");
+			goto cleanup;
+		}
+		if (dsigRefCtx->status == xmlSecDSigStatusSucceeded)
+			++good;
+	}
+	dI("SignedInfo references (ok/all): %d/%d", good, size);
+	if (good != size) {
+		res = 1;
+	}
+
+	/* compare good/bad manifests */
+	size = xmlSecPtrListGetSize(&(dsigCtx->manifestReferences));
+	for (int i = good = 0; i < size; ++i) {
+		dsigRefCtx = (xmlSecDSigReferenceCtxPtr)xmlSecPtrListGetItem(&(dsigCtx->manifestReferences), i);
+		if (dsigRefCtx == NULL) {
+			oscap_seterr(OSCAP_EFAMILY_XML, "Reference ctx is null");
+			goto cleanup;
+		}
+		if (dsigRefCtx->status == xmlSecDSigStatusSucceeded)
+			++good;
+	}
+	dI("Manifests references (ok/all): %d/%d", good, size);
+	if (good != size) {
+		res = 1;
+	}
+
+cleanup:
+	/* cleanup */
+	if (dsigCtx != NULL)
+		xmlSecDSigCtxDestroy(dsigCtx);
+
+	/* destroy keys manager */
+	if (mngr != NULL)
+		xmlSecKeysMngrDestroy(mngr);
+
+	/* Shutdown xmlsec-crypto library */
+	xmlSecCryptoShutdown();
+
+	/* Shutdown crypto library */
+	xmlSecCryptoAppShutdown();
+
+	/* Shutdown xmlsec library */
+	xmlSecShutdown();
+
+	/* Shutdown libxslt/libxml */
+	xsltFreeSecurityPrefs(xsltSecPrefs);
+
+	/* Unset the default XSLT security settings to prevent
+	 * blockage of other oscap XSLT features (generate guide etc.)
+	 */
+	xsltSetDefaultSecurityPrefs(NULL);
+
+	xsltCleanupGlobals();
+
+	return res;
+}
+
+int oscap_signature_validate(struct oscap_source *source, struct oscap_signature_ctx *ctx)
+{
+	int ret = 0;
+	oscap_document_type_t scap_type = oscap_source_get_scap_type(source);
+	const char *origin = oscap_source_readable_origin(source);
+
+	if (scap_type == OSCAP_DOCUMENT_UNKNOWN) {
+		oscap_seterr(OSCAP_EFAMILY_OSCAP, "Unrecognized document type for: %s", origin);
+		ret = -1;
+	} else if (scap_type == OSCAP_DOCUMENT_SDS) {
+		xmlDocPtr doc = oscap_source_get_xmlDoc(source);
+		ret = _oscap_signature_validate_doc(doc, scap_type, ctx);
+	} else {
+		const char *type_name = oscap_document_type_to_string(scap_type);
+		oscap_seterr(OSCAP_EFAMILY_OSCAP, "Unsupported document type %s for XML signature validation: %s", type_name, origin);
+		ret = -1;
+	}
+	return ret;
+}

src/XCCDF/public/xccdf_session.h
@@ -117,6 +117,14 @@ OSCAP_API void xccdf_session_set_rule(struct xccdf_session *session, const char 
 OSCAP_API void xccdf_session_set_validation(struct xccdf_session *session, bool validate, bool full_validation);
 
 /**
+ * Set XML signature validation
+ * @memberof xccdf_session
+ * @param session XCCDF Session
+ * @param validate False value indicates to skip any XML signature validation.
+ */
+OSCAP_API void xccdf_session_set_signature_validation(struct xccdf_session *session, bool validate);
+
+/**
  * Set whether the thin results override is enabled.
  * If true the OVAL results put in ARF or separate files will have thin results.
  * Thin results do not contain details about the evaluated criteria, only

src/XCCDF/xccdf_session.c
@@ -50,6 +50,7 @@
 #include "DS/sds_priv.h"
 #include "OVAL/results/oval_results_impl.h"
 #include "source/xslt_priv.h"
+#include "source/signature_priv.h"
 #include "XCCDF/xccdf_impl.h"
 #include "XCCDF_POLICY/public/xccdf_policy.h"
 #include "XCCDF_POLICY/xccdf_policy_priv.h"
@@ -115,6 +116,8 @@ struct xccdf_session {
 	} tailoring;
 	bool validate;					///< False value indicates to skip any XSD validation.
 	bool full_validation;				///< True value indicates that every possible step will be validated by XSD.
+	bool validate_signature;		///< False value indicates to skip XML signature validation.
+	struct oscap_signature_ctx *signature_ctx; ///< Paths to public keys, certificates, signature related info
 
 	struct oscap_list *check_engine_plugins; ///< Extra non-OVAL check engines that may or may not have been loaded
 	xccdf_session_loading_flags_t loading_flags; ///< Load referenced files while loading XCCDF
@@ -151,6 +154,8 @@ struct xccdf_session *xccdf_session_new_from_source(struct oscap_source *source)
 		return NULL;
 	}
 	session->validate = true;
+	session->validate_signature = true;
+	session->signature_ctx = oscap_signature_ctx_new();
 	session->xccdf.base_score = 0;
 	session->oval.progress = download_progress_empty_calllback;
 	session->check_engine_plugins = oscap_list_new();
@@ -345,6 +350,7 @@ void xccdf_session_free(struct xccdf_session *session)
 	free(session->tailoring.user_component_id);
 	oscap_htable_free(session->oval.results_mapping, (oscap_destruct_func) free);
 	oscap_htable_free(session->oval.arf_report_mapping, (oscap_destruct_func) free);
+	oscap_signature_ctx_free(session->signature_ctx);
 	free(session);
 }
 
@@ -367,6 +373,11 @@ void xccdf_session_set_validation(struct xccdf_session *session, bool validate, 
 {
 	session->validate = validate;
 	session->full_validation = full_validation;
+}
+
+void xccdf_session_set_signature_validation(struct xccdf_session *session, bool validate)
+{
+	session->validate_signature = validate;
 }
 
 void xccdf_session_set_thin_results(struct xccdf_session *session, bool thin_results)
@@ -792,6 +803,15 @@ int xccdf_session_load_xccdf(struct xccdf_session *session)
 		if (session->validate) {
 			if (oscap_source_validate(session->source, _reporter, NULL)) {
 				oscap_seterr(OSCAP_EFAMILY_OSCAP, "Invalid %s (%s) content in %s",
+						oscap_document_type_to_string(oscap_source_get_scap_type(session->source)),
+						oscap_source_get_schema_version(session->source),
+						oscap_source_readable_origin(session->source));
+				return 1;
+			}
+		}
+		if (session->validate_signature) {
+			if (oscap_signature_validate(session->source, session->signature_ctx)) {
+				oscap_seterr(OSCAP_EFAMILY_OSCAP, "Invalid signature in %s (%s) content in %s",
 						oscap_document_type_to_string(oscap_source_get_scap_type(session->source)),
 						oscap_source_get_schema_version(session->source),
 						oscap_source_readable_origin(session->source));

src/source/signature_priv.h
@@ -1,0 +1,38 @@
+/*
+ * Copyright 2021 Red Hat Inc., Durham, North Carolina.
+ * All Rights Reserved.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ *
+ * Author:
+ *     Jan Černý <jcerny@redhat.com>
+ */
+
+#ifndef OSCAP_SIGNATURE_PRIV_H
+#define OSCAP_SIGNATURE_PRIV_H
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+struct oscap_signature_ctx;
+
+struct oscap_signature_ctx *oscap_signature_ctx_new(void);
+void oscap_signature_ctx_free(struct oscap_signature_ctx *ctx);
+void oscap_signature_ctx_set_pubkey_pem(struct oscap_signature_ctx *ctx, const char *path);
+void oscap_signature_ctx_set_pubkey_cert_pem(struct oscap_signature_ctx *ctx, const char *path);
+int oscap_signature_validate(struct oscap_source *source, struct oscap_signature_ctx *ctx);
+
+#endif /* OSCAP_SIGNATURE_PRIV_H */

utils/oscap-tool.c
@@ -58,6 +58,7 @@ static void oscap_action_init(struct oscap_action *action)
     assert(action != NULL);
     memset(action, 0, sizeof(*action));
     action->validate = 1;
+    action->validate_signature = 1;
 }
 
 static void oscap_action_release(struct oscap_action *action)

utils/oscap-tool.h
@@ -159,6 +159,7 @@ struct oscap_action {
 	int doctype;
 	int force;
 	int validate;
+	int validate_signature;
 	int schematron;
 	int remote_resources;
 	int progress;

utils/oscap-xccdf.c
@@ -167,6 +167,8 @@ static struct oscap_module XCCDF_EVAL = {
 		"   --report <file>               - Write HTML report into file.\n"
 		"   --skip-valid                  - Skip validation.\n"
 		"   --skip-validation\n"
+		"   --skip-signature-validation   - Skip data stream signature validation.\n"
+		"                                   (only applicable for source datastreams)\n"
 		"   --fetch-remote-resources      - Download remote content referenced by XCCDF.\n"
 		"   --progress                    - Switch to sparse output suitable for progress reporting.\n"
 		"                                   Format is \"$rule_id:$result\\n\".\n"
@@ -256,6 +258,8 @@ static struct oscap_module XCCDF_GEN_GUIDE = {
 		"   --tailoring-file <file>       - Use given XCCDF Tailoring file.\n"
 		"                                   (only applicable for source datastreams)\n"
 		"   --tailoring-id <component-id> - Use given DS component as XCCDF Tailoring file.\n"
+		"                                   (only applicable for source datastreams)\n"
+		"   --skip-signature-validation   - Skip data stream signature validation.\n"
 		"                                   (only applicable for source datastreams)\n",
     .opt_parser = getopt_xccdf,
     .user = NULL,
@@ -280,6 +284,8 @@ static struct oscap_module XCCDF_GEN_FIX = {
 		"   --tailoring-file <file>       - Use given XCCDF Tailoring file.\n"
 		"                                   (only applicable for source datastreams)\n"
 		"   --tailoring-id <component-id> - Use given DS component as XCCDF Tailoring file.\n"
+		"                                   (only applicable for source datastreams)\n"
+		"   --skip-signature-validation   - Skip data stream signature validation.\n"
 		"                                   (only applicable for source datastreams)\n",
     .opt_parser = getopt_xccdf,
     .user = "legacy-fix.xsl",
@@ -546,6 +552,7 @@ int app_evaluate_xccdf(const struct oscap_action *action)
 	if (session == NULL)
 		goto cleanup;
 	xccdf_session_set_validation(session, action->validate, getenv("OSCAP_FULL_VALIDATION") != NULL);
+	xccdf_session_set_signature_validation(session, action->validate_signature);
 	if (action->thin_results) {
 		xccdf_session_set_thin_results(session, true);
 		xccdf_session_set_without_sys_chars_export(session, true);
@@ -920,6 +927,7 @@ int app_generate_fix(const struct oscap_action *action)
 		goto cleanup;
 
 	xccdf_session_set_validation(session, action->validate, getenv("OSCAP_FULL_VALIDATION") != NULL);
+	xccdf_session_set_signature_validation(session, action->validate_signature);
 	xccdf_session_set_user_cpe(session, action->cpe);
 	xccdf_session_set_remote_resources(session, action->remote_resources, download_reporting_callback);
 	xccdf_session_set_custom_oval_files(session, action->f_ovals);
@@ -994,6 +1002,7 @@ int app_generate_guide(const struct oscap_action *action)
 	}
 
 	xccdf_session_set_validation(session, action->validate, getenv("OSCAP_FULL_VALIDATION") != NULL);
+	xccdf_session_set_signature_validation(session, action->validate_signature);
 	xccdf_session_set_remote_resources(session, action->remote_resources, download_reporting_callback);
 	xccdf_session_set_user_tailoring_file(session, action->tailoring_file);
 	xccdf_session_set_user_tailoring_cid(session, action->tailoring_id);
@@ -1145,6 +1154,7 @@ bool getopt_xccdf(int argc, char **argv, struct oscap_action *action)
 		{"check-engine-results", no_argument, &action->check_engine_results, 1},
 		{"skip-valid",		no_argument, &action->validate, 0},
 		{"skip-validation",		no_argument, &action->validate, 0},
+		{"skip-signature-validation", no_argument, &action->validate_signature, 0},
 		{"fetch-remote-resources", no_argument, &action->remote_resources, 1},
 		{"progress", no_argument, &action->progress, 1},
 		{"remediate", no_argument, &action->remediate, 1},

commit 0845172ae30c40ddf26a2ff31a308a12357c81d9

Fix broken HTML report generation
Broken by fbb762b358861e52e850404cefa1de03c492a8f9.
xsltCleanupGlobals() is already executed in oscap_cleanup()
in src/common/oscapxml.c, line 122

Addressing:
[jcerny@localhost openscap{maint-1.3}]$ c_oscap xccdf eval  --report report.html /usr/share/xml/scap/ssg/content/ssg-fedora-ds.xml
xmlXPathCompOpEval: function node-set not found
XPath error : Unregistered function
runtime error: file /home/jcerny/work/git/openscap/xsl/xccdf-report.xsl line 83 element variable
Failed to evaluate the expression of variable 'last_test_time'.
OpenSCAP Error: Could not apply XSLT /home/jcerny/work/git/openscap/xsl/xccdf-report.xsl to XML file: NONEXISTENT [/home/jcerny/work/git/openscap/src/source/xslt.c:177]

src/source/signature.c
@@ -264,8 +264,6 @@ cleanup:
 	 */
 	xsltSetDefaultSecurityPrefs(NULL);
 
-	xsltCleanupGlobals();
-
 	return res;
 }
 