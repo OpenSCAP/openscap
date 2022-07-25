caadd89e61f5d70e251180055686a3b52c763c66
Jan Černý <jcerny@redhat.com>
2022-07-25 09:00:45 +0200

    Improve unit test for read_common_sizet
    Check for multiple different situations.

--- tests/API/probes/test_memusage.c
+++ tests/API/probes/test_memusage.c
@@ -12,16 +12,34 @@
 static int test_basic()
 {
 	size_t size;
-	char *strval = strdup("17 MB");
-	read_common_sizet(&size, strval);
+	char *strval = strdup("17 kB\n");
+	int ret = read_common_sizet(&size, strval);
 	free(strval);
-	return (size == 17);
+	return (size == 17 && ret == 0);
+}
+
+static int test_no_unit()
+{
+	size_t size;
+	char *strval = strdup("42");
+	int ret = read_common_sizet(&size, strval);
+	free(strval);
+	return (ret == -1);
+}
+
+static int test_invalid_number()
+{
+	size_t size;
+	char *strval = strdup("www kB\n");
+	int ret = read_common_sizet(&size, strval);
+	free(strval);
+	return (size == 0 && ret == 0);
 }
 
 static int test_errno()
 {
 	size_t size;
-	char *strval = strdup("17 MB");
+	char *strval = strdup("17 kB\n");
 
 	/* Test that setting errno outside of the read_common_sizet function
 	 * doesn't influence the function and doesn't make the function fail.
@@ -37,6 +55,14 @@ int main(int argc, char *argv[])
 {
 	if (!test_basic()) {
 		fprintf(stderr, "test_basic has failed\n");
+		return 1;
+	}
+	if (!test_no_unit()) {
+		fprintf(stderr, "test_no_unit has failed\n");
+		return 1;
+	}
+	if (!test_invalid_number()) {
+		fprintf(stderr, "test_invalid_number has failed\n");
 		return 1;
 	}
 	if (!test_errno()) {
