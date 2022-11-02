9c2052febe494ca5fe8e3fef7996fd2c2c736785
Jan Černý <jcerny@redhat.com>
2022-11-02 09:04:25 +0100

    Don't emit items if XPath doesn't match
    This commit fixes the behavior of the xmlfilecontent probe in situation
    when the XPath query in xmlfilecontent_object doesn't match any node in
    the given XML file and the query returns an empty node set. Currently,
    in this situation, we emit an item in which we add an empty value_of
    element. However, this value_of element has its datatype attribute set
    to an empty string, which is invalid according to the OVAL schema. When
    we try to make the OVAL results valid, we face the problem that it isn't
    clear what should be the value of the datatype attribute for empty
    elements. But as we can realize the XPath doesn't match anything means
    that the requested object doesn't exist on the system, so a better
    behavior would be to not produce a xmlfilecontent54_item.  That is
    consistent with eg. situation when a regular expression matched nothing
    in textfilecontent54_object.  This commit therefore stops the item
    generation in this situation.
    
    This commit also extends the existing test to cover the situation
    of XPath queries for nonexistent element and nonexistent attribute.
    
    Fixes: #1890, rhbz#2138884, rhbz#2139060

--- tests/probes/xmlfilecontent/test_xmlfilecontent_probe.sh
+++ tests/probes/xmlfilecontent/test_xmlfilecontent_probe.sh
@@ -6,9 +6,15 @@ set -e -o pipefail
 cp $srcdir/example.xml /tmp/
 result=$(mktemp)
 $OSCAP oval eval --results $result $srcdir/test_xmlfilecontent_probe.xml
+# Even if OSCAP_FULL_VALIDATION is set, an invalid OVAL result doesn't cause
+# the "oscap oval eval" to return a non-zero value, so let's run validation
+# as a separate command
+$OSCAP oval validate "$result"
 assert_exists 1 '/oval_results/results/system/definitions/definition[@definition_id="oval:x:def:1" and @result="true"]'
 assert_exists 1 '/oval_results/results/system/definitions/definition[@definition_id="oval:x:def:2" and @result="true"]'
 assert_exists 1 '/oval_results/results/system/definitions/definition[@definition_id="oval:x:def:3" and @result="true"]'
 assert_exists 1 '/oval_results/results/system/definitions/definition[@definition_id="oval:x:def:4" and @result="true"]'
 assert_exists 1 '/oval_results/results/system/definitions/definition[@definition_id="oval:x:def:5" and @result="true"]'
+assert_exists 1 '/oval_results/results/system/definitions/definition[@definition_id="oval:x:def:6" and @result="true"]'
+assert_exists 1 '/oval_results/results/system/definitions/definition[@definition_id="oval:x:def:7" and @result="true"]'
 rm -f $result
