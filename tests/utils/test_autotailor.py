fbc49d388bfea63508452d482a224f977ac2ce65
Jan Černý <jcerny@redhat.com>
2023-10-02 15:40:23 +0200

    Add unit tests for _full_id_* methods

--- tests/utils/test_autotailor.py
+++ tests/utils/test_autotailor.py
@@ -23,3 +23,22 @@ def test_is_valid_xccdf_id():
         "xccdf_com_example_www_rule_selinux_state")
     assert not autotailor.is_valid_xccdf_id("xccdf_rule_selinux_state")
     assert not autotailor.is_valid_xccdf_id("xccdf_com.example.www_rule_")
+
+
+def test_full_id():
+    t = autotailor.Tailoring()
+    assert t._full_rule_id("accounts_tmout") == \
+        "xccdf_org.ssgproject.content_rule_accounts_tmout"
+    assert t._full_rule_id(
+        "xccdf_org.ssgproject.content_rule_accounts_tmout") == \
+        "xccdf_org.ssgproject.content_rule_accounts_tmout"
+    assert t._full_profile_id("stig") == \
+        "xccdf_org.ssgproject.content_profile_stig"
+    assert t._full_profile_id(
+        "xccdf_org.ssgproject.content_profile_stig") == \
+        "xccdf_org.ssgproject.content_profile_stig"
+    assert t._full_var_id("var_crypto_policy") == \
+        "xccdf_org.ssgproject.content_value_var_crypto_policy"
+    assert t._full_var_id(
+        "xccdf_org.ssgproject.content_value_var_crypto_policy") == \
+        "xccdf_org.ssgproject.content_value_var_crypto_policy"
