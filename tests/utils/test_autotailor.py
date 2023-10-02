c3e32108cf70ecd7f91c2b58a1af23ec4281b52d
Jan Černý <jcerny@redhat.com>
2023-10-02 15:13:30 +0200

    Add unit test for `is_valid_xccdf_id`
    and fix the failed asserts

--- tests/utils/test_autotailor.py
+++ tests/utils/test_autotailor.py
@@ -10,3 +10,16 @@ def import_arbitrary_file_as_module(path, module_name):
 
 
 autotailor = import_arbitrary_file_as_module("utils/autotailor", "autotailor")
+
+
+def test_is_valid_xccdf_id():
+    assert autotailor.is_valid_xccdf_id(
+        "xccdf_com.example.www_profile_abcd")
+    assert autotailor.is_valid_xccdf_id(
+        "xccdf_com.example.www_rule_selinux_state")
+    assert not autotailor.is_valid_xccdf_id("")
+    assert not autotailor.is_valid_xccdf_id("foo")
+    assert not autotailor.is_valid_xccdf_id(
+        "xccdf_com_example_www_rule_selinux_state")
+    assert not autotailor.is_valid_xccdf_id("xccdf_rule_selinux_state")
+    assert not autotailor.is_valid_xccdf_id("xccdf_com.example.www_rule_")
