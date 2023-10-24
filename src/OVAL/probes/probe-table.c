557ddeed1e3e234a655ad77a691869554064b293
David du Colombier <djc@datadoghq.com>
2023-10-24 09:14:24 +0200

    Rewrite dpkginfo probe without using APT
    This change rewrites the dpkginfo probe without using the
    APT library.
    
    The dpkginfo now parses the list of installed package
    (/var/lib/dpkg/status) directly, instead of relying on
    the APT library.
    
    This prevents loading the full list of packages in memory
    and various issues related to the use of the APT library.
    
    The dpkginfo probe is now stateless and doesn't require
    init and fini functions. Also, the dpkginfo_get_by_name
    function can now be called from multiple threads without
    having to be protected by a lock.
    
    The dependency on the APT library has been removed from OpenSCAP.

--- src/OVAL/probes/probe-table.c
+++ src/OVAL/probes/probe-table.c
@@ -257,7 +257,7 @@ static const probe_table_entry_t probe_table[] = {
 	{OVAL_INDEPENDENT_YAML_FILE_CONTENT, NULL, yamlfilecontent_probe_main, NULL, yamlfilecontent_probe_offline_mode_supported},
 #endif
 #ifdef OPENSCAP_PROBE_LINUX_DPKGINFO
-	{OVAL_LINUX_DPKG_INFO, dpkginfo_probe_init, dpkginfo_probe_main, dpkginfo_probe_fini, dpkginfo_probe_offline_mode_supported},
+	{OVAL_LINUX_DPKG_INFO, NULL, dpkginfo_probe_main, NULL, dpkginfo_probe_offline_mode_supported},
 #endif
 #ifdef OPENSCAP_PROBE_LINUX_IFLISTENERS
 	{OVAL_LINUX_IFLISTENERS, NULL, iflisteners_probe_main, NULL, NULL},
--- src/OVAL/probes/unix/linux/dpkginfo-helper.h
+++ src/OVAL/probes/unix/linux/dpkginfo-helper.h
@@ -22,10 +22,6 @@
 #ifndef __DPKGINFO_HELPER__
 #define __DPKGINFO_HELPER__
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
 struct dpkginfo_reply_t {
         char *name;
         char *arch;
@@ -35,15 +31,8 @@ struct dpkginfo_reply_t {
         char *evr;
 };
 
-int dpkginfo_init();
-int dpkginfo_fini();
-
 struct dpkginfo_reply_t * dpkginfo_get_by_name(const char *name, int *err);
 
 void dpkginfo_free_reply(struct dpkginfo_reply_t *reply);
-
-#ifdef __cplusplus
-}
-#endif
 
 #endif /* __DPKGINFO_HELPER__ */
