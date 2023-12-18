c316bc4cc61a3646dec26bcf60884662e9c22c5a
Jan Černý <jcerny@redhat.com>
2023-12-18 18:10:14 +0100

    Extend test_skip_paths
    Extend test_skip_paths to test the "skip selected paths" feature
    also in the xmlfilecontent probe.

--- tests/API/OVAL/skip_paths/test_skip_paths.sh
+++ tests/API/OVAL/skip_paths/test_skip_paths.sh
@@ -8,10 +8,13 @@ result=$(mktemp)
 root="/tmp/oscap_test_skip_paths"
 mkdir -p "$root/a"
 touch "$root/a/x"
+cp "$srcdir/test.xml" "$root/a/"
 mkdir -p "$root/b"
 touch "$root/b/y"
+cp "$srcdir/test.xml" "$root/b/"
 mkdir -p "$root/c"
 touch "$root/c/z"
+cp "$srcdir/test.xml" "$root/c/"
 # oscap probes will skip directories "b" and "c"
 export OSCAP_PROBE_IGNORE_PATHS="$root/b:$root/c"
 $OSCAP oval eval --results $result "$srcdir/test_skip_paths.xml"
@@ -37,5 +40,12 @@ assert_exists 1 '/oval_results/results/system/definitions/definition[@definition
 assert_exists 1 '/oval_results/results/system/oval_system_characteristics/collected_objects/object[@id="oval:x:obj:6" and @flag="does not exist"]'
 assert_exists 0 '/oval_results/results/system/oval_system_characteristics/system_data/ind-sys:filehash58_item/ind-sys:filepath[text()="/tmp/oscap_test_skip_paths/b/y"]'
 assert_exists 0 '/oval_results/results/system/oval_system_characteristics/system_data/ind-sys:filehash58_item/ind-sys:filepath[text()="/tmp/oscap_test_skip_paths/c/z"]'
+assert_exists 1 '/oval_results/results/system/definitions/definition[@definition_id="oval:x:def:7" and @result="true"]'
+assert_exists 1 '/oval_results/results/system/oval_system_characteristics/collected_objects/object[@id="oval:x:obj:7" and @flag="complete"]'
+assert_exists 1 '/oval_results/results/system/oval_system_characteristics/system_data/ind-sys:xmlfilecontent_item/ind-sys:filepath[text()="/tmp/oscap_test_skip_paths/a/test.xml"]'
+assert_exists 1 '/oval_results/results/system/definitions/definition[@definition_id="oval:x:def:8" and @result="true"]'
+assert_exists 1 '/oval_results/results/system/oval_system_characteristics/collected_objects/object[@id="oval:x:obj:8" and @flag="does not exist"]'
+assert_exists 0 '/oval_results/results/system/oval_system_characteristics/system_data/ind-sys:xmlfilecontent_item/ind-sys:filepath[text()="/tmp/oscap_test_skip_paths/b/test.xml"]'
+assert_exists 0 '/oval_results/results/system/oval_system_characteristics/system_data/ind-sys:xmlfilecontent_item/ind-sys:filepath[text()="/tmp/oscap_test_skip_paths/c/test.xml"]'
 rm -f $result
 rm -rf "$root"
