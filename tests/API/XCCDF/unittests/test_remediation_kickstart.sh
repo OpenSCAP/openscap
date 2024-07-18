796cf3278aad1ee46a36c0a19fe361c87540dde4
Jan Černý <jcerny@redhat.com>
2024-07-18 16:01:00 +0200

    Extend test test_remediation_kickstart.sh

--- tests/API/XCCDF/unittests/test_remediation_kickstart.sh
+++ tests/API/XCCDF/unittests/test_remediation_kickstart.sh
@@ -3,6 +3,7 @@
 
 set -e -o pipefail
 
+
 kickstart=$(mktemp)
 stderr=$(mktemp)
 
@@ -20,7 +21,11 @@ stderr=$(mktemp)
 
 $OSCAP xccdf generate fix --fix-type kickstart --output "$kickstart" --profile common "$srcdir/test_remediation_kickstart.ds.xml"
 
-grep -q "# Kickstart for Common hardening profile" "$kickstart"
+grep -q '# Kickstart for Common hardening profile' "$kickstart"
+grep -q 'services --disabled=telnet --enabled=auditd,rsyslog,sshd' "$kickstart"
+grep -q 'logvol /var/tmp --fstype=xfs --name=vartmp --vgname=VolGroup --size=1024' "$kickstart"
+grep -q 'mkdir /etc/scap' "$kickstart"
+grep -q '\-usbguard' "$kickstart"
 
 rm -rf "$kickstart"
 rm -rf "$stderr"
