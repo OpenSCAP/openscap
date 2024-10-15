d977ad569ecec37e1124ae52493f09ebf38ae256
Jan Černý <jcerny@redhat.com>
2024-10-15 11:22:52 +0200

    Add a test case for the invalid value

--- tests/sce/test_prefer_sce.sh
+++ tests/sce/test_prefer_sce.sh
@@ -44,7 +44,21 @@ test_prefer_oval_explicit () {
     rm -rf "$stdout" "$stderr" "$arf"
 }
 
+test_invalid_envi_variable () {
+    stdout=$(mktemp)
+    stderr=$(mktemp)
+    arf=$(mktemp)
+
+    OSCAP_PREFERRED_ENGINE="FOOBARVIM" $OSCAP xccdf eval --verbose INFO --progress --profile common --results-arf "$arf" > "$stdout" 2> "$stderr" "$srcdir/test_prefer_sce.ds.xml" || ret="$?"
+    grep -q "Unknown value of OSCAP_PREFFERED_ENGINE: 'FOOBARVIM'. It will be ignored." "$stderr"
+    grep -q "xccdf_org.openscap.www_rule_1:pass" "$stdout"
+    grep -q "I: oscap: Evaluating definition 'oval:org.openscap.www:def:1': OVAL check for rule 1." "$stderr"
+    ! grep -q "I: oscap: Executing SCE check 'fedora/checks/sce/rule_1.sh'" "$stderr"
+
+    rm -rf "$stdout" "$stderr" "$arf"
+}
 
 test_prefer_sce_on
 test_prefer_sce_off
 test_prefer_oval_explicit
+test_invalid_envi_variable
